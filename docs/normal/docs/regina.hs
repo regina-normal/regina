84ada53ae9247f3f29edd2aa6127718fa914983b
Benjamin Burton <bab@debian.org>
2001-02-24 18:42:30 +0000

    Initial revision

---
+++ docs/normal/docs/regina.hs
@@ -0,0 +1,16 @@
+
+<helpset>
+	<title>Regina - A Normal Surface Theory Calculator</title>
+	
+	<maps>
+		<homeID>welcome</homeID>
+		<mapref location="regina.jhm"/>
+	</maps>
+
+	<view>
+		<name>TOC</name>
+		<label>Table of Contents</label>
+		<type>javax.help.TOCView</type>
+		<data>reginaTOC.xml</data>
+	</view>
+</helpset>
