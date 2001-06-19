20b14ec6ce9b6f392c55df84ff0a4a3729a72b24
Benjamin Burton <bab@debian.org>
2001-06-19 00:01:03 +0000

    Displays fundamental group.

--- javaui/normal/packetui/triangulation/AlgebraViewer.java
+++ javaui/normal/packetui/triangulation/AlgebraViewer.java
@@ -29,10 +29,13 @@
 package normal.packetui.triangulation;
 
 import java.awt.*;
+import java.util.*;
 import javax.swing.*;
+import normal.engine.algebra.*;
 import normal.engine.packet.NPacket;
 import normal.engine.triangulation.*;
 import normal.packetui.*;
+import btools.gui.component.PaddedPane;
 
 /**
  * An interface through which a user can view the algebraic details
@@ -42,61 +45,51 @@ public class AlgebraViewer extends DefaultPacketViewer
         implements PacketInfoUI {
     /**
      * The triangulation under examination.
-     * @serial
      */
     private NTriangulation triangulation;
 
     /**
      * Displays the first homology group.
-     * @serial
      */
     private JLabel H1;
     /**
      * Displays the first homology group relative to the boundary.
-     * @serial
      */
     private JLabel H1Rel;
     /**
      * Displays the first homology group of the boundary.
-     * @serial
      */
     private JLabel H1Bdry;
     /**
      * Displays the second homology group.
-     * @serial
      */
     private JLabel H2;
     /**
      * Displays the second homology group with coefficients in Z_2.
-     * @serial
      */
     private JLabel H2Z2;
 
-    /**
-     * Label for the first homology group.
-     * @serial
-     */
-    private JLabel labelH1;
-    /**
-     * Label for the first homology group relative to the boundary.
-     * @serial
-     */
-    private JLabel labelH1Rel;
-    /**
-     * Label for the first homology group of the boundary.
-     * @serial
-     */
-    private JLabel labelH1Bdry;
-    /**
-     * Label for the second homology group.
-     * @serial
-     */
-    private JLabel labelH2;
-    /**
-     * Label for the second homology group with coefficients in Z_2.
-     * @serial
-     */
-    private JLabel labelH2Z2;
+	/**
+	 * The short name of the fundamental group.
+	 */
+	private JLabel fgpName;
+	/**
+	 * The generators of the fundamental group.
+	 */
+	private JLabel fgpGenerators;
+	/**
+	 * The relations in the fundamental group.
+	 */
+	private JList fgpRelations;
+	/**
+	 * The list model for the fundamental group relations.
+	 */
+	private DefaultListModel fgpRelModel;
+	
+	/**
+	 * Components to disable when the packet is being edited.
+	 */
+	private Vector componentsToDisable = new Vector();
 
     /**
      * Create a new interface to display the given algebraic information.
@@ -114,43 +107,109 @@ public class AlgebraViewer extends DefaultPacketViewer
      * Create the interface elements.
      */
     private void init() {
-        // Set the layout manager.
-        setLayout(new GridBagLayout());
-        
-        // Initialise the components.
+        // Set up the homology pane.
+		JPanel homologyPane = new JPanel();
+        homologyPane.setLayout(new GridBagLayout());
+
         H1 = new JLabel();
         H1Rel = new JLabel();
         H1Bdry = new JLabel();
         H2 = new JLabel();
         H2Z2 = new JLabel();
+		componentsToDisable.addElement(H1);
+		componentsToDisable.addElement(H1Rel);
+		componentsToDisable.addElement(H1Bdry);
+		componentsToDisable.addElement(H2);
+		componentsToDisable.addElement(H2Z2);
 
-        labelH1 = new JLabel("H1(M)");
-        labelH1Rel = new JLabel("H1(M, Bdry M)");
-        labelH1Bdry = new JLabel("H1(Bdry M)");
-        labelH2 = new JLabel("H2(M)");
-        labelH2Z2 = new JLabel("H2(M ; Z_2)");
+		JLabel labelHom = new JLabel("Homology");
+        JLabel labelH1 = new JLabel("H1(M)");
+        JLabel labelH1Rel = new JLabel("H1(M, Bdry M)");
+        JLabel labelH1Bdry = new JLabel("H1(Bdry M)");
+        JLabel labelH2 = new JLabel("H2(M)");
+        JLabel labelH2Z2 = new JLabel("H2(M ; Z_2)");
+		componentsToDisable.addElement(labelHom);
+		componentsToDisable.addElement(labelH1);
+		componentsToDisable.addElement(labelH1Rel);
+		componentsToDisable.addElement(labelH1Bdry);
+		componentsToDisable.addElement(labelH2);
+		componentsToDisable.addElement(labelH2Z2);
 
+		GridBagConstraints cTitle = new GridBagConstraints();
         GridBagConstraints cLabel = new GridBagConstraints();
         GridBagConstraints cTotal = new GridBagConstraints();
+		cTitle.anchor = cTitle.CENTER;
+		cTitle.fill = cTitle.NONE;
+		cTitle.gridx = 0;
+		cTitle.gridwidth = cTitle.REMAINDER;
+		cTitle.insets = new Insets(0, 0, 5, 0);
         cLabel.anchor = cLabel.WEST;
         cLabel.fill = cLabel.NONE;
         cLabel.gridx = 0;
         cTotal.anchor = cTotal.CENTER;
         cTotal.fill = cTotal.NONE;
         cTotal.gridx = 1;
-        cTotal.insets = new Insets(0, 5, 0, 5);
+        cTotal.insets = new Insets(0, 10, 0, 5);
 
-        // Insert the components.
-        add(labelH1, cLabel);
-        add(H1, cTotal);
-        add(labelH1Rel, cLabel);
-        add(H1Rel, cTotal);
-        add(labelH1Bdry, cLabel);
-        add(H1Bdry, cTotal);
-        add(labelH2, cLabel);
-        add(H2, cTotal);
-        add(labelH2Z2, cLabel);
-        add(H2Z2, cTotal);
+		homologyPane.add(labelHom, cTitle);
+        homologyPane.add(labelH1, cLabel);
+        homologyPane.add(H1, cTotal);
+        homologyPane.add(labelH1Rel, cLabel);
+        homologyPane.add(H1Rel, cTotal);
+        homologyPane.add(labelH1Bdry, cLabel);
+        homologyPane.add(H1Bdry, cTotal);
+        homologyPane.add(labelH2, cLabel);
+        homologyPane.add(H2, cTotal);
+        homologyPane.add(labelH2Z2, cLabel);
+        homologyPane.add(H2Z2, cTotal);
+
+		// Set up the fundamental group pane.
+		JPanel fgpPane = new JPanel();
+		fgpPane.setLayout(new GridBagLayout());
+
+		fgpName = new JLabel();
+		fgpGenerators = new JLabel();
+		fgpRelModel = new DefaultListModel();
+		fgpRelations = new JList(fgpRelModel);
+		fgpRelations.setVisibleRowCount(6);
+		fgpRelations.setBorder(BorderFactory.createLoweredBevelBorder());
+		componentsToDisable.addElement(fgpName);
+		componentsToDisable.addElement(fgpGenerators);
+		componentsToDisable.addElement(fgpRelations);
+
+		JLabel labelFgpName = new JLabel("Fundamental Group");
+		JLabel labelFgpRelations = new JLabel("Relations:");
+		componentsToDisable.add(labelFgpName);
+		componentsToDisable.add(labelFgpRelations);
+
+        GridBagConstraints cLabel2 = new GridBagConstraints();
+        GridBagConstraints cTotal2 = new GridBagConstraints();
+		GridBagConstraints cList = new GridBagConstraints();
+        cLabel2.anchor = cLabel.CENTER;
+        cLabel2.fill = cLabel.NONE;
+        cLabel2.gridx = 0;
+        cTotal2.anchor = cTotal.CENTER;
+        cTotal2.fill = cTotal.NONE;
+        cTotal2.gridx = 0;
+        cTotal2.insets = new Insets(0, 0, 5, 0);
+		cList.anchor = cList.CENTER;
+		cList.fill = cList.BOTH;
+		cList.gridx = 0;
+		cList.weightx = 1;
+		cList.weighty = 1;
+
+		fgpPane.add(labelFgpName, cTitle);
+		fgpPane.add(fgpName, cTotal2);
+		fgpPane.add(fgpGenerators, cLabel2);
+		fgpPane.add(labelFgpRelations, cLabel2);
+		fgpPane.add(fgpRelations, cList);
+
+		// Put everything together.
+		JSplitPane all = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+			homologyPane, new PaddedPane(fgpPane, 5));
+		all.setResizeWeight(0.5);
+		setLayout(new BorderLayout());
+		add(all, BorderLayout.CENTER);
     }
 
     public NPacket getPacket() {
@@ -158,16 +217,9 @@ public class AlgebraViewer extends DefaultPacketViewer
     }
 
     public void reflectPacket() {
-        H1.setEnabled(true);
-        H1Rel.setEnabled(true);
-        H1Bdry.setEnabled(true);
-        H2.setEnabled(true);
-        H2Z2.setEnabled(true);
-        labelH1.setEnabled(true);
-        labelH1Rel.setEnabled(true);
-        labelH1Bdry.setEnabled(true);
-        labelH2.setEnabled(true);
-        labelH2Z2.setEnabled(true);
+		Enumeration e = componentsToDisable.elements();
+		while (e.hasMoreElements())
+			((Component)e.nextElement()).setEnabled(true);
 
         H1.setText(triangulation.getHomologyH1().toString());
 
@@ -188,24 +240,43 @@ public class AlgebraViewer extends DefaultPacketViewer
             H2.setText("Invalid Triangulation");
             H2Z2.setText("Invalid Triangulation");
         }
+
+		NGroupPresentation fgp = triangulation.getFundamentalGroup();
+		String name = fgp.recogniseGroup();
+		if (name.length() == 0)
+			fgpName.setText("Not recognised");
+		else
+			fgpName.setText(name);
+
+		long n = fgp.getNumberOfGenerators();
+		if (n == 0)
+			fgpGenerators.setText("Generators: (none)");
+		else if (n == 1)
+			fgpGenerators.setText("Generator: g0");
+		else if (n == 2)
+			fgpGenerators.setText("Generators: g0, g1");
+		else
+			fgpGenerators.setText("Generators: g0 .. g" +
+				String.valueOf(n - 1));
+
+		n = fgp.getNumberOfRelations();
+		for (long i =0; i < n; i++)
+			fgpRelModel.addElement("1 = " + fgp.getRelation(i).toString());
     }
 
     public void editingElsewhere() {
-        H1.setEnabled(false);
-        H1Rel.setEnabled(false);
-        H1Bdry.setEnabled(false);
-        H2.setEnabled(false);
-        H2Z2.setEnabled(false);
-        labelH1.setEnabled(false);
-        labelH1Rel.setEnabled(false);
-        labelH1Bdry.setEnabled(false);
-        labelH2.setEnabled(false);
-        labelH2Z2.setEnabled(false);
+		Enumeration e = componentsToDisable.elements();
+		while (e.hasMoreElements())
+			((Component)e.nextElement()).setEnabled(false);
 
         H1.setText("");
         H1Rel.setText("");
         H1Bdry.setText("");
         H2.setText("");
         H2Z2.setText("");
+
+		fgpName.setText("");
+		fgpGenerators.setText("Generators:");
+		fgpRelModel.removeAllElements();
     }
 }
