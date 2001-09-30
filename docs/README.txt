63c73c98fea6eed5666f8dd967551cc0c692a2c3
Benjamin Burton <bab@debian.org>
2001-09-30 03:13:32 +0000

    Complete documentation overhaul including move to SGML.

--- docs/README.txt
+++ docs/README.txt
@@ -1,4 +1,1 @@
 This directory tree contains the documentation for Regina.
-
-  man/ : Man pages for regina startup scripts
-  normal/ : Full HTML documentation
---
+++ docs/regina.hs
@@ -0,0 +1,9 @@
+
+<helpset>
+	<title>Regina - A Normal Surface Theory Calculator</title>
+	
+	<maps>
+		<homeID>index</homeID>
+		<mapref location="regina.jhm"/>
+	</maps>
+</helpset>
