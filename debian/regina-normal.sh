4a62d56a901f73076b6f52223dbac0f40155e300
Benjamin Burton <bab@debian.org>
2001-10-02 06:12:01 +0000

    Don't clobber any existing environment variables.

--- debian/regina-normal.sh
+++ debian/regina-normal.sh
@@ -2,11 +2,28 @@
 # Wrapper startup script that sets environment variables and calls the
 # real startup script.
 
-export REGINA_HOME=/usr/share/regina-normal
-export REGINA_OPTIONS_GLOBAL=/etc/regina-normal
-export REGINA_BTOOLS=/usr/share/java/libbtools-java.jar
-export REGINA_JYTHON=/usr/share/java/jython.jar
-export REGINA_JNIDIR=/usr/lib/regina-normal/lib
-export REGINA_JARDIR=/usr/share/regina-normal/jar
+if [ -z "$REGINA_HOME" ]; then
+  export REGINA_HOME=/usr/share/regina-normal
+fi
+if [ -z "$REGINA_OPTIONS_GLOBAL" ]; then
+  export REGINA_OPTIONS_GLOBAL=/etc/regina-normal
+fi
+if [ -z "$REGINA_BTOOLS" ]; then
+  export REGINA_BTOOLS=/usr/share/java/libbtools-java.jar
+fi
+if [ -z "$REGINA_JYTHON" ]; then
+  export REGINA_JYTHON=/usr/share/java/jython.jar
+fi
+if [ -z "$REGINA_JNIDIR" ]; then
+  export REGINA_JNIDIR=/usr/lib/regina-normal/lib
+fi
+if [ -z "$REGINA_JARDIR" ]; then
+  export REGINA_JARDIR=/usr/share/regina-normal/jar
+fi
+
+# Change the default verbosity to quiet.
+if [ -z "$REGINA_VERBOSITY" ]; then
+  export REGINA_VERBOSITY=0
+fi
 
 /usr/share/regina-normal/regina "$@"
