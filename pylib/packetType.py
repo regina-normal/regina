8f36c14b538a2b96bce8ca818153affa4c092759
Benjamin Burton <bab@debian.org>
2002-06-15 04:41:47 +0000

    Slight rearrangement; added angle structure list recognition.

--- pylib/packetType.py
+++ pylib/packetType.py
@@ -1,11 +1,39 @@
 # A library of routines to determine if a packet is of a particular type.
 
+# Is the given packet an angle structure list?
+#
+# Return: 1 for true, 0 for false.
+def isAngleStructureList(packet):
+    return (packet.getPacketType() == \
+            normal.engine.angle.NAngleStructureList.packetType)
+
 # Is the given packet a container?
 #
 # Return: 1 for true, 0 for false.
 def isContainer(packet):
     return (packet.getPacketType() == \
             normal.engine.packet.NContainer.packetType)
+
+# Is the given packet a script?
+#
+# Return: 1 for true, 0 for false.
+def isScript(packet):
+    return (packet.getPacketType() == \
+            normal.engine.packet.NScript.packetType)
+
+# Is the given packet a surface filter?
+#
+# Return: 1 for true, 0 for false.
+def isSurfaceFilter(packet):
+    return (packet.getPacketType() == \
+            normal.engine.surfaces.NSurfaceFilter.packetType)
+
+# Is the given packet a normal surface list?
+#
+# Return: 1 for true, 0 for false.
+def isSurfaceList(packet):
+    return (packet.getPacketType() == \
+            normal.engine.surfaces.NNormalSurfaceList.packetType)
 
 # Is the given packet a text packet?
 #
@@ -20,25 +48,4 @@ def isText(packet):
 def isTriangulation(packet):
     return (packet.getPacketType() == \
             normal.engine.triangulation.NTriangulation.packetType)
-
-# Is the given packet a normal surface list?
-#
-# Return: 1 for true, 0 for false.
-def isSurfaceList(packet):
-    return (packet.getPacketType() == \
-            normal.engine.surfaces.NNormalSurfaceList.packetType)
-
-# Is the given packet a surface filter?
-#
-# Return: 1 for true, 0 for false.
-def isSurfaceFilter(packet):
-    return (packet.getPacketType() == \
-            normal.engine.surfaces.NSurfaceFilter.packetType)
-
-# Is the given packet a script?
-#
-# Return: 1 for true, 0 for false.
-def isScript(packet):
-    return (packet.getPacketType() == \
-            normal.engine.packet.NScript.packetType)
 
