351798da2e8b228d590e5ab62dcc11cf0e8e9860
Benjamin Burton <bab@debian.org>
2002-07-09 07:33:40 +0000

    Added copyright/license notice.

--- admin/ac_gen.sh
+++ admin/ac_gen.sh
@@ -1,9 +1,30 @@
-#!/bin/sh
-# CVS utility script for Regina.
+#
+# Regina - A Normal Surface Theory Calculator
+# CVS Utility Script
+#
+# Copyright (c) 2002, Ben Burton
+# For further details contact Ben Burton (benb@acm.org).
+#
 # Regenerates ../configure.ac.
 #
 # This script should NOT be run directly; to use it run "make -f Makefile.cvs"
 # from the top-level source directory (the directory above this).
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
 
 set -e
 
