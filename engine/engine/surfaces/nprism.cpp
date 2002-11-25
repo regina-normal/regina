3ad841b20774362c39ab464bff74160482af2d30
Benjamin Burton <bab@debian.org>
2002-11-25 07:22:24 +0000

    New prism classes.

---
+++ engine/engine/surfaces/nprism.cpp
@@ -0,0 +1,63 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2002, Ben Burton                                   *
+ *  For further details contact Ben Burton (benb@acm.org).                *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,        *
+ *  MA 02111-1307, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+#include "surfaces/nnormalsurface.h"
+#include "surfaces/nprism.h"
+#include "triangulation/ntriangulation.h"
+
+namespace regina {
+
+std::ostream& operator << (std::ostream& out, const NPrismSpec& spec) {
+    out << '(' << spec.tetIndex << ", " << spec.edge << ')';
+    return out;
+}
+
+NPrismSetSurface::NPrismSetSurface(const NNormalSurface& surface) {
+    NTriangulation* tri = surface.getTriangulation();
+    unsigned long nTet = tri->getNumberOfTetrahedra();
+
+    if (nTet == 0) {
+        quadType = 0;
+        return;
+    }
+
+    // Work out which tetrahedra contain which quad types.
+    quadType = new (signed char)[nTet];
+    for (unsigned long tet = 0; tet < nTet; tet++)
+        if (surface.getQuadCoord(tet, 0) != 0)
+            quadType[tet] = 0;
+        else if (surface.getQuadCoord(tet, 1) != 0)
+            quadType[tet] = 1;
+        else if (surface.getQuadCoord(tet, 2) != 0)
+            quadType[tet] = 2;
+        else
+            quadType[tet] = -1;
+}
+
+} // namespace regina
+
---
+++ engine/engine/surfaces/nprism.h
@@ -0,0 +1,192 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2002, Ben Burton                                   *
+ *  For further details contact Ben Burton (benb@acm.org).                *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,        *
+ *  MA 02111-1307, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+/*! \file nprism.h
+ *  \brief Deals with triangular prisms defined by slicing along normal
+ *  quads in a tetrahedron.
+ */
+
+#ifndef __NPRISM_H
+#ifndef __DOXYGEN
+#define __NPRISM_H
+#endif
+
+#include <iostream>
+
+namespace regina {
+
+/**
+ * \weakgroup surfaces
+ * @{
+ */
+
+class NNormalSurface;
+
+/**
+ * Specifies a single triangular prism in a tetrahedron.
+ *
+ * If a tetrahedron contains normal quads, slicing along these quads
+ * splits the tetrahedron into two triangular prisms (and possibly some
+ * additional product regions).  Each triangular prism contains two of
+ * the vertices and one of the edges of the original tetrahedron.
+ *
+ * \pre This class should only be used with \a embedded
+ * normal surfaces.
+ *
+ * \ifaces Not present.
+ */
+struct NPrismSpec {
+    unsigned long tetIndex;
+        /**< The index in the triangulation of the tetrahedron
+             containing the prism. */
+    int edge;
+        /**< The edge of the tetrahedron that is contained in this prism. */
+
+    /**
+     * Creates a new uninitialised prism specifier.
+     */
+    NPrismSpec();
+    /**
+     * Creates a new prism specifier containing the given values.
+     *
+     * @param newTetIndex the index in the triangulation of the tetrahedron
+     * containing the prism.
+     * @param newEdge the edge of the tetrahedron that is contained in
+     * this prism; this must be between 0 and 5 inclusive.
+     */
+    NPrismSpec(unsigned long newTetIndex, int newEdge);
+    /**
+     * Creates a new prism specifier that is a clone of the given specifier.
+     *
+     * @param cloneMe the prism specifier to clone.
+     */
+    NPrismSpec(const NPrismSpec& cloneMe);
+
+    /**
+     * Copies the values from the given prism specifier into this specifier.
+     *
+     * @param cloneMe the prism specifier whose values should be copied.
+     * @return a reference to this prism specifier.
+     */
+    NPrismSpec& operator = (const NPrismSpec& cloneMe);
+    /**
+     * Determines if this and the given prism specifier contain identical
+     * information.
+     *
+     * @param other the prism specifier to compare with this.
+     * @return \c true if and only if this and the given prism specifier
+     * contain identical information.
+     */
+    bool operator == (const NPrismSpec& other) const;
+
+    friend std::ostream& operator << (std::ostream& out,
+        const NPrismSpec& spec);
+};
+
+/**
+ * Writes the given prism specifier to the given output stream.
+ * The prism specifier will be written as a pair <tt>(tetIndex, edge)</tt>.
+ *
+ * \ifaces Not present.
+ *
+ * @param out the output stream to which to write.
+ * @param spec the prism specifier to write.
+ * @return a reference to \a out.
+ */
+std::ostream& operator << (std::ostream& out, const NPrismSpec& spec);
+
+/**
+ * Represents the set of prisms defined by slicing along all the quads
+ * in a particular normal surface.
+ *
+ * Note that each tetrahedron in the underlying triangulation will supply
+ * either zero or two prisms (depending upon whether or not it contains
+ * any normal quads).
+ *
+ * \pre This class should only be used with \e embedded normal surfaces
+ * containing no octahedral discs.
+ *
+ * \ifaces Not present.
+ */
+class NPrismSetSurface {
+    private:
+        signed char* quadType;
+            /**< A list of which types of normal quad are contained in which
+                 tetrahedra.  Tetrahedra containing no quads at all
+                 will have quad type -1 in this array. */
+    public:
+        /**
+         * Creates a new prism set corresponding to the prisms defined
+         * by the given normal surface.
+         *
+         * \pre The given normal surface is embedded and contains no
+         * octahedral discs.
+         *
+         * @param surface the normal surface that defines the prisms in
+         * this set.
+         */
+        NPrismSetSurface(const NNormalSurface& surface);
+        /**
+         * Destroys this prism set.
+         */
+        virtual ~NPrismSetSurface();
+};
+
+/*@}*/
+
+// Inline functions for NPrismSpec
+
+inline NPrismSpec::NPrismSpec() {
+}
+inline NPrismSpec::NPrismSpec(unsigned long newTetIndex, int newEdge) :
+        tetIndex(newTetIndex), edge(newEdge) {
+}
+inline NPrismSpec::NPrismSpec(const NPrismSpec& cloneMe) :
+        tetIndex(cloneMe.tetIndex), edge(cloneMe.edge) {
+}
+
+inline NPrismSpec& NPrismSpec::operator = (const NPrismSpec& cloneMe) {
+    tetIndex = cloneMe.tetIndex;
+    edge = cloneMe.edge;
+    return *this;
+}
+inline bool NPrismSpec::operator == (const NPrismSpec& other) const {
+    return (tetIndex == other.tetIndex && edge == other.edge);
+}
+
+// Inline functions for NPrismSetSurface
+
+inline NPrismSetSurface::~NPrismSetSurface() {
+    if (quadType)
+        delete[] quadType;
+}
+
+} // namespace regina
+
+#endif
+
