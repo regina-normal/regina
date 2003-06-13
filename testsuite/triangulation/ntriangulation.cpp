244ef854713ba2e40f915599216410350d89bccc
Benjamin Burton <bab@debian.org>
2003-06-13 00:33:54 +0000

    Added homology checks and a 100-tetrahedron triangulation.

--- testsuite/triangulation/ntriangulation.cpp
+++ testsuite/triangulation/ntriangulation.cpp
@@ -28,9 +28,11 @@
 
 #include <sstream>
 #include <cppunit/extensions/HelperMacros.h>
+#include "algebra/nabeliangroup.h"
 #include "triangulation/ntriangulation.h"
 #include "testsuite/triangulation/testtriangulation.h"
 
+using regina::NAbelianGroup;
 using regina::NPerm;
 using regina::NTetrahedron;
 using regina::NTriangulation;
@@ -40,13 +42,16 @@ class NTriangulationTest : public CppUnit::TestFixture {
 
     CPPUNIT_TEST(validity);
     CPPUNIT_TEST(orientability);
+    CPPUNIT_TEST(homologyH1);
 
     CPPUNIT_TEST_SUITE_END();
 
     private:
-        NTriangulation lens83;
+        NTriangulation lens8_3;
             /**< The lens space L(8,3) : closed, orbl. */
-        NTriangulation lst347;
+        NTriangulation lens100_1;
+            /**< The lens space L(8,3) : closed, orbl. */
+        NTriangulation lst3_4_7;
             /**< The layered solid torus LST(3,4,7) : bounded, orbl. */
         NTriangulation figure8;
             /**< The figure eight knot complement : ideal, orbl. */
@@ -60,8 +65,9 @@ class NTriangulationTest : public CppUnit::TestFixture {
     public:
         void setUp() {
             // Some of our triangulations can be constructed automatically.
-            lens83.insertLensSpace(8, 3);
-            lst347.insertLayeredSolidTorus(3, 4);
+            lens8_3.insertLensSpace(8, 3);
+            lens100_1.insertLensSpace(100, 1);
+            lst3_4_7.insertLayeredSolidTorus(3, 4);
 
             // The rest alas must be done manually.
             NTetrahedron* r;
@@ -114,9 +120,11 @@ class NTriangulationTest : public CppUnit::TestFixture {
 
         void validity() {
             CPPUNIT_ASSERT_MESSAGE("L(8,3) is not valid.",
-                lens83.isValid());
+                lens8_3.isValid());
+            CPPUNIT_ASSERT_MESSAGE("L(100,1) is not valid.",
+                lens100_1.isValid());
             CPPUNIT_ASSERT_MESSAGE("LST(3,4,7) is not valid.",
-                lst347.isValid());
+                lst3_4_7.isValid());
             CPPUNIT_ASSERT_MESSAGE("The figure eight knot complement "
                 "is not valid.",
                 figure8.isValid());
@@ -130,9 +138,11 @@ class NTriangulationTest : public CppUnit::TestFixture {
 
         void orientability() {
             CPPUNIT_ASSERT_MESSAGE("L(8,3) is not orientable.",
-                lens83.isOrientable());
+                lens8_3.isOrientable());
+            CPPUNIT_ASSERT_MESSAGE("L(100,1) is not orientable.",
+                lens100_1.isOrientable());
             CPPUNIT_ASSERT_MESSAGE("LST(3,4,7) is not orientable.",
-                lst347.isOrientable());
+                lst3_4_7.isOrientable());
             CPPUNIT_ASSERT_MESSAGE("The figure eight knot complement "
                 "is not orientable.",
                 figure8.isOrientable());
@@ -142,6 +152,35 @@ class NTriangulationTest : public CppUnit::TestFixture {
                 ! solidKB.isOrientable());
             CPPUNIT_ASSERT_MESSAGE("The Gieseking manifold is orientable.",
                 ! gieseking.isOrientable());
+        }
+
+        bool verifyGroup(const NAbelianGroup& g, unsigned rank,
+                unsigned long torsionDegree) {
+            if (g.getRank() != rank)
+                return false;
+
+            if (torsionDegree == 1)
+                return (g.getNumberOfInvariantFactors() == 0);
+            else
+                return (g.getNumberOfInvariantFactors() == 1 &&
+                    g.getInvariantFactor(0) == torsionDegree);
+        }
+
+        void homologyH1() {
+            CPPUNIT_ASSERT_MESSAGE("H1(L(8,3)) is not Z_8.",
+                verifyGroup(lens8_3.getHomologyH1(), 0, 8));
+            CPPUNIT_ASSERT_MESSAGE("H1(L(100,1)) is not Z_100.",
+                verifyGroup(lens100_1.getHomologyH1(), 0, 100));
+            CPPUNIT_ASSERT_MESSAGE("H1(LST(3,4,7)) is not Z.",
+                verifyGroup(lst3_4_7.getHomologyH1(), 1, 1));
+            CPPUNIT_ASSERT_MESSAGE("H1(figure eight knot complement) is not Z.",
+                verifyGroup(figure8.getHomologyH1(), 1, 1));
+            CPPUNIT_ASSERT_MESSAGE("H1(RP^2 x S^1) is not Z + Z_2.",
+                verifyGroup(rp2xs1.getHomologyH1(), 1, 2));
+            CPPUNIT_ASSERT_MESSAGE("H1(solid Klein bottle) is not Z.",
+                verifyGroup(solidKB.getHomologyH1(), 1, 1));
+            CPPUNIT_ASSERT_MESSAGE("H1(Gieseking manifold) is not Z.",
+                verifyGroup(gieseking.getHomologyH1(), 1, 1));
         }
 };
 
