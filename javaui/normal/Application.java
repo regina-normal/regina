2c382f1b537b4a09c242efc268ccbba87dec7120
Benjamin Burton <bab@debian.org>
2003-06-16 00:49:18 +0000

    Bumping version from 3.1 to 3.2.

--- javaui/normal/Application.java
+++ javaui/normal/Application.java
@@ -52,7 +52,7 @@ public class Application extends Applet implements ActionListener {
     /**
      * Program version.
      */
-    public static final String version = "3.1";
+    public static final String version = "3.2";
 
     /**
      * Program date.
