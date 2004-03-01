cbf6b5216442aceec8cd2de2f3727887152744ef
Benjamin Burton <bab@debian.org>
2004-03-01 05:05:31 +0000

    Minor rewording.

--- kdeui/src/part/packettypes/nfacegluingitems.cpp
+++ kdeui/src/part/packettypes/nfacegluingitems.cpp
@@ -218,7 +218,7 @@ void FaceGluingItem::setContentFromEditor(QWidget* editor) {
                 "form: <i>tet (face)</i>.  An example is <i>5 (032)</i>, "
                 "which represents face 032 of tetrahedron 5.<p>"
                 "For a method of entering face gluings that is slower but "
-                "easier to understand, you can change to pop-up dialog "
+                "easier to understand, you can switch to pop-up dialog "
                 "mode in the triangulation preferences.</qt>"));
             return;
         } else {
