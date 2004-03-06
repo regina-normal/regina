8dbc8fc77290d02cee81f3e14054338d47ee7f0a
Benjamin Burton <bab@debian.org>
2004-03-06 08:09:01 +0000

    Added a please-wait dialog.

---
+++ kdeui/src/part/patiencedialog.cpp
@@ -0,0 +1,64 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  KDE User Interface                                                    *
+ *                                                                        *
+ *  Copyright (c) 1999-2004, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,        *
+ *  MA 02111-1307, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+#include "patiencedialog.h"
+
+#include <kapplication.h>
+#include <kiconloader.h>
+#include <klocale.h>
+#include <qlabel.h>
+#include <qlayout.h>
+
+PatienceDialog* PatienceDialog::warn(const QString& message,
+        KInstance* instance, QWidget* parent) {
+    PatienceDialog* dlg = new PatienceDialog(message, instance, parent);
+    dlg->show();
+
+    KApplication::kApplication()->processEvents();
+
+    return dlg;
+}
+
+PatienceDialog::PatienceDialog(const QString& message, KInstance* instance,
+        QWidget* parent, const char* name) :
+        KDialogBase(Plain, i18n("Please Wait"), 0 /* no buttons */,
+        static_cast<ButtonCode>(0), parent, name, false) {
+    QFrame* page = plainPage();
+    QHBoxLayout* layout = new QHBoxLayout(page, 0, spacingHint());
+
+    QLabel* clock = new QLabel(page);
+    clock->setPixmap(UserIcon("patience", instance));
+    clock->setAlignment(AlignCenter);
+    layout->add(clock);
+
+    QLabel* msg = new QLabel(message, page);
+    layout->add(msg);
+    layout->setStretchFactor(msg, 1);
+}
+
+#include "patiencedialog.moc"
---
+++ kdeui/src/part/patiencedialog.h
@@ -0,0 +1,72 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  KDE User Interface                                                    *
+ *                                                                        *
+ *  Copyright (c) 1999-2004, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,        *
+ *  MA 02111-1307, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+/*! \file patiencedialog.h
+ *  \brief Provides a dialog that warns the user that they might have a
+ *  bit of a wait on their hands.
+ */
+
+#ifndef __PATIENCEDIALOG_H
+#define __PATIENCEDIALOG_H
+
+#include <kdialogbase.h>
+
+class KInstance;
+
+/**
+ * A non-modal dialog warning the user that they might have to wait a
+ * bit for the current operation to finish.
+ */
+class PatienceDialog : public KDialogBase {
+    Q_OBJECT
+
+    public:
+        /**
+         * Display a non-modal dialog that presents the given message to
+         * the user.  The dialog is intended to be shown while some slow
+         * calculation is taking place.  It will have no buttons, and will
+         * contain an appropriate "waiting" icon.
+         *
+         * This routine will take care of updating the display itself, so
+         * it may be called immediately before the calculation begins.
+         * After the calculation has finished, the dialog (which is
+         * returned by this routine) needs to be destroyed.
+         */
+        static PatienceDialog* warn(const QString& message,
+            KInstance* instance, QWidget* parent = 0);
+
+    private:
+        /**
+         * Creates a new non-modal dialog that warns the user with the
+         * given message.
+         */
+        PatienceDialog(const QString& message, KInstance* instance,
+                QWidget* parent, const char* name = 0);
+};
+
+#endif
