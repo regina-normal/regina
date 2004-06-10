e26850078a49f55f2d337b38b354d1ed6cdbb6ad
Benjamin Burton <bab@debian.org>
2004-06-10 15:12:34 +0000

    Version bump.

--- kdeui/src/common/reginaabout.cpp
+++ kdeui/src/common/reginaabout.cpp
@@ -45,7 +45,7 @@ const QString ReginaAbout::regDescription(
 const QString ReginaAbout::regName(I18N_NOOP("Regina"));
 
 const QString ReginaAbout::regReleased(
-    I18N_NOOP("Released ") + QDate(2004, 4, 24).toString(Qt::TextDate));
+    I18N_NOOP("Released ") + QDate(2004, 6, 13).toString(Qt::TextDate));
 
 const QString ReginaAbout::regVersion(PACKAGE_VERSION);
 
