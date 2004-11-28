f30978bbf3601cde4af775059bfcf42fc88b404d
Benjamin Burton <bab@debian.org>
2004-11-28 01:30:31 +0000

    Tests for triangulations that SnapPea should refuse to deal with.

--- testsuite/snappea/nsnappeatriangulation.cpp
+++ testsuite/snappea/nsnappeatriangulation.cpp
@@ -33,12 +33,15 @@
 #include "triangulation/ntriangulation.h"
 #include "testsuite/snappea/testsnappea.h"
 
+using regina::NPerm;
 using regina::NSnapPeaTriangulation;
+using regina::NTetrahedron;
 using regina::NTriangulation;
 
 class NSnapPeaTriangulationTest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(NSnapPeaTriangulationTest);
 
+    CPPUNIT_TEST(incompatible);
     CPPUNIT_TEST(volume);
 
     CPPUNIT_TEST_SUITE_END();
@@ -56,11 +59,49 @@ class NSnapPeaTriangulationTest : public CppUnit::TestFixture {
         // Non-orientable 3-manifolds:
         NTriangulation n1_1, n2_1, n2_1_2, n4_14, n4_9_2, n4_1_2_1;
 
+        /**
+         * Triangulations from "Symmetries, isometries and length
+         * spectra of closed hyperbolic three-manifolds",
+         * Craig D. Hodgson and Jeffrey R. Weeks,
+         * Experiment. Math. 3/4, 1994, pp 261--274.
+         */
+        // TODO
+
+        /**
+         * Triangulations of 3-manifolds whose reported volume should be zero.
+         */
+        // TODO
+        NTriangulation ordinaryVertex;
+            /**< A triangulation with a torus cusp and an internal vertex. */
+            // TODO: Item 15 2
+
+        /**
+         * Triangulations that SnapPea should refuse to deal with.
+         */
+        NTriangulation empty;
+            /**< An empty triangulation. */
+        NTriangulation lst123;
+            /**< A (1,2,3) layered solid torus. */
+        NTriangulation m2_1_m2_1;
+            /**< Two disconnected copies of m2_1. */
+        NTriangulation genusTwoTorusCusp;
+            /**< A orientable triangulation with a genus two torus cusp. */
+        NTriangulation projPlaneCusps;
+            /**< A non-orientable triangulation with two projective plane
+                 cusps. */
+        NTriangulation genusFourNonOrCusp;
+            /**< A triangulation with a genus four non-orientable cusp
+             * (i.e., a non-orientable analogue of the two-holed torus). */
+        // TODO: invalid
+
     public:
         void setUp() {
             // Keep the kernel quiet.  It interferes with the test
             // suite's running progress messages.
             NSnapPeaTriangulation::disableKernelMessages();
+
+            NTetrahedron* t;
+            NTetrahedron* s;
 
             m2_1.insertRehydration("cabbbbaei");
             m2_2.insertRehydration("cabbbbapt");
@@ -75,9 +116,110 @@ class NSnapPeaTriangulationTest : public CppUnit::TestFixture {
             n4_14.insertRehydration("eahdccddakfhq");
             n4_9_2.insertRehydration("ebdbcdddcemre");
             n4_1_2_1.insertRehydration("eahbcdddjxxxj");
+
+            lst123.insertLayeredSolidTorus(1, 2);
+            m2_1_m2_1.insertRehydration("cabbbbaei");
+            m2_1_m2_1.insertRehydration("cabbbbaei");
+
+            t = new NTetrahedron();
+            s = new NTetrahedron();
+            t->joinTo(0, s, NPerm(0,2,3,1));
+            t->joinTo(1, s, NPerm(2,1,3,0));
+            t->joinTo(2, s, NPerm(1,3,2,0));
+            t->joinTo(3, s, NPerm(2,0,1,3));
+            genusTwoTorusCusp.addTetrahedron(t);
+            genusTwoTorusCusp.addTetrahedron(s);
+
+            t = new NTetrahedron();
+            s = new NTetrahedron();
+            t->joinTo(0, t, NPerm(1,0,2,3));
+            t->joinTo(2, s, NPerm(1,2,0,3));
+            t->joinTo(3, s, NPerm(3,2,0,1));
+            s->joinTo(2, s, NPerm(0,2,3,1));
+            projPlaneCusps.addTetrahedron(t);
+            projPlaneCusps.addTetrahedron(s);
+
+            t = new NTetrahedron();
+            s = new NTetrahedron();
+            t->joinTo(0, t, NPerm(1,2,0,3));
+            t->joinTo(2, s, NPerm(1,2,0,3));
+            t->joinTo(3, s, NPerm(0,2,3,1));
+            s->joinTo(2, s, NPerm(0,2,3,1));
+            genusFourNonOrCusp.addTetrahedron(t);
+            genusFourNonOrCusp.addTetrahedron(s);
         }
 
         void tearDown() {
+        }
+
+        void testIncompatible(NTriangulation& tri, const char* message) {
+            NSnapPeaTriangulation s(tri);
+            CPPUNIT_ASSERT_MESSAGE(message, s.isNull());
+        }
+
+        void incompatible() {
+            // As well as verifying that SnapPea cannot work with the
+            // various triangulations, run sanity checks to ensure we
+            // got the gluings right.  Otherwise a typo in the setUp()
+            // code could well go unnoticed.
+
+            testIncompatible(empty, "The empty triangulation "
+                "should not be representable in SnapPea format.");
+
+            CPPUNIT_ASSERT_MESSAGE("LST(1,2,3) "
+                "appears to have been incorrectly constructed.",
+                lst123.isValid() && lst123.isConnected() &&
+                lst123.isOrientable() && (! lst123.isIdeal()) &&
+                lst123.hasBoundaryFaces());
+            testIncompatible(lst123, "LST(1,2,3) "
+                "should not be representable in SnapPea format.");
+
+            CPPUNIT_ASSERT_MESSAGE("The disconnected double M 2_1 "
+                "appears to have been incorrectly constructed.",
+                m2_1_m2_1.isValid() && (! m2_1_m2_1.isConnected()) &&
+                m2_1_m2_1.isOrientable() && m2_1_m2_1.isIdeal() &&
+                m2_1_m2_1.isStandard() && (! m2_1_m2_1.hasBoundaryFaces()));
+            testIncompatible(m2_1_m2_1, "The disconnected double M 2_1 "
+                "should not be representable in SnapPea format.");
+
+            CPPUNIT_ASSERT_MESSAGE(
+                "The triangulation with a genus two torus cusp "
+                "appears to have been incorrectly constructed.",
+                genusTwoTorusCusp.isValid() &&
+                genusTwoTorusCusp.isConnected() &&
+                genusTwoTorusCusp.isOrientable() &&
+                genusTwoTorusCusp.isIdeal() &&
+                (! genusTwoTorusCusp.isStandard()) &&
+                (! genusTwoTorusCusp.hasBoundaryFaces()));
+            testIncompatible(genusTwoTorusCusp,
+                "A triangulation with a genus two torus cusp "
+                "should not be representable in SnapPea format.");
+
+            CPPUNIT_ASSERT_MESSAGE(
+                "The triangulation with two projective plane cusps "
+                "appears to have been incorrectly constructed.",
+                projPlaneCusps.isValid() &&
+                projPlaneCusps.isConnected() &&
+                (! projPlaneCusps.isOrientable()) &&
+                projPlaneCusps.isIdeal() &&
+                (! projPlaneCusps.isStandard()) &&
+                (! projPlaneCusps.hasBoundaryFaces()));
+            testIncompatible(projPlaneCusps,
+                "A triangulation with two projective plane cusps "
+                "should not be representable in SnapPea format.");
+
+            CPPUNIT_ASSERT_MESSAGE(
+                "The triangulation with a genus four non-orientable cusp "
+                "appears to have been incorrectly constructed.",
+                genusFourNonOrCusp.isValid() &&
+                genusFourNonOrCusp.isConnected() &&
+                (! genusFourNonOrCusp.isOrientable()) &&
+                genusFourNonOrCusp.isIdeal() &&
+                (! genusFourNonOrCusp.isStandard()) &&
+                (! genusFourNonOrCusp.hasBoundaryFaces()));
+            testIncompatible(genusFourNonOrCusp,
+                "A triangulation with a genus four non-orientable cusp "
+                "should not be representable in SnapPea format.");
         }
 
         void testVolume(NTriangulation& tri, const char* triName,
