7790a3893be8512a9c1ce631e975d5c762c7d8f3
Benjamin Burton <bab@debian.org>
2005-06-28 07:20:56 +0000

    First round of refinements.

--- utils/tricensus-mpi.cpp
+++ utils/tricensus-mpi.cpp
@@ -28,6 +28,7 @@
 
 #include <cctype>
 #include <fstream>
+#include <iostream>
 #include <memory>
 #include <sstream>
 #include <popt.h>
@@ -52,11 +53,25 @@ int minimal = 0;
 int minimalPrime = 0;
 int minimalPrimeP2 = 0;
 
+// Filenames read from the command line.
 std::string outputStub;
 std::string pairsFile;
 
+// Generic globals.
+long whichPairing = 0;
+
+// Controller-specific globals.
+std::ofstream logger;
 bool controllerError = false;
 
+// Slave-specific globals.
+std::string pairStub;
+
+/**
+ * Generic helper routine.
+ *
+ * Parse command-line arguments for census and filename options.
+ */
 int parseCmdLine(int argc, const char* argv[]) {
     // Set up the command-line arguments.
     int argOr = 0;
@@ -143,20 +158,31 @@ int parseCmdLine(int argc, const char* argv[]) {
     return 0;
 }
 
-bool nextPairingToSlave(std::istream& input, int slave, long whichPairing) {
+/**
+ * Controller helper routine.
+ *
+ * Read the next face pairing from the given input file.
+ * If it exists, send it to the given slave for processing and return true.
+ * Otherwise do nothing and return false.
+ */
+bool ctrlFarmToSlave(std::istream& input, int slave) {
     std::string pairingRep;
-    regina::NFacePairing* pairing;
 
     while (true) {
         std::getline(input, pairingRep);
 
         if (pairingRep.length() > MAX_PAIRING_REP_LEN) {
-            std::cout << "ERROR: Ignoring pairing #" << whichPairing
+            // This will cause a buffer overflow in the slave if we try
+            // to send it.  Ignore it and note the error.
+            whichPairing++;
+            logger << "ERROR: Ignoring pairing #" << whichPairing
                 << " (line too long)" << std::endl;
             controllerError = true;
         } else if (pairingRep.length() > 0) {
-            std::cout << "Farming pairing #" << whichPairing
-                << " to slave #" << slave << std::endl;
+            // We have a real pairing.
+            whichPairing++;
+            logger << "#" << whichPairing << ": --> slave #" << slave
+                << " ..." << std::endl;
             MPI_Send(&whichPairing, 1, MPI_LONG, slave,
                 TAG_REQUEST_DATA, MPI_COMM_WORLD);
             MPI_Send(const_cast<char*>(pairingRep.c_str()),
@@ -174,12 +200,20 @@ bool nextPairingToSlave(std::istream& input, int slave, long whichPairing) {
     return false;
 }
 
-void sendStopToSlave(int slave) {
-    long whichPairing = -1;
-    MPI_Send(&whichPairing, 1, MPI_LONG, slave, TAG_REQUEST_DATA,
-        MPI_COMM_WORLD);
+/**
+ * Controller helper routine.
+ *
+ * Inform the given slave that it has no more pairings to process, and
+ * that it may therefore exit.
+ */
+void ctrlStopSlave(int slave) {
+    long msg = -1;
+    MPI_Send(&msg, 1, MPI_LONG, slave, TAG_REQUEST_DATA, MPI_COMM_WORLD);
 }
 
+/**
+ * Main routine for the controller.
+ */
 int mainController(int nSlaves) {
     // Prepare to read in the face pairings.
     std::ifstream input(pairsFile.c_str());
@@ -189,19 +223,27 @@ int mainController(int nSlaves) {
         return 1;
     }
 
-    // Start farming out tasks to slaves.
+    // Start logging.
+    std::string logFile = outputStub + ".log";
+    logger.open(logFile.c_str());
+    if (! logger) {
+        std::cerr << "Could not open log file " << logFile << " for writing.\n";
+        return 1;
+    }
+
+    // Farm out an initial set of tasks to slaves.
     int nRunningSlaves = 0;
-    int i;
-    long whichPairing = 0;
+    int slave;
     bool noMorePairings = false;
-    for (i = 1; i <= nSlaves; i++) {
+
+    for (slave = 1; slave <= nSlaves; slave++) {
         if (noMorePairings)
-            sendStopToSlave(i);
-        else if (nextPairingToSlave(input, i, ++whichPairing))
+            ctrlStopSlave(slave);
+        else if (ctrlFarmToSlave(input, slave))
             nRunningSlaves++;
         else {
             noMorePairings = true;
-            sendStopToSlave(i);
+            ctrlStopSlave(slave);
         }
     }
 
@@ -210,50 +252,59 @@ int mainController(int nSlaves) {
     long totTri = 0;
     MPI_Status status;
     while (nRunningSlaves > 0) {
+        // Wait for a slave to return its results.
         MPI_Recv(results, 2, MPI_LONG, MPI_ANY_SOURCE, TAG_RESULT,
             MPI_COMM_WORLD, &status);
-        i = status.MPI_SOURCE;
-        std::cout << "#" << results[0] << ": ";
+        slave = status.MPI_SOURCE;
+
+        logger << "#" << results[0] << ": ";
         if (results[1] < 0) {
-            std::cout << "ERROR";
+            logger << "ERROR" << std::endl;
             controllerError = true;
         } else {
-            std::cout << results[1] << " triangulation(s)";
+            logger << results[1] << " triangulation(s)" << std::endl;
             totTri += results[1];
         }
-        std::cout << " (node #" << i << ")" << std::endl;
 
         if (! noMorePairings)
-            if (! nextPairingToSlave(input, i, ++whichPairing))
+            if (! ctrlFarmToSlave(input, slave))
                 noMorePairings = true;
 
         if (noMorePairings) {
-            sendStopToSlave(i);
+            ctrlStopSlave(slave);
             nRunningSlaves--;
         }
     }
-    whichPairing--;
 
-    // All done.
+    // All done!
     input.close();
-    std::cout << "Done:" << std::endl;
+    logger << "Done:" << std::endl;
     if (controllerError)
-        std::cout << "  - ONE OR MORE ERRORS OCCURRED!" << std::endl;
-    std::cout << "  - " << whichPairing << " pairing(s) read" << std::endl;
-    std::cout << "  - " << totTri << " triangulation(s) found" << std::endl;
+        logger << "  - ONE OR MORE ERRORS OCCURRED!" << std::endl;
+    logger << "  - " << whichPairing << " pairing(s) read" << std::endl;
+    logger << "  - " << totTri << " triangulation(s) found" << std::endl;
+    logger.close();
     return 0;
 }
 
 /**
- * Return a new text packet storing the census parameters.
+ * Slave helper routine.
+ *
+ * Construct a skeleton packet tree for the census data file.
+ * The census container will be the last child of the parent.
  */
-regina::NText* parameterPacket(const regina::NFacePairing* pairing) {
+regina::NPacket* slaveSkeletonTree(const regina::NFacePairing* pairing) {
+    // Create the overall parent packet.
+    regina::NContainer* parent = new regina::NContainer();
+    parent->setPacketLabel("Partial MPI census");
+
+    // Create a child packet that describes the census parameters.
     regina::NText* desc = new regina::NText();
     desc->setPacketLabel("Parameters");
     std::ostringstream descStream;
 
     descStream << "Only used a single face pairing:\n"
-        << pairing->toString() << '\n';
+        << pairing->toString() << "\n\n";
 
     if (finiteness == regina::NBoolSet::sTrue)
         descStream << "Finite only\n";
@@ -279,42 +330,80 @@ regina::NText* parameterPacket(const regina::NFacePairing* pairing) {
         descStream << "Ignored obviously non-minimal triangulations\n";
 
     desc->setText(descStream.str());
-    return desc;
+    parent->insertChildLast(desc);
+
+    // Create a child packet that will hold the census triangulations.
+    regina::NContainer* census = new regina::NContainer();
+    census->setPacketLabel("Triangulations");
+    parent->insertChildLast(census);
+
+    // All done!
+    return parent;
 }
 
-void sendResult(long whichPairing, long result) {
-    long data[2] = { whichPairing, result };
+/**
+ * Slave helper routine.
+ *
+ * Inform the controller that the partial census for this face pairing
+ * was successfully completed.
+ */
+void slaveSendResult(long nTriangulations) {
+    long data[2] = { whichPairing, nTriangulations };
     MPI_Send(data, 2, MPI_LONG, 0, TAG_RESULT, MPI_COMM_WORLD);
 }
 
-void bail(long whichPairing, const std::string& errFile,
-        const std::string& error) {
+/**
+ * Slave helper routine.
+ *
+ * Signal that a fatal error occurred whilst trying to process this
+ * particular face pairing.
+ *
+ * A message is written to a newly created error file (specific to that
+ * face pairing), and the controller is informed also.
+ */
+void slaveBail(const std::string& error) {
+    // Write the error to file.
+    std::string errFile = pairStub + ".err";
     std::ofstream out(errFile.c_str());
     if (out)
         out << "ERROR (pairing #" << whichPairing << "):\n"
             << error << std::endl;
-    sendResult(whichPairing, -1);
+
+    // Inform the controller (by returning a negative number of
+    // triangulations).
+    slaveSendResult(-1);
 }
 
+/**
+ * Main routine for a slave (ranks 1..size).
+ */
 int mainSlave() {
-    long whichPairing;
-    char pairingRep[MAX_PAIRING_REP_LEN + 1];
+    // MPI message passing:
     MPI_Status status;
+    char pairingRep[MAX_PAIRING_REP_LEN + 1];
+
+    // Helper variables for the census:
     int whichPurge;
     long ans;
 
-    // TODO: time file
-    std::string pairStub, outFile, timeFile, errFile;
+    // Files that we will want to write to:
+    std::string outFile, timeFile;
 
+    // Keep fetching and processing pairings until there are no more.
     while (true) {
+        // Get the index number of the new pairing.  A negative index
+        // number signifies that there are no more.
         MPI_Recv(&whichPairing, 1, MPI_LONG, 0, TAG_REQUEST_DATA,
             MPI_COMM_WORLD, &status);
         if (whichPairing < 0)
             break;
 
+        // Fetch the pairing itself.
         MPI_Recv(pairingRep, MAX_PAIRING_REP_LEN + 1, MPI_CHAR, 0,
             TAG_REQUEST_PAIRING, MPI_COMM_WORLD, &status);
 
+        // All filenames will include the pairing index number, so that
+        // people don't clobber each other's files.
         {
             std::ostringstream s;
             s << outputStub << '_' << whichPairing;
@@ -323,35 +412,23 @@ int mainSlave() {
 
         outFile = pairStub + ".rga";
         timeFile = pairStub + ".time";
-        errFile = pairStub + ".err";
 
         // Parse the face pairing.
         regina::NFacePairing* pairing =
             regina::NFacePairing::fromTextRep(pairingRep);
         if (! pairing) {
-            bail(whichPairing, errFile,
-                std::string("Invalid face pairing: ") + pairingRep);
+            slaveBail(std::string("Invalid face pairing: ") + pairingRep);
             continue;
         }
         if (! pairing->isCanonical()) {
-            bail(whichPairing, errFile,
-                std::string("Non-canonical face pairing: ") + pairingRep);
+            slaveBail(std::string("Non-canonical face pairing: ") + pairingRep);
             continue;
         }
 
-        // Prepare the packet tree.
-        regina::NContainer parent;
-        parent.setPacketLabel("Partial MPI census");
-
-        regina::NText* desc = parameterPacket(pairing);
-
-        regina::NContainer* census = new regina::NContainer();
-        census->setPacketLabel("Triangulations");
-
-        parent.insertChildLast(desc);
-        parent.insertChildLast(census);
-
         // Run the partial census.
+        // TODO: Time file.
+        regina::NPacket* parent = slaveSkeletonTree(pairing);
+
         if (minimalPrimeP2)
             whichPurge = regina::NCensus::PURGE_NON_MINIMAL_PRIME |
                 regina::NCensus::PURGE_P2_REDUCIBLE;
@@ -362,37 +439,54 @@ int mainSlave() {
         else
             whichPurge = 0;
 
-        ans = regina::NCensus::formPartialCensus(pairing, census,
+        ans = regina::NCensus::formPartialCensus(pairing,
+            parent->getLastTreeChild(),
             finiteness, orientability, whichPurge,
             ((minimal || minimalPrime || minimalPrimeP2) ?
             regina::NCensus::mightBeMinimal : 0), 0);
 
         // Write the completed census to file.
-        if (regina::writeXMLFile(outFile.c_str(), &parent))
-            sendResult(whichPairing, ans);
+        if (regina::writeXMLFile(outFile.c_str(), parent))
+            slaveSendResult(ans);
         else
-            bail(whichPairing, errFile, "Output file cound not be written.");
+            slaveBail("Output file could not be written.");
+
+        delete parent;
     }
 
     return 0;
 }
 
+/**
+ * Main routine for all processors.
+ *
+ * Parse the command-line arguments for census options, then determine
+ * whether we are controller or slave and run a specialised main routine
+ * accordingly.
+ */
 int main(int argc, char* argv[]) {
-    int rank, size;
-
     MPI_Init(&argc, &argv);
 
     int retVal = parseCmdLine(argc, (const char**)argv);
     if (retVal == 0) {
+        // Which processor are we?
+        int rank;
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
         if (rank == 0) {
+            // We're the controller.
+            // How many processors in total?
+            int size;
             MPI_Comm_size(MPI_COMM_WORLD, &size);
+
             if (size <= 1) {
-                std::cerr << "ERROR: At least two processors are required.\n";
+                std::cerr << "ERROR: At least two processors are required "
+                    "(one controller and one slave).\n";
                 retVal = 1;
             } else
                 retVal = mainController(size - 1);
         } else {
+            // We're one of many slaves.
             retVal = mainSlave();
         }
     }
