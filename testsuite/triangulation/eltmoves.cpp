052f72c84c9ecc95404df6966011e1a724a4bbe1
Benjamin Burton <bab@debian.org>
2005-07-02 00:35:21 +0000

    Full set of tests for 2-0 edge moves.

--- testsuite/triangulation/eltmoves.cpp
+++ testsuite/triangulation/eltmoves.cpp
@@ -56,6 +56,12 @@ class ElementaryMovesTest : public CppUnit::TestFixture {
         // identity permutation.
         NTriangulation base;
 
+        // As for base, but with two diagonally opposite faces joined
+        // also to form a solid Klein bottle.
+        // This acts as a two-sided Mobius strip that can be flattened,
+        // with faces 0/012 <--> 1/301.
+        NTriangulation baseKB;
+
     public:
         void setUp() {
             NTetrahedron* r = new NTetrahedron();
@@ -64,6 +70,12 @@ class ElementaryMovesTest : public CppUnit::TestFixture {
             r->joinTo(1, s, NPerm());
             base.addTetrahedron(r);
             base.addTetrahedron(s);
+
+            baseKB.insertTriangulation(base);
+            baseKB.getTetrahedron(0)->joinTo(2,
+                baseKB.getTetrahedron(1), NPerm(1, 2, 3, 0));
+
+            baseKB.gluingsHaveChanged();
         }
 
         void tearDown() {
@@ -150,7 +162,8 @@ class ElementaryMovesTest : public CppUnit::TestFixture {
                 // Force a recalculation of the skeleton, since
                 // NTetrahedron routines such as getEdge() are oblivious
                 // to the encompassing triangulation.  There has got to
-                // be a better way.
+                // be a better way (TODO).  We do this several times
+                // here in this file.
                 t.getNumberOfEdges();
 
                 NEdge* e = tet->getEdge(regina::edgeNumber[0][1]);
@@ -163,6 +176,142 @@ class ElementaryMovesTest : public CppUnit::TestFixture {
                 ball.addTetrahedron(new NTetrahedron());
 
                 verify20Edge(&t, 3, &ball, "boundary-loop-tet");
+            }
+
+            {
+                // Two boundary faces, the others attached to the top of
+                // an LST.
+                NTriangulation orig;
+                orig.insertLayeredSolidTorus(4, 7);
+                NTetrahedron* top = new NTetrahedron();
+                orig.getTetrahedron(0)->joinTo(2, top, NPerm(2, 3, 0, 1));
+                orig.getTetrahedron(0)->joinTo(3, top, NPerm(2, 3, 0, 1));
+                orig.addTetrahedron(top);
+
+                NTriangulation lst;
+                lst.insertLayeredSolidTorus(3, 4);
+
+                verify20Edge(&orig, 0, &lst, "boundary-layer");
+            }
+
+            {
+                // Wedged in between two adjacent internal faces in an
+                // LST(3,4,7).
+                const int adj[5][4] = {
+                    { 1, 1, -1, -1},
+                    { 4, 4, 0, 0},
+                    { 2, 2, 3, 3},
+                    { 4, 4, 2, 2},
+                    { 3, 3, 1, 1}
+                };
+
+                const int glu[5][4][4] = {
+                    { { 2,1,3,0 }, { 0,3,1,2 }, { 0,0,0,0 }, { 0,0,0,0 } },
+                    { { 3,1,2,0 }, { 0,2,1,3 }, { 3,1,0,2 }, { 0,2,3,1 } },
+                    { { 1,2,3,0 }, { 3,0,1,2 }, { 0,1,2,3 }, { 0,1,2,3 } },
+                    { { 0,1,2,3 }, { 0,1,2,3 }, { 0,1,2,3 }, { 0,1,2,3 } },
+                    { { 0,1,2,3 }, { 0,1,2,3 }, { 0,2,1,3 }, { 3,1,2,0 } }
+                };
+
+                NTriangulation orig;
+                orig.insertConstruction(5, adj, glu);
+                NTriangulation lst;
+                lst.insertLayeredSolidTorus(3, 4);
+
+                verify20Edge(&orig, 5, &lst, "internal-flat");
+            }
+
+            {
+                // Wedged into the tip of a layered L(10,3), with two
+                // faces joined in a loop.
+                const int adj[5][4] = {
+                    { 1, 1, 3, 3},
+                    { 2, 2, 0, 0},
+                    { 2, 2, 1, 1},
+                    { 4, 4, 0, 0},
+                    { 3, 3, 4, 4}
+                };
+
+                const int glu[5][4][4] = {
+                    { { 2,1,3,0 }, { 0,3,1,2 }, { 0,1,2,3 }, { 0,1,2,3 } },
+                    { { 3,1,2,0 }, { 0,2,1,3 }, { 3,1,0,2 }, { 0,2,3,1 } },
+                    { { 1,2,3,0 }, { 3,0,1,2 }, { 0,2,1,3 }, { 3,1,2,0 } },
+                    { { 0,1,2,3 }, { 0,1,2,3 }, { 0,1,2,3 }, { 0,1,2,3 } },
+                    { { 0,1,2,3 }, { 0,1,2,3 }, { 1,2,3,0 }, { 3,0,1,2 } }
+                };
+
+                NTriangulation orig;
+                orig.insertConstruction(5, adj, glu);
+                NTriangulation lens;
+                lens.insertLayeredLensSpace(10, 3);
+                NTriangulation copy(orig);
+
+                verify20Edge(&orig, 5, &lens, "internal-loop-twist");
+                verify20Edge(&copy, 0, &lens, "internal-flat-lens");
+            }
+
+            {
+                // A hand-constructed example formed by squeezing baseKB
+                // into a two-sided Mobius band face in K(iii | 1,0 | 0,1)
+                // from the 6-tetrahedron non-orientable census.
+                const int adjOrig[8][4] = {
+                    { 1, 1, 2, 2},
+                    { 0, 0, 6, 3},
+                    { 0, 0, 7, 4},
+                    { 1, 4, 5, 5},
+                    { 2, 3, 5, 5},
+                    { 3, 3, 4, 4},
+                    { 7, 7, 7, 1},
+                    { 6, 6, 2, 6}
+                };
+
+                const int gluOrig[8][4][4] = {
+                    { { 0,2,1,3 }, { 2,1,3,0 }, { 1,3,0,2 }, { 3,0,2,1 } },
+                    { { 0,2,1,3 }, { 3,1,0,2 }, { 2,0,3,1 }, { 2,3,1,0 } },
+                    { { 2,0,3,1 }, { 1,3,2,0 }, { 1,3,2,0 }, { 2,3,1,0 } },
+                    { { 3,2,0,1 }, { 0,1,2,3 }, { 3,1,0,2 }, { 2,0,3,1 } },
+                    { { 3,2,0,1 }, { 0,1,2,3 }, { 1,3,2,0 }, { 0,2,1,3 } },
+                    { { 2,1,3,0 }, { 1,3,0,2 }, { 3,0,2,1 }, { 0,2,1,3 } },
+                    { { 0,1,2,3 }, { 0,1,2,3 }, { 1,2,3,0 }, { 1,3,0,2 } },
+                    { { 0,1,2,3 }, { 0,1,2,3 }, { 3,0,2,1 }, { 3,0,1,2 } }
+                };
+
+                NTriangulation orig;
+                orig.insertConstruction(8, adjOrig, gluOrig);
+
+                const int adjResult[6][4] = {
+                    { 1, 1, 2, 2},
+                    { 0, 0, 2, 3},
+                    { 0, 0, 1, 4},
+                    { 1, 4, 5, 5},
+                    { 2, 3, 5, 5},
+                    { 3, 3, 4, 4}
+                };
+
+                const int gluResult[6][4][4] = {
+                    { { 0,2,1,3 }, { 2,1,3,0 }, { 1,3,0,2 }, { 3,0,2,1 } },
+                    { { 0,2,1,3 }, { 3,1,0,2 }, { 0,1,2,3 }, { 2,3,1,0 } },
+                    { { 2,0,3,1 }, { 1,3,2,0 }, { 0,1,2,3 }, { 2,3,1,0 } },
+                    { { 3,2,0,1 }, { 0,1,2,3 }, { 3,1,0,2 }, { 2,0,3,1 } },
+                    { { 3,2,0,1 }, { 0,1,2,3 }, { 1,3,2,0 }, { 0,2,1,3 } },
+                    { { 2,1,3,0 }, { 1,3,0,2 }, { 3,0,2,1 }, { 0,2,1,3 } }
+                };
+
+                NTriangulation result;
+                result.insertConstruction(6, adjResult, gluResult);
+
+                verify20Edge(&orig, 8, &result, "internal-cross");
+            }
+
+            {
+                // Two copies of baseKB glued along a single face.
+                NTriangulation t;
+                t.insertTriangulation(baseKB);
+                t.insertTriangulation(baseKB);
+                t.getTetrahedron(0)->joinTo(3, t.getTetrahedron(2), NPerm());
+                t.gluingsHaveChanged();
+
+                verify20Edge(&t, 3, &baseKB, "bdry-cross-tet");
             }
         }
 
@@ -226,6 +375,9 @@ class ElementaryMovesTest : public CppUnit::TestFixture {
                 t.addTetrahedron(r);
                 t.addTetrahedron(s);
 
+                // Ugly hack to regenerate skeleton, see notes above.
+                t.getNumberOfEdges();
+
                 NEdge* e = t.getTetrahedron(0)->getEdge(
                     regina::edgeNumber[0][1]);
                 CPPUNIT_ASSERT_MESSAGE(
@@ -256,6 +408,9 @@ class ElementaryMovesTest : public CppUnit::TestFixture {
                 t.addTetrahedron(r);
                 t.addTetrahedron(s);
 
+                // Ugly hack to regenerate skeleton, see notes above.
+                t.getNumberOfEdges();
+
                 NEdge* e = t.getTetrahedron(0)->getEdge(
                     regina::edgeNumber[0][1]);
                 CPPUNIT_ASSERT_MESSAGE(
@@ -274,10 +429,7 @@ class ElementaryMovesTest : public CppUnit::TestFixture {
                 tet->joinTo(2, tet, NPerm(2, 3));
                 t.gluingsHaveChanged();
 
-                // Force a recalculation of the skeleton, since
-                // NTetrahedron routines such as getEdge() are oblivious
-                // to the encompassing triangulation.  There has got to
-                // be a better way (TODO).
+                // Ugly hack to regenerate skeleton, see notes above.
                 t.getNumberOfEdges();
 
                 NEdge* e = tet->getEdge(regina::edgeNumber[0][1]);
@@ -287,6 +439,63 @@ class ElementaryMovesTest : public CppUnit::TestFixture {
                         t.isOrientable());
 
                 verify20EdgeInvalid(&t, "boundary-loop-boundary");
+            }
+
+            {
+                // Two faces boundary, the other joined in a cross.
+                NTriangulation t;
+                t.insertTriangulation(baseKB);
+
+                // Ugly hack to regenerate skeleton, see notes above.
+                t.getNumberOfEdges();
+
+                NEdge* e = t.getTetrahedron(0)->
+                    getEdge(regina::edgeNumber[0][1]);
+                CPPUNIT_ASSERT_MESSAGE(
+                    "Case boundary-cross-boundary is malformed.",
+                    e->isBoundary() && e->getDegree() == 3 &&
+                        ! t.isOrientable());
+
+                verify20EdgeInvalid(&t, "boundary-cross-boundary");
+            }
+
+            {
+                // Two opposite faces boundary, the other two wedged
+                // inside an LST(3,4,7).
+                const int adj[5][4] = {
+                    { 1, 1, -1, -1},
+                    { 4, 2, 0, 0},
+                    { 2, 2, 1, 3},
+                    { 4, 4, -1, 2},
+                    { 3, 3, -1, 1}
+                };
+
+                const int glu[5][4][4] = {
+                    { { 2,1,3,0 }, { 0,3,1,2 }, { 0,0,0,0 }, { 0,0,0,0 } },
+                    { { 3,1,2,0 }, { 0,2,1,3 }, { 3,1,0,2 }, { 0,2,3,1 } },
+                    { { 1,2,3,0 }, { 3,0,1,2 }, { 0,2,1,3 }, { 0,1,2,3 } },
+                    { { 0,1,2,3 }, { 0,1,2,3 }, { 0,0,0,0 }, { 0,1,2,3 } },
+                    { { 0,1,2,3 }, { 0,1,2,3 }, { 0,0,0,0 }, { 3,1,2,0 } }
+                };
+
+                NTriangulation orig;
+                orig.insertConstruction(5, adj, glu);
+                verify20EdgeInvalid(&orig, "boundary-opposite-wedge");
+            }
+
+            {
+                // Two diagonally opposite faces boundary, the other two
+                // glued to an LST boundary.  Doesn't really matter how.
+                NTriangulation orig;
+                orig.insertLayeredSolidTorus(3,4);
+                orig.insertTriangulation(base);
+
+                NTetrahedron* top = orig.getTetrahedron(0);
+                orig.getTetrahedron(3)->joinTo(3, top, NPerm(1,2,0,3));
+                orig.getTetrahedron(4)->joinTo(2, top, NPerm(0,3,2,1));
+                orig.gluingsHaveChanged();
+
+                verify20EdgeInvalid(&orig, "boundary-diag");
             }
         }
 };
