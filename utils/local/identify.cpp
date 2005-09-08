5002e87308847de25f8666d02b0c52358b62a54d
Benjamin Burton <bab@debian.org>
2005-09-08 13:42:47 +0000

    New local utility (identify).

---
+++ utils/local/identify.cpp
@@ -0,0 +1,155 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Combine a set of data files as a first draft for a census             *
+ *                                                                        *
+ *  Copyright (c) 2005, Ben Burton                                        *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,        *
+ *  MA 02111-1307, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+/**
+ * The given topology data file is read.
+ *
+ * Each triangulation is examined to see it is recognised, i.e., if it
+ * has a combinatorial structure that Regina is familiar with.  If so,
+ * the name of the combinatorial triangulation is output (plus the name of
+ * the underlying 3-manifold if possible).  If not, the word UNKNOWN is
+ * output instead.
+ *
+ * If the option -c is passed, the labels of all container packets are
+ * also output as they appear in the packet tree.  This may be useful for
+ * dividing the output into sections.
+ *
+ * All output is written to standard output.
+ */
+
+#include <file/nxmlfile.h>
+#include <manifold/nmanifold.h>
+#include <packet/ncontainer.h>
+#include <subcomplex/nstandardtri.h>
+#include <triangulation/ntriangulation.h>
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace regina;
+
+unsigned totTris = 0;
+unsigned trisOk = 0;
+unsigned mfdsOk = 0;
+
+bool outputContainers = false;
+NPacket* tree;
+
+void usage(const char* progName, const std::string& error = std::string()) {
+    if (! error.empty())
+        std::cerr << error << "\n\n";
+
+    std::cerr << "Usage:\n";
+    std::cerr << "    " << progName << " [ -c ] <file.rga>\n";
+    std::cerr << std::endl;
+    std::cerr << "    -c : Output container packet labels also\n";
+    std::cerr << std::endl;
+    std::cerr << "Resulting data is written to standard output.\n";
+    std::cerr << "Statistics and diagnostic messages are written to standard error.\n";
+    exit(1);
+}
+
+void process(NTriangulation* t) {
+    std::cout << t->getPacketLabel() << "  -->  ";
+    totTris++;
+
+    NStandardTriangulation* s =
+        NStandardTriangulation::isStandardTriangulation(t);
+    if (s) {
+        std::cout << s->getName();
+        trisOk++;
+
+        NManifold* m = s->getManifold();
+        if (m) {
+            std::cout << "  ==  " << m->getName();
+            mfdsOk++;
+
+            delete m;
+        }
+
+        delete s;
+    } else
+        std::cout << "UNKNOWN";
+
+    std::cout << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    // Command-line parsing.
+    char optChar;
+    int i;
+    for (i = 1; i < argc && *argv[i] == '-'; i++) {
+        if (! argv[i][1])
+            usage(argv[0], std::string("Invalid option: ") + argv[i]);
+        if (argv[i][2])
+            usage(argv[0], std::string("Invalid option: ") + argv[i]);
+
+        // The argument has length precisely 2.
+        optChar = argv[i][1];
+
+        if (optChar == '-') {
+            i++;
+            break;
+        } else if (optChar == 'c')
+            outputContainers = true;
+        else
+            usage(argv[0], std::string("Invalid option: ") + argv[i]);
+    }
+
+    // argv[i] is the first filename.
+    if (i != argc - 1)
+        usage(argv[0], "Precisely one data file must be given.");
+
+    // Read the data file.
+    if (! (tree = readXMLFile(argv[i]))) {
+        std::cerr << "ERROR: Could not read data from " << argv[i] << '.'
+            << std::endl;
+        return 1;
+    }
+
+    // Process the packets.
+    for (NPacket* p = tree; p; p = p->nextTreePacket())
+        if (p->getPacketType() == NTriangulation::packetType)
+            process(static_cast<NTriangulation*>(p));
+        else if (outputContainers &&
+                p->getPacketType() == NContainer::packetType)
+            std::cout << "----- " << p->getPacketLabel() << " -----"
+                << std::endl;
+
+    // Write statistics and clean up.
+    delete tree;
+
+    std::cerr << std::endl;
+    std::cerr << "Final statistics:" << std::endl;
+    std::cerr << "    Triangulations read:       " << totTris << std::endl;
+    std::cerr << "    Triangulations recognised: " << trisOk << std::endl;
+    std::cerr << "    3-manifolds recognised:    " << mfdsOk << std::endl;
+
+    return 0;
+}
+
