04f847185e203e6d9c45eaae2aebf8c98c1622c2
Benjamin Burton <bab@debian.org>
2005-09-24 01:36:28 +0000

    Update python bindings for the new SFS classes.

--- python/manifold/nsfs.cpp
+++ python/manifold/nsfs.cpp
@@ -32,42 +32,63 @@
 #include <boost/python.hpp>
 
 using namespace boost::python;
-using regina::NExceptionalFibre;
-using regina::NSFS;
+using regina::NSFSFibre;
+using regina::NSFSpace;
 
 namespace {
-    void (NSFS::*insertFibre_fibre)(const NExceptionalFibre&) =
-        &NSFS::insertFibre;
-    void (NSFS::*insertFibre_longs)(long, long) = &NSFS::insertFibre;
+    void (NSFSpace::*insertFibre_fibre)(const NSFSFibre&) =
+        &NSFSpace::insertFibre;
+    void (NSFSpace::*insertFibre_longs)(long, long) = &NSFSpace::insertFibre;
+
+    BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(OL_addHandle, addHandle, 0, 1);
+    BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(OL_addCrosscap, addCrosscap, 0, 1);
 }
 
-void addNSFS() {
-    class_<NExceptionalFibre>("NExceptionalFibre")
+void addNSFSpace() {
+    class_<NSFSFibre>("NSFSFibre")
         .def(init<long, long>())
-        .def(init<const NExceptionalFibre&>())
-        .def_readwrite("alpha", &NExceptionalFibre::alpha)
-        .def_readwrite("beta", &NExceptionalFibre::beta)
+        .def(init<const NSFSFibre&>())
+        .def_readwrite("alpha", &NSFSFibre::alpha)
+        .def_readwrite("beta", &NSFSFibre::beta)
         .def(self == self)
         .def(self < self)
         .def(self_ns::str(self))
     ;
 
-    class_<NSFS, bases<regina::NManifold>, std::auto_ptr<NSFS> >("NSFS")
-        .def(init<unsigned long, bool, optional<unsigned long> >())
-        .def(init<const NSFS&>())
-        .def("getOrbitGenus", &NSFS::getOrbitGenus)
-        .def("isOrbitOrientable", &NSFS::isOrbitOrientable)
-        .def("getOrbitPunctures", &NSFS::getOrbitPunctures)
-        .def("getFibreCount", &NSFS::getFibreCount)
-        .def("getFibre", &NSFS::getFibre)
+    scope s = class_<NSFSpace, bases<regina::NManifold>,
+            std::auto_ptr<NSFSpace> >("NSFSpace")
+        .def(init<NSFSpace::classType, unsigned long,
+            optional<unsigned long, unsigned long> >())
+        .def(init<const NSFSpace&>())
+        .def("getBaseClass", &NSFSpace::getBaseClass)
+        .def("getBaseGenus", &NSFSpace::getBaseGenus)
+        .def("isBaseOrientable", &NSFSpace::isBaseOrientable)
+        .def("getBasePunctures", &NSFSpace::getBasePunctures)
+        .def("getBaseReflectors", &NSFSpace::getBaseReflectors)
+        .def("getFibreCount", &NSFSpace::getFibreCount)
+        .def("getFibre", &NSFSpace::getFibre)
+        .def("getObstruction", &NSFSpace::getObstruction)
+        .def("addHandle", &NSFSpace::addHandle, OL_addHandle())
+        .def("addCrosscap", &NSFSpace::addHandle, OL_addCrosscap())
+        .def("addPuncture", &NSFSpace::addPuncture)
+        .def("addReflector", &NSFSpace::addReflector)
         .def("insertFibre", insertFibre_fibre)
         .def("insertFibre", insertFibre_longs)
-        .def("reduce", &NSFS::reduce)
-        .def("isLensSpace", &NSFS::isLensSpace,
+        .def("reduce", &NSFSpace::reduce)
+        .def("isLensSpace", &NSFSpace::isLensSpace,
             return_value_policy<manage_new_object>())
     ;
 
-    implicitly_convertible<std::auto_ptr<NSFS>,
+    enum_<NSFSpace::classType>("classType")
+        .value("o1", NSFSpace::o1)
+        .value("o2", NSFSpace::o2)
+        .value("n1", NSFSpace::n1)
+        .value("n2", NSFSpace::n2)
+        .value("n3", NSFSpace::n3)
+        .value("n4", NSFSpace::n4)
+        ;
+
+    implicitly_convertible<std::auto_ptr<NSFSpace>,
         std::auto_ptr<regina::NManifold> >();
 }
 
