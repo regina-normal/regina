25fe888681a9f1f41f50cd2d45bedd2585957498
Benjamin Burton <bab@debian.org>
2005-09-29 01:11:07 +0000

    Beginning the mass rework of plugged SFS triangulations.

---
+++ engine/engine/subcomplex/npluggedsfs.cpp
@@ -0,0 +1,154 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2005, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+#include "manifold/nsfs.h"
+#include "subcomplex/npluggedsfs.h"
+#include "triangulation/nisomorphism.h"
+
+namespace regina {
+
+bool NSFSAnnulus::meetsBoundary() const {
+    if (! tet[0]->getAdjacentTetrahedron(roles[0][3]))
+        return true;
+    if (! tet[1]->getAdjacentTetrahedron(roles[1][3]))
+        return true;
+    return false;
+}
+
+void NSFSAnnulus::switchSides() {
+    unsigned which, face;
+    for (which = 0; which < 2; which++) {
+        face = roles[which][3];
+        roles[which] = tet[which]->getAdjacentTetrahedronGluing(face) *
+            roles[which];
+        tet[which] = tet[which]->getAdjacentTetrahedron(face);
+    }
+}
+
+void NSFSAnnulus::transform(const NTriangulation* originalTri,
+        const NIsomorphism* iso, const NTriangulation* newTri) {
+    unsigned which;
+    unsigned long tetID;
+    for (which = 0; which < 2; which++) {
+        tetID = originalTri->getTetrahedronIndex(tet[which]);
+        tet[which] = newTri->getTetrahedron(iso->tetImage(tetID));
+        roles[which] = iso->facePerm(tetID) * roles[which];
+    }
+}
+
+NSFSSocketHolder::NSFSSocketHolder(const NSFSSocketHolder& cloneMe) :
+        nSockets_(cloneMe.nSockets_),
+        socket_(new NAnnulusBoundary[cloneMe.nSockets_]),
+        socketOrient_(new bool[cloneMe.nSockets_]),
+        plug_(new NSFSPlug*[cloneMe.nSockets_]) {
+    for (unsigned i = 0; i < nSockets_; i++) {
+        socket_[i] = cloneMe.socket_[i];
+        socketOrient_[i] = cloneMe.socketOrient_[i];
+        plug_[i] = cloneMe.plug_[i];
+    }
+}
+
+NSFSSocketHolder::NSFSSocketHolder(const NSFSSocketHolder& preImage,
+        const NTriangulation* preImageTri, const NIsomorphism* iso,
+        const NTriangulation* useTri) :
+        nSockets_(preImage.nSockets_),
+        socket_(new NAnnulusBoundary[preImage.nSockets_]),
+        socketOrient_(new bool[preImage.nSockets_]),
+        plug_(new NSFSPlug*[preImage.nSockets_]) {
+    unsigned s, f;
+    for (unsigned s = 0; s < nSockets_; s++) {
+        socket_[s] = preImage.socket_[s].image(preImageTri, iso, useTri);
+        socketOrient_[s] = preImage.socketOrient_[s];
+        plug_[s] = 0;
+    }
+}
+
+NSFSTree::~NSFSTree() {
+    delete rootIso;
+
+    for (unsigned i = 0; i < nSockets_; i++)
+        delete plug_[i];
+}
+
+NSFSTree* NSFSTree::hunt(NTriangulation* tri, const NSFSRoot& root) {
+    std::list<NIsomorphism*> isos;
+    if (! root.root().findAllSubcomplexesIn(*tri, isos))
+        return 0;
+
+    // Run through each isomorphism and look for the corresponding plugs.
+    for (std::list<NIsomorphism*>::const_iterator it = isos.begin();
+            it != isos.end(); it++) {
+        NSFSSocketHolder sockets(root, root.root(), *it, tri);
+        if (! sockets.isFullyPlugged()) {
+            // Delete this isomorphism; we won't need it any more.
+            delete *it;
+            continue;
+        }
+
+        // All good!
+
+        // Before we return, delete the remaining isomorphisms that we
+        // never even looked at.
+        for (it++; it != isos.end(); it++)
+            delete *it;
+
+        return new NSFSTree(root, *it, sockets);
+    }
+
+    // Nothing found.
+    return 0;
+}
+
+NManifold* NSFSTree::getManifold() const {
+    NSFSpace* ans = root_.createSFS();
+
+    for (unsigned i = 0; i < nSockets_; i++)
+        plug_[i]->adjustSFS(*ans, ! socketOrient_[s]);
+
+    ans->reduce();
+    return ans;
+}
+
+std::ostream& NSFSTree::writeCommonName(std::ostream& out, bool tex) const {
+    out << (tex ? "F_{" : "F(");
+    root_.writeCommonName(out, tex);
+
+    // TODO: Normalise parameters?
+    for (unsigned i = 0; i < nSockets_; i++) {
+        out << " | ";
+        if (tex)
+            plug_[i]->writeTeXName(out);
+        else
+            plug_[i]->writeName(out);
+    }
+
+    return out << (tex ? '}' : ')');
+}
+
+} // namespace regina
+
---
+++ engine/engine/subcomplex/npluggedsfs.h
@@ -0,0 +1,368 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2005, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+/*! \file npluggedsfs.h
+ *  \brief Deals with triangulations of Seifert fibred spaces formed
+ *  from connections between plugs and sockets.
+ */
+
+#ifndef __NPLUGGEDSFS_H
+#ifndef __DOXYGEN
+#define __NPLUGGEDSFS_H
+#endif
+
+#include <memory>
+#include "subcomplex/nstandardtri.h"
+#include "triangulation/ntriangulation.h"
+
+namespace regina {
+
+/**
+ * \weakgroup subcomplex
+ * @{
+ */
+
+/**
+ * Represents a two-sided annulus formed from a pair of faces in a
+ * Seifert fibred space.  This annulus is saturated, i.e., a union of
+ * fibres.  More than that, the fibres run parallel to the two boundary
+ * edges of the annulus.
+ *
+ * The annulus is described from one side only.  The description
+ * includes the two tetrahedra that provide the faces of the annulus, as
+ * well as an additional permutation \a roles describing what roles the
+ * individual tetrahedron vertices play.  More specifically, the annulus
+ * can be drawn as follows, with the upper edge identified with the lower:
+ *
+ * <pre>
+ *            *--->---*
+ *            |0  2 / |
+ *     First  |    / 1|  Second
+ *     face   |   /   |   face
+ *            |1 /    |
+ *            | / 2  0|
+ *            *--->---*
+ * </pre>
+ *
+ * Suppose that \a tet[0] and \a tet[1] are the tetrahedra providing the
+ * first and second faces respectively.  Then the tetrahedron vertices...
+ *
+ * TODO: Finish this description.
+ */
+struct NSFSAnnulus {
+    NTetrahedron* tet[2];
+    NPerm roles[2];
+
+    NSFSAnnulus();
+    NSFSAnnulus(const NSFSAnnulus& cloneMe);
+    NSFSAnnulus(NTetrahedron* t0, NPerm r0, NTetrahedron* t1, NPerm r1);
+    NSFSAnnulus& operator = (const NSFSAnnulus& cloneMe);
+
+    bool meetsBoundary() const;
+
+    /**
+     * PRE: Must be something on the other side!
+     */
+    void switchSides();
+    /**
+     * PRE: Must be something on the other side!
+     */
+    NSFSAnnulus otherSide() const;
+
+    void transform(const NTriangulation* originalTri,
+            const NIsomorphism* iso, const NTriangulation* newTri);
+    NSFSAnnulus image(const NTriangulation* originalTri,
+            const NIsomorphism* iso, const NTriangulation* newTri) const;
+};
+
+class NSFSSocketHolder {
+    protected:
+        unsigned nSockets_;
+        const NAnnulusBoundary* socket_;
+        bool *socketOrient_;
+        NSFSPlug** plug_;
+            /** Guaranteed non-zero. */
+
+    public:
+        NSFSSocketHolder(const NSFSSocketHolder& cloneMe);
+
+        /**
+         * Takes everything else through the isomorphism, but set all
+         * plugs to zero.
+         */
+        NSFSSocketHolder(const NSFSSocketHolder& preImage,
+                const NTriangulation* preImageTri, const NIsomorphism* iso,
+                const NTriangulation* useTri);
+
+        /**
+         * Does \e not destroy plugs.  Just the arrays.
+         */
+        virtual ~NSFSSocketHolder();
+
+        unsigned numberOfSockets() const;
+        const NAnnulusBoundary& socket(unsigned which) const;
+        bool socketOrient(unsigned which) const;
+        const NSFSPlug* plug(unsigned which) const;
+
+        /**
+         * Returns true if and only if all plugs were filled in.
+         */
+        bool isFullyPlugged();
+
+    protected:
+        /**
+         * Initialises all plugs to zero.
+         * Socket arrays are contructed but uninitialised.
+         *
+         * \a numSockets must be at least one.
+         */
+        NSFSSocketHolder(unsigned numSockets);
+
+    private:
+        /**
+         * Do not allow direct assignment.
+         *
+         * \warning This operator does nothing!  Don't use it.
+         */
+        NSFSSocketHolder& operator = (const NSFSSocketHolder& cloneMe);
+};
+
+class NSFSPlug : public ShareableObject {
+    protected:
+        NAnnulusBoundary toSocket_;
+
+    public:
+        /**
+         * My side of the socket.
+         */
+        const NAnnulusBoundary& toSocket() const;
+
+        virtual void adjustSFS(NSFSpace& sfs, bool reflect) const = 0;
+
+        virtual std::ostream& writeName(std::ostream& out) const = 0;
+        virtual std::ostream& writeTeXName(std::ostream& out) const = 0;
+
+        void writeTextShort(std::ostream& out) const;
+        void writeTextLong(std::ostream& out) const;
+};
+
+class NSFSRoot : public ShareableObject, NSFSSocketHolder {
+    protected:
+        NTriangulation root_;
+
+    public:
+        const NTriangulation& root() const;
+
+        virtual void NSFSSpace* createSFS() const = 0;
+
+        virtual std::ostream& writeName(std::ostream& out) const = 0;
+        virtual std::ostream& writeTeXName(std::ostream& out) const = 0;
+
+        void writeTextShort(std::ostream& out) const;
+        void writeTextLong(std::ostream& out) const;
+
+    protected:
+        NSFSRoot(unsigned numSockets);
+};
+
+/**
+ * No plugs can be zero.
+ */
+class NSFSTree : public NStandardTriangulation, NSFSSocketHolder {
+    private:
+        const NSFSRoot& root_;
+        NIsomorphism* rootIso_;
+            /** Must be non-zero. */
+
+    public:
+        /**
+         * Destroys the plugs.
+         */
+        ~NSFSTree();
+
+        const NSFSRoot& root() const;
+        const NIsomorphism* rootIso(); const
+
+        static NSFSTree* isSFSTree(NTriangulation* tri);
+
+        NManifold* getManifold() const;
+        std::ostream& writeName(std::ostream& out) const;
+        std::ostream& writeTeXName(std::ostream& out) const;
+        void writeTextLong(std::ostream& out) const;
+
+    private:
+        NSFSTree(const NSFSRoot& whichRoot, NIsomorphism* whichRootIso,
+                const NSFSSocketHolder& useSockets);
+
+        /**
+         * Contains code common to both writeName() and writeTeXName().
+         *
+         * @param out the output stream to which to write.
+         * @param tex \c true if this routine is called from
+         * writeTeXName() or \c false if it is called from writeName().
+         * @return a reference to \a out.
+         */
+        std::ostream& writeCommonName(std::ostream& out, bool tex) const;
+
+        static NSFSTree* hunt(NTriangulation* tri, const NSFSRoot& root);
+};
+
+/*@}*/
+
+// Inline functions for NSFSAnnulus
+
+inline NSFSAnnulus::NSFSAnnulus() {
+    tet[0] = tet[1] = 0;
+}
+
+inline NSFSAnnulus::NSFSAnnulus(const NSFSAnnulus& cloneMe) {
+    tet[0] = cloneMe.tet[0]; tet[1] = cloneMe.tet[1];
+    roles[0] = cloneMe.roles[0]; roles[1] = cloneMe.roles[1];
+}
+
+inline NSFSAnnulus::NSFSAnnulus(NTetrahedron* t0, NPerm r0,
+        NTetrahedron* t1, NPerm r1) {
+    tet[0] = t0; tet[1] = t1;
+    roles[0] = r0; roles[1] = r1;
+}
+
+inline NSFSAnnulus& NSFSAnnulus::operator = (const NSFSAnnulus& cloneMe) {
+    tet[0] = cloneMe.tet[0]; tet[1] = cloneMe.tet[1];
+    roles[0] = cloneMe.roles[0]; roles[1] = cloneMe.roles[1];
+}
+
+inline NSFSAnnulus NSFSAnnulus::otherSide() const {
+    return NSFSAnnulus(*this).switchSides();
+}
+
+inline NSFSAnnulus NSFSAnnulus::image(const NTriangulation* originalTri,
+        const NIsomorphism* iso, const NTriangulation* newTri) const {
+    return NSFSAnnulus(*this).transform(originalTri, iso, newTri);
+}
+
+// Inline functions for NSFSSocketHolder
+
+inline NSFSSocketHolder& NSFSSocketHolder::operator = (
+        const NSFSSocketHolder&) {
+    // Vacuous inaccessible assignment operator.
+}
+
+inline NSFSSocketHolder::NSFSSocketHolder(unsigned numSockets) :
+        nSockets_(numSockets), socket_(new NAnnulusBoundary[numsockets]),
+        socketOrient_(new bool[numSockets]), plug_(new NSFSPlug*[numSockets]) {
+    for (unsigned i = 0; i < numSockets; i++)
+        plug_[i] = 0;
+}
+
+inline NSFSSocketHolder::~NSFSSocketHolder() {
+    delete[] socket_;
+    delete[] socketOrient_;
+    delete[] plug_;
+}
+
+inline unsigned NSFSSocketHolder::numberOfSockets() const {
+    return nSockets_;
+}
+
+inline const NAnnulusBoundary& NSFSSocketHolder::socket(unsigned which) const {
+    return socket_[which];
+}
+
+inline bool NSFSSocketHolder::socketOrient(unsigned which) const {
+    return socketOrient_[which];
+}
+
+inline const NSFSPlug* NSFSSocketHolder::plug(unsigned which) const {
+    return plug_[which];
+}
+
+// Inline functions for NSFSPlug
+
+inline const NAnnulusBoundary& NSFSPlug::toSocket() const {
+    return toSocket_;
+}
+
+inline void NSFSPlug::writeTextShort(std::ostream& out) const {
+    writeName(out);
+}
+
+inline void NSFSPlug::writeTextLong(std::ostream& out) const {
+    out << "SFS plug: ";
+    writeName(out);
+}
+
+// Inline functions for NSFSRoot
+
+inline const NTriangulation& NSFSRoot::root() const {
+    return root_;
+}
+
+inline void NSFSRoot::writeTextShort(std::ostream& out) const {
+    writeName(out);
+}
+
+inline void NSFSRoot::writeTextLong(std::ostream& out) const {
+    out << "SFS root: ";
+    writeName(out);
+}
+
+inline NSFSRoot::NSFSRoot(unsigned numSockets) : NSFSSocketHolder(numSockets) {
+}
+
+// Inline functions for NSFSTree
+
+inline NSFSTree::NSFSTree(const NSFSRoot& whichRoot,
+        NIsomorphism* whichRootIso, const NSFSSocketHolder& useSockets) :
+        NSFSSocketHolder(useSockets), root_(whichRoot), rootIso_(whichRootIso) {
+}
+
+inline const NSFSRoot& NSFSTree::root() const {
+    return root_;
+}
+
+inline const NIsomorphism* NSFSTree::rootIso() const {
+    return rootIso_;
+}
+
+inline std::ostream& NSFSTree::writeName(std::ostream& out) const {
+    return writeCommonName(out, false);
+}
+
+inline std::ostream& NSFSTree::writeTeXName(std::ostream& out) const {
+    return writeCommonName(out, true);
+}
+
+inline void NSFSTree::writeTextLong(std::ostream& out) const {
+    out << "SFS tree: ";
+    writeName(out);
+}
+
+} // namespace regina
+
+#endif
+
