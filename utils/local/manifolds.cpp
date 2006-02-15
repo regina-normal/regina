a51161f7dc58994bd8e43a5a1df1149dab0745f8
Benjamin Burton <bab@debian.org>
2006-02-15 06:09:25 +0000

    Add sorting by manifold type, not label.

--- utils/local/manifolds.cpp
+++ utils/local/manifolds.cpp
@@ -44,21 +44,34 @@
  * 3-manifolds (e.g., simpler quotient spaces will be named according to
  * their full Seifert structures).
  *
- * If the option -r is passed, container packets will be renamed
- * according to the newly discovered 3-manifold names and the original
- * file will be overwritten with this new data.
+ * If the option -o is passed, the original file will be overwritten
+ * with any changes that were made.  Otherwise output will be written to
+ * the console only.
  *
- * All output is written to standard output.
+ * If the option -r is passed, container packets will be renamed
+ * according to the newly discovered 3-manifold names.
+ *
+ * If the option -s is passed, container packets will be sorted (within
+ * their respective parent containers) according to the underlying
+ * 3-manifold.  The method of ordering 3-manifolds is subject to change;
+ * currently it bears some resemblance to the ordering used by Martelli
+ * and Petronio in their tables of closed orientable 3-manifolds.
  */
 
 #include <file/nxmlfile.h>
-#include <manifold/nmanifold.h>
+#include <manifold/nlensspace.h>
+#include <manifold/nngsfsloop.h>
+#include <manifold/nngsfspair.h>
+#include <manifold/nsfs.h>
+#include <manifold/ntorusbundle.h>
 #include <packet/ncontainer.h>
 #include <subcomplex/nstandardtri.h>
 #include <triangulation/ntriangulation.h>
 
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 using namespace regina;
 
@@ -68,24 +81,162 @@ unsigned totMfdsRenamed = 0;
 
 bool detailedNames = false;
 bool renameMfds = false;
+bool sortMfds = false;
+bool saveChanges = false;
 NPacket* tree;
+
+struct ManifoldSpec {
+    NPacket* container;
+    bool hasTriangulation;
+    NManifold* manifold; // 0 if unknown
+
+    ManifoldSpec() : container(0), hasTriangulation(false), manifold(0) {
+    }
+    ManifoldSpec(NPacket* c, bool t, NManifold* m) :
+            container(c), hasTriangulation(t), manifold(m) {
+    }
+
+    bool operator < (const ManifoldSpec& other) const;
+};
+
+std::vector<ManifoldSpec> children;
 
 void usage(const char* progName, const std::string& error = std::string()) {
     if (! error.empty())
         std::cerr << error << "\n\n";
 
     std::cerr << "Usage:\n";
-    std::cerr << "    " << progName << " [ -d ] [ -r ] <file.rga>\n";
+    std::cerr << "    " << progName <<
+        " [ -d ] [ -o ] [ -r ] [ -s ] <file.rga>\n";
     std::cerr << std::endl;
     std::cerr << "    -d : Use more detailed 3-manifold names\n";
-    std::cerr << "    -r : Rename container packets and overwrite file\n";
+    std::cerr << "    -o : Save changes to the original file\n";
+    std::cerr << "    -r : Rename container packets according to 3-manifold\n";
+    std::cerr << "    -s : Sort container packets by 3-manifold\n";
     std::cerr << std::endl;
     std::cerr << "Resulting data is written to standard output.\n";
     std::cerr << "Statistics and diagnostic messages are written to standard error.\n";
     exit(1);
 }
 
-void process(NContainer* c) {
+bool ManifoldSpec::operator < (const ManifoldSpec& other) const {
+    // Here comes the part where we sort manifolds.
+
+    // Before we continue, anything that doesn't come with a
+    // triangulation goes first, and anything with a triangulation but
+    // no manifold goes last.
+
+    if (! other.hasTriangulation)
+        return false;
+    if (! hasTriangulation)
+        return true;
+
+    if (! manifold)
+        return false;
+    if (! other.manifold)
+        return true;
+
+    // Both structures have a triangulation and a manifold.
+
+    // Lens spaces go first.
+    NLensSpace* lens1 = dynamic_cast<NLensSpace*>(manifold);
+    NLensSpace* lens2 = dynamic_cast<NLensSpace*>(other.manifold);
+
+    if (lens1 && ! lens2)
+        return true;
+    if (lens2 && ! lens1)
+        return false;
+    if (lens1 && lens2) {
+        return (lens1->getP() < lens2->getP() ||
+            (lens1->getP() == lens2->getP() && lens1->getQ() < lens2->getQ()));
+    }
+
+    // Next go through Seifert fibred spaces.
+    NSFSpace* sfs1 = dynamic_cast<NSFSpace*>(manifold);
+    NSFSpace* sfs2 = dynamic_cast<NSFSpace*>(other.manifold);
+
+    if (sfs1 && ! sfs2)
+        return true;
+    if (sfs2 && ! sfs1)
+        return false;
+    if (sfs1 && sfs2) {
+        long genus1 = (sfs1->isBaseOrientable() ? sfs1->getBaseGenus() * 2 :
+            sfs1->getBaseGenus());
+        long genus2 = (sfs2->isBaseOrientable() ? sfs2->getBaseGenus() * 2 :
+            sfs2->getBaseGenus());
+        if (genus1 < genus2) return true;
+        if (genus2 < genus1) return false;
+
+        if (sfs1->getBaseReflectors() < sfs2->getBaseReflectors()) return true;
+        if (sfs2->getBaseReflectors() < sfs1->getBaseReflectors()) return false;
+
+        if (sfs1->getBasePunctures() < sfs2->getBasePunctures()) return true;
+        if (sfs2->getBasePunctures() < sfs1->getBasePunctures()) return false;
+
+        if (sfs2->isBaseOrientable() && ! sfs1->isBaseOrientable())
+            return true;
+        if (sfs1->isBaseOrientable() && ! sfs2->isBaseOrientable())
+            return false;
+
+        if (sfs1->getBaseClass() < sfs2->getBaseClass()) return true;
+        if (sfs2->getBaseClass() < sfs1->getBaseClass()) return false;
+
+        if (sfs1->getFibreCount() < sfs2->getFibreCount()) return true;
+        if (sfs2->getFibreCount() < sfs1->getFibreCount()) return false;
+
+        unsigned long n = sfs1->getFibreCount();
+        for (unsigned long i = 0; i < n; i++) {
+            if (sfs1->getFibre(i) < sfs2->getFibre(i)) return true;
+            if (sfs2->getFibre(i) < sfs1->getFibre(i)) return false;
+        }
+
+        if (sfs1->getObstruction() < sfs2->getObstruction()) return true;
+        if (sfs2->getObstruction() < sfs1->getObstruction()) return false;
+    }
+
+    // Now for torus bundles.
+    NTorusBundle* bundle1 = dynamic_cast<NTorusBundle*>(manifold);
+    NTorusBundle* bundle2 = dynamic_cast<NTorusBundle*>(other.manifold);
+
+    if (bundle1 && ! bundle2)
+        return true;
+    if (bundle2 && ! bundle1)
+        return false;
+    if (bundle1 && bundle2) {
+        // Just sort by name here, since bundle parameters will probably
+        // need to be made canonical anyway.
+        return manifold->getName() < other.manifold->getName();
+    }
+
+    // Finally non-geometric stuff (SFS pairs and loops).
+    // In all of these cases sort by name for want of something more
+    // sophisticated (TODO).
+    NNGSFSPair* pair1 = dynamic_cast<NNGSFSPair*>(manifold);
+    NNGSFSPair* pair2 = dynamic_cast<NNGSFSPair*>(other.manifold);
+    if (pair1 && ! pair2)
+        return true;
+    if (pair2 && ! pair1)
+        return false;
+    if (pair1 && pair2)
+        return manifold->getName() < other.manifold->getName();
+
+    NNGSFSLoop* loop1 = dynamic_cast<NNGSFSLoop*>(manifold);
+    NNGSFSLoop* loop2 = dynamic_cast<NNGSFSLoop*>(other.manifold);
+    if (loop1 && ! loop2)
+        return true;
+    if (loop2 && ! loop1)
+        return false;
+    if (loop1 && loop2)
+        return manifold->getName() < other.manifold->getName();
+
+    // No idea.  Use the dictionary.
+    return manifold->getName() < other.manifold->getName();
+}
+
+/**
+ * Returns true if and only if the given container contains any triangulations.
+ */
+bool process(NContainer* c) {
     bool foundTri = false;
 
     NStandardTriangulation* std;
@@ -116,7 +267,8 @@ void process(NContainer* c) {
                 name = structure;
         }
 
-        std::cout << c->getPacketLabel() << "  ->>  " << name << '\n';
+        if (! sortMfds)
+            std::cout << c->getPacketLabel() << "  ->>  " << name << '\n';
         totMfds++;
         totMfdsOk++;
 
@@ -126,15 +278,25 @@ void process(NContainer* c) {
                 totMfdsRenamed++;
             }
 
+        if (sortMfds)
+            children.push_back(ManifoldSpec(c, true, mfd));
+        else
+            delete mfd;
+
         delete std;
-        delete mfd;
-        return;
+        return true;
     }
 
     if (foundTri) {
-        std::cout << c->getPacketLabel() << "  ->>  UNKNOWN\n";
+        if (! sortMfds)
+            std::cout << c->getPacketLabel() << "  ->>  UNKNOWN\n";
         totMfds++;
     }
+
+    if (sortMfds)
+        children.push_back(ManifoldSpec(c, foundTri, 0));
+
+    return foundTri;
 }
 
 int main(int argc, char* argv[]) {
@@ -155,8 +317,12 @@ int main(int argc, char* argv[]) {
             break;
         } else if (optChar == 'd')
             detailedNames = true;
+        else if (optChar == 'o')
+            saveChanges = true;
         else if (optChar == 'r')
             renameMfds = true;
+        else if (optChar == 's')
+            sortMfds = true;
         else
             usage(argv[0], std::string("Invalid option: ") + argv[i]);
     }
@@ -173,18 +339,62 @@ int main(int argc, char* argv[]) {
     }
 
     // Process the packets.
-    for (NPacket* p = tree; p; p = p->nextTreePacket())
-        if (p->getPacketType() == NContainer::packetType && p != tree)
-            process(static_cast<NContainer*>(p));
+    if (! sortMfds) {
+        // Just run through them in prefix order.
+        for (NPacket* p = tree; p; p = p->nextTreePacket())
+            if (p->getPacketType() == NContainer::packetType)
+                process(static_cast<NContainer*>(p));
+    } else {
+        // We need to be careful about how we run through the tree.
+        // We need to do all child containers of each parent container at
+        // once, so we can sort within parents.
+
+        // Process the root first, since it doesn't have a parent to be
+        // sorted within.
+        if (tree->getPacketType() == NContainer::packetType)
+            process(static_cast<NContainer*>(tree));
+
+        NPacket *parent, *p;
+        bool foundManifolds;
+        for (parent = tree; parent; parent = parent->nextTreePacket()) {
+            // Process all children of parent, and then sort.
+            foundManifolds = false;
+            children.clear();
+
+            for (p = parent->getFirstTreeChild(); p;
+                    p = p->getNextTreeSibling())
+                if (p->getPacketType() == NContainer::packetType) {
+                    if (process(static_cast<NContainer*>(p)))
+                        foundManifolds = true;
+                } else
+                    children.push_back(ManifoldSpec(p, false, 0));
+
+            if (foundManifolds) {
+                std::stable_sort(children.begin(), children.end());
+
+                for (std::vector<ManifoldSpec>::iterator it = children.begin();
+                        it != children.end(); it++)
+                    it->container->moveToLast();
+            }
+        }
+
+        // Finally tell us what we did.
+        std::cout << "Final container listing:\n\n";
+        for (p = tree; p; p = p->nextTreePacket())
+            if (p->getPacketType() == NContainer::packetType)
+                std::cout << p->getPacketLabel() << '\n';
+    }
 
     // Save the data file if required.
-    if (renameMfds) {
+    if (saveChanges) {
         tree->makeUniqueLabels(0);
 
         std::cerr << std::endl;
-        if (totMfdsRenamed == 0)
+        if (totMfdsRenamed == 0 && ! sortMfds)
             std::cerr << "No 3-manifolds were renamed; not saving data.\n";
         else {
+            if (sortMfds)
+                std::cerr << "Manifolds were sorted and ";
             std::cerr << totMfdsRenamed
                 << (totMfdsRenamed == 1 ? " manifold" : " manifolds")
                 << " will be renamed.\n";
