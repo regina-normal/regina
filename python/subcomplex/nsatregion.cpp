ff0ca3468295ee7e0ba036aefe28266a9dd506b6
Benjamin Burton <bab@debian.org>
2006-03-18 12:04:12 +0000

    Oops.

--- python/subcomplex/nsatregion.cpp
+++ python/subcomplex/nsatregion.cpp
@@ -78,7 +78,7 @@ void addNSatRegion() {
             std::auto_ptr<NSatRegion> >("NSatRegion", init<NSatBlock*>())
         .def("numberOfBlocks", &NSatRegion::numberOfBlocks)
         .def("block", &NSatRegion::block, return_internal_reference<>())
-        .def("blockIndex", &NSatRegion::block)
+        .def("blockIndex", &NSatRegion::blockIndex)
         .def("numberOfBoundaryAnnuli", &NSatRegion::numberOfBoundaryAnnuli)
         .def("boundaryAnnulus", boundaryAnnulus_tuple)
         .def("createSFS", &NSatRegion::createSFS,
