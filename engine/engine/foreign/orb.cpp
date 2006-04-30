d3ce71e8ee173aaff6c9f0d786b6b561be811499
Benjamin Burton <bab@debian.org>
2006-04-30 06:10:27 +0000

    Replace tabs with spaces, so it looks correct for people who have tabstop < 8.

--- engine/engine/foreign/orb.cpp
+++ engine/engine/foreign/orb.cpp
@@ -77,7 +77,7 @@ NTriangulation *cassonToNTriangulation( CassonFormat *cf )
  //  Orb::cassonToTriangulation as much as possible.
  NTetrahedron **tet = new NTetrahedron*[cf->num_tet]; // tet corresponds to tet_array in Orb
  for (i=0; i<cf->num_tet; i++)
-	tet[i]=new NTetrahedron();
+        tet[i]=new NTetrahedron();
  // now tet is a pointer to an array of NTetrahedrons,
  //  so for each tet[i] we need to run
  //   for (j=0; j<4; j++)
@@ -93,42 +93,42 @@ NTriangulation *cassonToNTriangulation( CassonFormat *cf )
  // tetrahedra and assembled the information into tet. this code is
  // adapted from Orb::cassonToTriangulation in Orb's organizer.cpp
  while (ei!=NULL) // if we have a non-trivial edge, proceed
-	{
-	tei1 = ei->head;
-	while (tei1!=NULL) // now we spin about the tetrahedra adj to ei.
-		{
-		if (tei1->next==NULL)
-			tei2 = ei->head;
-		else	tei2 = tei1->next;
+        {
+        tei1 = ei->head;
+        while (tei1!=NULL) // now we spin about the tetrahedra adj to ei.
+                {
+                if (tei1->next==NULL)
+                        tei2 = ei->head;
+                else    tei2 = tei1->next;
 
-		t1 = tei1->tet_index;
-		a1 = tei1->f1;
-		a2 = tei1->f2;
-		a3 = vertex_at_faces[a1][a2];
-		a4 = vertex_at_faces[a2][a1];
+                t1 = tei1->tet_index;
+                a1 = tei1->f1;
+                a2 = tei1->f2;
+                a3 = vertex_at_faces[a1][a2];
+                a4 = vertex_at_faces[a2][a1];
 
-		t2 = tei2->tet_index;
-		b1 = tei2->f1;
-		b2 = tei2->f2;
-		b3 = vertex_at_faces[b1][b2];
-		b4 = vertex_at_faces[b2][b1];
+                t2 = tei2->tet_index;
+                b1 = tei2->f1;
+                b2 = tei2->f2;
+                b3 = vertex_at_faces[b1][b2];
+                b4 = vertex_at_faces[b2][b1];
 
-		tet[t1]->joinTo( tei1->f1 , tet[t2], // 1st entry is the face of tet[t1]
-				NPerm(a1,b2,a2,b1,a3,b3,a4,b4) ); // being attached to tet[t2]
+                tet[t1]->joinTo( tei1->f1 , tet[t2], // 1st entry is the face of tet[t1]
+                                NPerm(a1,b2,a2,b1,a3,b3,a4,b4) ); // being attached to tet[t2]
 
-		tet[t2]->joinTo( tei2->f2 , tet[t1],
-				NPerm(b1,a2,b2,a1,b3,a3,b4,a4) );
+                tet[t2]->joinTo( tei2->f2 , tet[t1],
+                                NPerm(b1,a2,b2,a1,b3,a3,b4,a4) );
 
-		tei1 = tei1->next;
-		}
-	ei = ei->next;
-	}
- 
+                tei1 = tei1->next;
+                }
+        ei = ei->next;
+        }
+
  for (i=0; i<cf->num_tet; i++)
-	triang->addTetrahedron(tet[i]);
+        triang->addTetrahedron(tet[i]);
  delete[] tet;
 
- return triang; 
+ return triang;
 }
 
 
@@ -145,73 +145,73 @@ CassonFormat *readCassonFormat( std::istream &ts )
         TetEdgeInfo     *ntei,
                         *tei;
 
-	cf = new CassonFormat;
-	cf->head = NULL;
-	cf->num_tet = 0;
+        cf = new CassonFormat;
+        cf->head = NULL;
+        cf->num_tet = 0;
 
-	// Find a non-empty line.
-	// The code from Orb used QString::skipWhiteSpace(); we do it
-	// manually.
-	do {
-		getline(ts, line);
-		stripWhitespace(line);
-	} while ((! ts.eof()) && line.empty());
+        // Find a non-empty line.
+        // The code from Orb used QString::skipWhiteSpace(); we do it
+        // manually.
+        do {
+                getline(ts, line);
+                stripWhitespace(line);
+        } while ((! ts.eof()) && line.empty());
 
-	// Process lines one at a time until we hit an empty line or EOF.
-	while ((! ts.eof()) && (! line.empty()) && (line != "% diagram"))
-	{
-		// The code from Orb used QString's record separation
-		// routines.  We don't have that in std::string, so
-		// we'll do it all with istringstreams instead.
-		std::istringstream tokens(line);
+        // Process lines one at a time until we hit an empty line or EOF.
+        while ((! ts.eof()) && (! line.empty()) && (line != "% diagram"))
+        {
+                // The code from Orb used QString's record separation
+                // routines.  We don't have that in std::string, so
+                // we'll do it all with istringstreams instead.
+                std::istringstream tokens(line);
 
-		nei = new EdgeInfo;
+                nei = new EdgeInfo;
 
                 if (cf->head==NULL)
                         cf->head = nei;
-                else	ei->next = nei;
+                else    ei->next = nei;
 
-		nei->next = NULL;
-		nei->head = NULL;
+                nei->next = NULL;
+                nei->head = NULL;
 
-		ei = nei;
+                ei = nei;
 
-		tokens >> ei->index;
-		ei->index--;
+                tokens >> ei->index;
+                ei->index--;
 
-		// We never use these two values; just suck them in and
-		// forget them.
-		tokens >> ei->singular_index >> ei->singular_order;
+                // We never use these two values; just suck them in and
+                // forget them.
+                tokens >> ei->singular_index >> ei->singular_order;
 
-		tokens >> section;
-		while (!section.empty())	
-		{
-			ntei = new TetEdgeInfo;
+                tokens >> section;
+                while (!section.empty())
+                {
+                        ntei = new TetEdgeInfo;
 
-			if (ei->head==NULL)
-				ei->head        = ntei;
-			else
-				tei->next       = ntei;
+                        if (ei->head==NULL)
+                                ei->head        = ntei;
+                        else
+                                tei->next       = ntei;
 
-			ntei->next = NULL;
-			tei = ntei;
+                        ntei->next = NULL;
+                        tei = ntei;
 
-			tei->f1 = LN(section[section.length()-2]);
-			tei->f2 = LN(section[section.length()-1]);
-			section.resize(section.length()-2);
-			tei->tet_index = atoi(section.c_str()) - 1;
+                        tei->f1 = LN(section[section.length()-2]);
+                        tei->f2 = LN(section[section.length()-1]);
+                        section.resize(section.length()-2);
+                        tei->tet_index = atoi(section.c_str()) - 1;
 
-			if (tei->tet_index + 1 > cf->num_tet)
-				cf->num_tet = tei->tet_index + 1;
+                        if (tei->tet_index + 1 > cf->num_tet)
+                                cf->num_tet = tei->tet_index + 1;
 
-			section.clear();
-			tokens >> section;
-		}
+                        section.clear();
+                        tokens >> section;
+                }
 
-		getline(ts, line);
-	}
+                getline(ts, line);
+        }
 
-	return cf;
+        return cf;
 }
 
 /**
@@ -219,49 +219,49 @@ CassonFormat *readCassonFormat( std::istream &ts )
  */
 bool verifyCassonFormat( CassonFormat *cf )
 {
-	int		i,j,k;
-	bool		check[4][4];
-	EdgeInfo	*ei;
-	TetEdgeInfo	*tei;
+        int             i,j,k;
+        bool            check[4][4];
+        EdgeInfo        *ei;
+        TetEdgeInfo     *tei;
 
-	for(i=0;i<cf->num_tet;i++)
-	{
-		for(j=0;j<4;j++)
-			for(k=0;k<4;k++)
-			if (j==k)
-				check[j][k] = true;
-			else	check[j][k] = false;
+        for(i=0;i<cf->num_tet;i++)
+        {
+                for(j=0;j<4;j++)
+                        for(k=0;k<4;k++)
+                        if (j==k)
+                                check[j][k] = true;
+                        else    check[j][k] = false;
 
-		ei = cf->head;
+                ei = cf->head;
 
-		if (ei == NULL)
-			return false;
+                if (ei == NULL)
+                        return false;
 
-		while(ei!=NULL)
-		{
-			tei = ei->head;
-			if (tei == NULL)
-				return false;
-			while(tei!=NULL)
-			{
-				if (tei->tet_index == i )
-				{
-					if (check[tei->f1][tei->f2])
-						return true;
-					check[tei->f1][tei->f2] = true;
-					check[tei->f2][tei->f1] = true;
-				}
-				tei = tei->next;
-			}
-			ei = ei->next;
-		}
+                while(ei!=NULL)
+                {
+                        tei = ei->head;
+                        if (tei == NULL)
+                                return false;
+                        while(tei!=NULL)
+                        {
+                                if (tei->tet_index == i )
+                                {
+                                        if (check[tei->f1][tei->f2])
+                                                return true;
+                                        check[tei->f1][tei->f2] = true;
+                                        check[tei->f2][tei->f1] = true;
+                                }
+                                tei = tei->next;
+                        }
+                        ei = ei->next;
+                }
 
-		for(j=0;j<4;j++)
-			for(k=0;k<4;k++)
-			if (check[j][k]==false)
-				return false;
-	}
-	return true;
+                for(j=0;j<4;j++)
+                        for(k=0;k<4;k++)
+                        if (check[j][k]==false)
+                                return false;
+        }
+        return true;
 }
 
 /**
@@ -269,24 +269,24 @@ bool verifyCassonFormat( CassonFormat *cf )
  */
 void freeCassonFormat( CassonFormat *cf )
 {
-	EdgeInfo *e1, *e2;
-	TetEdgeInfo *t1, *t2;
-	e1 = cf->head;
+        EdgeInfo *e1, *e2;
+        TetEdgeInfo *t1, *t2;
+        e1 = cf->head;
 
-	while (e1!=NULL)
-	{
-		e2 = e1->next;
-		t1 = e1->head;
-		while (t1!=NULL)
-		{
-			t2 = t1->next;
-			delete t1;
-			t1 = t2;
-		}
-		delete e1;
-		e1 = e2;
-	}
-	delete cf;
+        while (e1!=NULL)
+        {
+                e2 = e1->next;
+                t1 = e1->head;
+                while (t1!=NULL)
+                {
+                        t2 = t1->next;
+                        delete t1;
+                        t1 = t2;
+                }
+                delete e1;
+                e1 = e2;
+        }
+        delete cf;
 }
 
 /**
@@ -298,32 +298,32 @@ void freeCassonFormat( CassonFormat *cf )
 NTriangulation *readTriangulation( std::istream &ts,  std::string &file_id)
 {
 
-	NTriangulation *manifold = 0;
-	std::string line;
-	getline(ts, line);
-	CassonFormat *cf;
+        NTriangulation *manifold = 0;
+        std::string line;
+        getline(ts, line);
+        CassonFormat *cf;
 
-	if (line == "% orb")
-	 {
-		 getline(ts, file_id);
-	 cf=readCassonFormat( ts );
+        if (line == "% orb")
+         {
+                 getline(ts, file_id);
+         cf=readCassonFormat( ts );
 
-	 if (verifyCassonFormat( cf ))
-		 manifold = cassonToNTriangulation( cf );
-	 else	
+         if (verifyCassonFormat( cf ))
+                 manifold = cassonToNTriangulation( cf );
+         else
              {
-		 std::cout<<"Error (3) reading Orb/Casson file.\n";std::cout.flush();
+                 std::cout<<"Error (3) reading Orb/Casson file.\n";std::cout.flush();
              }
-	 }
-	else std::cout<<"Error (2) reading Orb/Casson file.\n";std::cout.flush();
+         }
+        else std::cout<<"Error (2) reading Orb/Casson file.\n";std::cout.flush();
 
-	freeCassonFormat( cf );
-	return manifold;
+        freeCassonFormat( cf );
+        return manifold;
 }
 
 } // End anonymous namespace
 
-NTriangulation *readOrb(const char *filename) 
+NTriangulation *readOrb(const char *filename)
  {
    std::string file_id;
 
