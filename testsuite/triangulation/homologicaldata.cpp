5ff55a7467bf47dae48ebdc167cfb36d4595758b
Benjamin Burton <bab@debian.org>
2006-11-30 10:40:01 +0000

    Commit all remaining homologicalData tests.

--- testsuite/triangulation/homologicaldata.cpp
+++ testsuite/triangulation/homologicaldata.cpp
@@ -117,9 +117,17 @@ class HomologicalDataTest : public CppUnit::TestFixture {
         void bdryManifoldMapH1() {
             verifyBdryManifoldMapH1(lens7_1, "L(7,1)", "zero map");
             verifyBdryManifoldMapH1(d88xz15, "S^3 / D_88 x Z_15", "zero map");
+            verifyBdryManifoldMapH1(closedHypA, "Closed Hyp (vol=1.01494161)",
+                "zero map");
+            verifyBdryManifoldMapH1(closedHypB, "Closed Hyp (vol=2.45402944)",
+                "zero map");
+            verifyBdryManifoldMapH1(norA, "SFS [M_/n2: (2,1)]", "zero map");
+            verifyBdryManifoldMapH1(s028, "SnapPea s028",
+                "kernel Z | cokernel Z_16 | image Z");
+            verifyBdryManifoldMapH1(s955, "SnapPea s955",
+                "kernel Z | cokernel Z_20 | image Z + Z_2");
             verifyBdryManifoldMapH1(lst3_4_7, "LST(3,4,7)",
                 "epic, with kernel Z");
-            // TODO
         }
 
         void verifyStandardCells(NTriangulation& tri, const char* name,
@@ -148,8 +156,14 @@ class HomologicalDataTest : public CppUnit::TestFixture {
         void standardCells() {
             verifyStandardCells(lens7_1, "L(7,1)", 1, 5, 8, 4);
             verifyStandardCells(d88xz15, "S^3 / D_88 x Z_15", 1, 10, 18, 9);
+            verifyStandardCells(closedHypA, "Closed Hyp (vol=1.01494161)",
+                1, 10, 18, 9);
+            verifyStandardCells(closedHypB, "Closed Hyp (vol=2.45402944)",
+                1, 13, 24, 12);
+            verifyStandardCells(norA, "SFS [M_/n2: (2,1)]", 1, 10, 18, 9);
+            verifyStandardCells(s028, "SnapPea s028", 12, 42, 36, 6);
+            verifyStandardCells(s955, "SnapPea s955", 12, 42, 36, 6);
             verifyStandardCells(lst3_4_7, "LST(3,4,7)", 1, 5, 7, 3);
-            // TODO
         }
 
         void verifyDualCells(NTriangulation& tri, const char* name,
@@ -178,8 +192,14 @@ class HomologicalDataTest : public CppUnit::TestFixture {
         void dualCells() {
             verifyDualCells(lens7_1, "L(7,1)", 4, 8, 5, 1);
             verifyDualCells(d88xz15, "S^3 / D_88 x Z_15", 9, 18, 10, 1);
+            verifyDualCells(closedHypA, "Closed Hyp (vol=1.01494161)",
+                9, 18, 10, 1);
+            verifyDualCells(closedHypB, "Closed Hyp (vol=2.45402944)",
+                12, 24, 13, 1);
+            verifyDualCells(norA, "SFS [M_/n2: (2,1)]", 9, 18, 10, 1);
+            verifyDualCells(s028, "SnapPea s028", 6, 12, 6, 0);
+            verifyDualCells(s955, "SnapPea s955", 6, 12, 6, 0);
             verifyDualCells(lst3_4_7, "LST(3,4,7)", 3, 5, 2, 0);
-            // TODO
         }
 
         void verifyTorsionRankVector(NTriangulation& tri,
@@ -198,8 +218,14 @@ class HomologicalDataTest : public CppUnit::TestFixture {
             verifyTorsionRankVector(lens7_1, "L(7,1)", "7(1)");
             verifyTorsionRankVector(d88xz15, "S^3 / D_88 x Z_15",
                 "2(0 0 1) 3(1) 5(1)");
+            verifyTorsionRankVector(closedHypA, "Closed Hyp (vol=1.01494161)",
+                "2(1) 3(2)");
+            verifyTorsionRankVector(closedHypB, "Closed Hyp (vol=2.45402944)",
+                "7(2)");
+            verifyTorsionRankVector(norA, "SFS [M_/n2: (2,1)]", "2(1 1)");
+            verifyTorsionRankVector(s028, "SnapPea s028", "2(0 0 0 1)");
+            verifyTorsionRankVector(s955, "SnapPea s955", "2(0 1) 5(1)");
             verifyTorsionRankVector(lst3_4_7, "LST(3,4,7)", "no torsion");
-            // TODO
         }
 
         void verifyTorsionSigmaVector(NTriangulation& tri,
@@ -217,8 +243,15 @@ class HomologicalDataTest : public CppUnit::TestFixture {
         void torsionSigmaVector() {
             verifyTorsionSigmaVector(lens7_1, "L(7,1)", "no 2-torsion");
             verifyTorsionSigmaVector(d88xz15, "S^3 / D_88 x Z_15", "7 7 inf");
+            verifyTorsionSigmaVector(closedHypA, "Closed Hyp (vol=1.01494161)",
+                "inf");
+            verifyTorsionSigmaVector(closedHypB, "Closed Hyp (vol=2.45402944)",
+                "no 2-torsion");
+            verifyTorsionSigmaVector(norA, "SFS [M_/n2: (2,1)]",
+                "manifold is non-orientable");
+            verifyTorsionSigmaVector(s028, "SnapPea s028", "7 7 7 inf");
+            verifyTorsionSigmaVector(s955, "SnapPea s955", "inf 0");
             verifyTorsionSigmaVector(lst3_4_7, "LST(3,4,7)", "no 2-torsion");
-            // TODO
         }
 
         void verifyTorsionLegendreSymbolVector(NTriangulation& tri,
@@ -237,9 +270,17 @@ class HomologicalDataTest : public CppUnit::TestFixture {
             verifyTorsionLegendreSymbolVector(lens7_1, "L(7,1)", "7(1)");
             verifyTorsionLegendreSymbolVector(d88xz15, "S^3 / D_88 x Z_15",
                 "3(1) 5(1)");
+            verifyTorsionLegendreSymbolVector(closedHypA,
+                "Closed Hyp (vol=1.01494161)", "3(1)");
+            verifyTorsionLegendreSymbolVector(closedHypB,
+                "Closed Hyp (vol=2.45402944)", "7(-1)");
+            verifyTorsionLegendreSymbolVector(norA, "SFS [M_/n2: (2,1)]",
+                "manifold is non-orientable");
+            verifyTorsionLegendreSymbolVector(s028, "SnapPea s028",
+                "no odd p-torsion");
+            verifyTorsionLegendreSymbolVector(s955, "SnapPea s955", "5(-1)");
             verifyTorsionLegendreSymbolVector(lst3_4_7, "LST(3,4,7)",
                 "no odd p-torsion");
-            // TODO
         }
 };
 
