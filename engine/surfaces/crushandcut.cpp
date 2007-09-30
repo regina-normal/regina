3ce82d67f5042773df94d19f8b85ccf73db35126
Benjamin Burton <bab@debian.org>
2007-09-30 08:33:37 +0000

    Annual copyright update, stage 2.

--- engine/surfaces/crushandcut.cpp
+++ engine/surfaces/crushandcut.cpp
@@ -4,7 +4,7 @@
  *  Regina - A Normal Surface Theory Calculator                           *
  *  Computational Engine                                                  *
  *                                                                        *
- *  Copyright (c) 1999-2006, Ben Burton                                   *
+ *  Copyright (c) 1999-2007, Ben Burton                                   *
  *  For further details contact Ben Burton (bab@debian.org).              *
  *                                                                        *
  *  This program is free software; you can redistribute it and/or         *
