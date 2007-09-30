ba9f331c24840a898b48fee051cd97fa52b13e57
Benjamin Burton <bab@debian.org>
2007-09-30 09:03:40 +0000

    The very last bit of the annual copyright update.  Till next year!

--- scripts/runscript.py
+++ scripts/runscript.py
@@ -3,7 +3,7 @@
 # Regina - A Normal Surface Theory Calculator
 # Python Environment Initialisation
 #
-# Copyright (c) 2003-2006, Ben Burton
+# Copyright (c) 2003-2007, Ben Burton
 # For further details contact Ben Burton (bab@debian.org).
 #
 # Usage: runscript.py [ <library> ... ] [ -- <script> [ <script-arg> ... ]]
