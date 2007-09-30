1d0cb3905d328bfdf994169b8a248b09c3a322d3
Benjamin Burton <bab@debian.org>
2007-09-30 08:59:29 +0000

    Mop up final copyright bits.

--- python/modulemain.cpp
+++ python/modulemain.cpp
@@ -57,7 +57,7 @@ namespace {
     std::string welcome() {
         return std::string(PACKAGE_STRING) +
             "\nA Normal Surface Theory Calculator" +
-            "\nCopyright (c) 1999-2006, Ben Burton";
+            "\nCopyright (c) 1999-2007, Ben Burton";
     }
 
     void shareableWriteTextShort(const ShareableObject& obj) {
