68940ced3ebe18c2caa32c8a587631bf5b453dd7
Benjamin Burton <bab@debian.org>
2008-04-17 12:13:08 +0000

    Support in the calculation for translation between character encodings.

---
+++ engine/utilities/i18nutils.cpp
@@ -0,0 +1,211 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2008, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+#include <cerrno>
+#include <cstring>
+#include <langinfo.h>
+#include <locale.h>
+#include "utilities/i18nutils.h"
+
+namespace regina {
+namespace i18n {
+
+bool Locale::initialised = false;
+
+#ifdef HAVE_ICONV
+const iconv_t IConvStreamBuffer::cdNone((iconv_t)(-1));
+#endif
+
+const char* Locale::codeset() {
+    if (! initialised) {
+        ::setlocale(LC_ALL, "");
+        initialised = true;
+    }
+
+    return nl_langinfo(CODESET);
+}
+
+IConvStreamBuffer* IConvStreamBuffer::open(std::ostream& dest,
+        const char* srcCode, const char* destCode) {
+    if (sink)
+        if (! close())
+            return 0;
+
+    sink = &dest;
+
+#ifdef HAVE_ICONV
+    cd = iconv_open(destCode, srcCode);
+    if (cd == cdNone) {
+        if (errno != EINVAL)
+            return 0;
+
+        // The given encodings are not supported.
+        // This is fine; we'll just pass data through to sink untranslated.
+    }
+#endif
+
+    setp(preBuffer, preBuffer + (sizeof(preBuffer) - 1));
+    return this;
+}
+
+IConvStreamBuffer* IConvStreamBuffer::close() throw() {
+    sync();
+
+#ifdef HAVE_ICONV
+    if (cd == cdNone) {
+        // We're passing data through untranslated; nothing more to do.
+        return this;
+    }
+
+    // Close down the internal iconv workings.
+    if (iconv_close(cd) == 0) {
+        cd = cdNone;
+        return this;
+    } else
+        return 0;
+#else
+    return this;
+#endif
+}
+
+IConvStreamBuffer::int_type IConvStreamBuffer::overflow(
+        IConvStreamBuffer::int_type c) {
+    // Are we even open?
+    if (sink == 0)
+        return traits_type::eof();
+
+    // Add the extra character to the end of the buffer before processing.
+    if (c != traits_type::eof()) {
+        *pptr() = c;
+        pbump(1);
+    }
+
+#ifdef HAVE_ICONV
+    // Do we know how to translate between encodings?  If not, just
+    // send the data straight through to the destination stream.
+    if (cd == cdNone) {
+        ptrdiff_t n = pptr() - preBuffer;
+        sink->write(preBuffer, n);
+        pbump(-n);
+
+        if (sink->fail())
+            return traits_type::eof();
+        else
+            return 0;
+    }
+
+    // Convert the data through iconv().
+    // We might need more than one run through this.
+    while (pptr() > preBuffer) {
+        size_t inBytes = pptr() - preBuffer;
+        size_t outBytes = sizeof(postBuffer);
+
+        ICONV_CONST char* inPtr = preBuffer;
+        char* outPtr = postBuffer;
+
+        ::iconv(cd, &inPtr, &inBytes, &outPtr, &outBytes);
+        int iconvErr = errno;
+        errno = 0;
+
+        // If we got any output, write it to the destination stream.
+        if (outPtr > postBuffer) {
+            sink->write(postBuffer, outPtr - postBuffer);
+            if (sink->fail())
+                return traits_type::eof();
+        }
+
+        // Are we completely finished?
+        if (inBytes == 0) {
+            // Yes!
+            pbump(- (inPtr - preBuffer));
+            return 0;
+        }
+
+        // Something went wrong.
+        if (iconvErr == E2BIG) {
+            // The output buffer filled up.  This shouldn't happen, but
+            // anyway; move the leftover input to the front of the input
+            // buffer and try again.
+            ::memmove(preBuffer, inPtr, inBytes);
+            pbump(- (inPtr - preBuffer));
+            continue;
+        }
+        if (iconvErr == EINVAL) {
+            // We hit an incomplete multibyte sequence.  Move the
+            // leftover input to the front of the buffer and stop, since
+            // we need more input before we can continue translating.
+            ::memmove(preBuffer, inPtr, inBytes);
+            pbump(- (inPtr - preBuffer));
+            return 0;
+        }
+        if (iconvErr == EILSEQ) {
+            // We hit an invalid multibyte sequence.
+            // Try to recover gracefully by just skipping over it.
+            ::memmove(preBuffer, inPtr + 1, inBytes - 1);
+            pbump(- (inPtr + 1 - preBuffer));
+
+            sink->write("?", 1);
+            if (sink->fail())
+                return traits_type::eof();
+
+            continue;
+        }
+        // We should never reach this point, since it indicates an error
+        // state that iconv() should not set.
+        std::cerr << "ERROR: Unexpected state after call to iconv().\n";
+        std::cerr << "Please report this as a bug to the Regina author(s).\n";
+        std::cerr.flush();
+        return traits_type::eof();
+    }
+
+    // We can never reach this point, but keep the compiler happy.
+    return 0;
+#else
+    // We're building without iconv support.
+    // Just send everything straight through to sink.
+    ptrdiff_t n = pptr() - preBuffer;
+    sink->write(preBuffer, n);
+    pbump(-n);
+
+    if (sink->fail())
+        return traits_type::eof();
+    else
+        return 0;
+#endif
+}
+
+int IConvStreamBuffer::sync() {
+    if (sink) {
+        IConvStreamBuffer::int_type ret = overflow(traits_type::eof());
+        sink->flush();
+        return ret == traits_type::eof() || sink->fail() ? -1 : 0;
+    } else
+        return -1;
+}
+
+} } // namespace regina::i18n
---
+++ engine/utilities/i18nutils.h
@@ -0,0 +1,276 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2008, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+/*! \file i18nutils.h
+ *  \brief Various classes and routines for working with internationalisation
+ *  and character encodings.
+ */
+
+#ifndef __I18NUTILS_H
+#ifndef __DOXYGEN
+#define __I18NUTILS_H
+#endif
+
+#include "regina-config.h"
+#include <iostream>
+#ifdef HAVE_ICONV
+#include <iconv.h>
+#endif
+
+namespace regina {
+
+/**
+ * Various classes and routines for working with internationalisation and
+ * character encodings.
+ */
+namespace i18n {
+
+/**
+ * \weakgroup utilities
+ * @{
+ */
+
+/**
+ * A simple class with static routines for querying information about the
+ * current locale.
+ *
+ * These routines use ::setlocale() to determine the current locale,
+ * which means they respect environment variables such as LANG and LC_ALL.
+ */
+class Locale {
+    private:
+        static bool initialised;
+            /**< Have we determined the current locale yet?  This is
+                 done the first time a query routine is called. */
+
+    public:
+        /**
+         * Returns the character encoding used in the current locale.
+         * This is a plain string, such as "UTF-8" or "ISO-8859-1".
+         *
+         * @return the character encoding for the current locale.
+         */
+        static const char* codeset();
+
+    private:
+        /**
+         * Disable the default constructor, since all routines are static.
+         */
+        Locale();
+};
+
+/**
+ * An output stream buffer that translates between character encodings.
+ * The \e iconv library is used to do the real work.
+ *
+ * Users should not normally instantiate this class directly; instead
+ * see IConvStream for a higher-level interface to character conversion.
+ *
+ * This class will still work perfectly well if \e iconv is not supported
+ * on the build machine, though it will simply pass data through without
+ * performing any translations.
+ *
+ * \ifacespython Not included.
+ *
+ * @author Parts of this code are modified from the cxxtools library
+ * (<tt>http://www.tntnet.org/cxxutils.html</tt>), which is
+ * copyright (c) 2003 by Tommi Maekitalo, and covered by the GNU Lesser
+ * General Public License.
+ */
+class IConvStreamBuffer : public std::streambuf {
+    private:
+        std::ostream* sink;
+            /**< The destination output stream, or 0 if it has not yet
+                 been initialised. */
+        char preBuffer[16];
+            /**< The internal pre-conversion character buffer. */
+#ifdef HAVE_ICONV
+        char postBuffer[64];
+            /**< The internal post-conversion character buffer.
+                 Allow characters to blow out to four times their size. */
+        iconv_t cd;
+            /**< The iconv conversion descriptor, or \a cdNone if no
+                 conversion descriptor is currently active. */
+
+        static const iconv_t cdNone;
+            /**< Signifies that no iconv conversion descriptor is
+                 currently active. */
+#endif
+
+    public:
+        /**
+         * Creates a new stream buffer.
+         */
+        IConvStreamBuffer();
+        /**
+         * Destroys this stream buffer.  This stream buffer will be
+         * closed, but the destination output stream will not be.
+         */
+        ~IConvStreamBuffer();
+
+        /**
+         * Opens a new stream buffer that wraps around the given output
+         * stream.  If this stream buffer is already open, it will be
+         * closed and then reopened with the given parameters.
+         *
+         * Any data that is sent to this stream buffer will be
+         * translated from \e srcCode to \e destCode and passed on to the
+         * given output stream.
+         *
+         * If the given encodings are invalid, this stream will still
+         * forward data to the given output stream but no conversion
+         * will take place.
+         *
+         * \pre The destination output stream is already open.
+         *
+         * @param dest the destination output stream.
+         * @param srcCode the character encoding for data that is to be
+         * written into this stream buffer.
+         * @param destCode the character encoding for the translated data
+         * that will subsequently be written to the destination output stream.
+         * @return this stream buffer on success, or 0 on error.
+         */
+        IConvStreamBuffer* open(std::ostream& dest,
+            const char* srcCode, const char* destCode);
+        /**
+         * Closes this stream buffer.
+         *
+         * @return this stream buffer on success, or 0 on error.
+         */
+        IConvStreamBuffer* close() throw();
+
+        /**
+         * Forwards the given character to the destination output stream,
+         * converting between character sets en route.
+         *
+         * @param c the unconverted character to write.
+         * @return 0 on success, or EOF on error.
+         */
+        int_type overflow(int_type c);
+        /**
+         * Simply returns EOF (since this is not an input stream).
+         *
+         * @return EOF.
+         */
+        int_type underflow();
+        /**
+         * Flushes the output buffer.
+         *
+         * @return 0 on success, or -1 on error.
+         */
+        int sync();
+};
+
+/**
+ * An output stream that convertes between character encodings.
+ * The \e iconv library does all the work behind the scenes.
+ *
+ * An IConvStream acts as a wrapper around some other destination
+ * output stream (for instance, std::cout).  To use an IConvStream:
+ *
+ * - Construct it, passing the destination output stream and the to/from
+ *   character encodings to the class constructor;
+ *
+ * - Write data to this IConvStream, which will be converted and
+ *   forwarded on to the destination output stream;
+ *
+ * - Destroy this IConvStream.  The destination output stream will
+ *   remain open.
+ *
+ * This class will still work perfectly well if \e iconv is not supported
+ * on the build machine.  In this case it will simply pass all data
+ * directly through to the destination output stream.
+ *
+ * \ifacespython Not present.
+ *
+ * @author Parts of this code are modified from the cxxtools library
+ * (<tt>http://www.tntnet.org/cxxutils.html</tt>), which is
+ * copyright (c) 2003 by Tommi Maekitalo, and covered by the GNU Lesser
+ * General Public License.
+ */
+class IConvStream : public std::ostream {
+    private:
+        IConvStreamBuffer buf;
+            /**< The IConvStreamBuffer that does all the actual work. */
+
+    public:
+        /**
+         * Creates a new IConvStream; see the class notes for details.
+         *
+         * If the given encodings are invalid, this stream will still
+         * forward data to the given output stream but no conversion
+         * will take place.
+         *
+         * \pre The destination output stream is already open.
+         *
+         * @param dest the destination output stream.
+         * @param srcCode the character encoding for data that is to be
+         * written into this IConvStream.
+         * @param destCode the character encoding for the translated data
+         * that will subsequently be written to the destination output stream.
+         */
+        IConvStream(std::ostream& dest,
+            const char* srcCode, const char* destCode);
+};
+
+// Inline functions for Locale
+
+inline Locale::Locale() {
+}
+
+// Inline functions for IConvStreamBuffer
+
+#ifdef HAVE_ICONV
+inline IConvStreamBuffer::IConvStreamBuffer() :
+        sink(0), cd(cdNone) {
+}
+#else
+inline IConvStreamBuffer::IConvStreamBuffer() :
+        sink(0) {
+}
+#endif
+
+inline IConvStreamBuffer::~IConvStreamBuffer() {
+    close();
+}
+
+inline IConvStreamBuffer::int_type IConvStreamBuffer::underflow() {
+    return traits_type::eof();
+}
+
+// Inline functions for IConvStream
+
+inline IConvStream::IConvStream(std::ostream& dest,
+        const char* srcCode, const char* destCode) : std::ostream(&buf) {
+    if (buf.open(dest, srcCode, destCode) == 0)
+        setstate(std::ios::failbit);
+}
+
+} } // namespace regina::i18n
+
+#endif
