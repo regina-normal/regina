c97887287843bf78bddd58eff42d35544dfbeaf9
Benjamin Burton <bab@debian.org>
2008-04-26 23:19:49 +0000

    For pre-utf8 data files, change encodings by surgery on the data stream, rather than by the more fickle switchEncoding().

--- engine/file/nxmlfile.cpp
+++ engine/file/nxmlfile.cpp
@@ -27,6 +27,7 @@
 /* end stub */
 
 #include <cctype>
+#include <cstring>
 #include <fstream>
 #include "engine.h"
 #include "file/nfile.h"
@@ -50,15 +51,11 @@ namespace {
             bool isReginaData;
                 /**< Are we actually reading a \<reginadata ...\> element? */
 
-            regina::xml::XMLParser* parser;
-                /**< The current XML parser, or 0 if we have not yet been
-                     given this information. */
-
         public:
             /**
              * Create a new top-level reader.
              */
-            ReginaDataReader() : isReginaData(false), parser(0) {
+            ReginaDataReader() : isReginaData(false) {
             }
 
             virtual NPacket* getPacket() {
@@ -68,23 +65,10 @@ namespace {
                     return 0;
             }
 
-            virtual void usingParser(regina::xml::XMLParser* p) {
-                parser = p;
-            }
-
             virtual void startElement(const std::string& n,
-                    const regina::xml::XMLPropertyDict& tagProps,
-                    NXMLElementReader*) {
-                if (n == "reginadata") {
+                    const regina::xml::XMLPropertyDict&, NXMLElementReader*) {
+                if (n == "reginadata")
                     isReginaData = true;
-
-                    // Old files (pre-4.5) are stored in latin1, not utf8.
-                    regina::xml::XMLPropertyDict::const_iterator it =
-                        tagProps.find("engine");
-                    if (it != tagProps.end())
-                        if (parser && ! versionUsesUTF8(it->second.c_str()))
-                            parser->switchEncoding(XML_CHAR_ENCODING_8859_1);
-                }
             }
 
             virtual void abort(NXMLElementReader*) {
@@ -95,6 +79,21 @@ namespace {
                 }
             }
     };
+
+    /**
+     * The number of characters by which we expect to have finished the
+     * <reginadata ...> opening tag in an old pre-utf8 XML data stream.
+     */
+    const int regDataOpenBy = 200;
+
+    /**
+     * The size of the XML chunks that are sent through to libxml2 for parsing.
+     *
+     * Since we plan to perform surgery on pre-utf8 data streams, the chunk
+     * size must be large enough to catch the regina engine version in the
+     * first bite.  In particular, we require regChunkSize > regDataOpenBy.
+     */
+    const int regChunkSize = 1024;
 }
 
 bool writeXMLFile(const char* fileName, NPacket* subtree, bool compressed) {
@@ -119,7 +118,116 @@ NPacket* readXMLFile(const char* fileName) {
 
     ReginaDataReader reader;
     regina::NXMLCallback callback(reader, std::cerr);
-    regina::xml::XMLParser::parse_stream(callback, in);
+
+    // Instead of using the ready-made regina::xml::XMLParser::parse_stream(),
+    // we split the stream into parseable chunks manually.  This allows us to
+    // perform surgery on pre-utf8 streams and tell the parser that old files
+    // actually store their data in latin1 (which the old file format
+    // neglected to specify in the XML prologue).
+    {
+        regina::xml::XMLParser parser(callback);
+
+        char* buf = new char[regChunkSize];
+        int chunkRead;
+        bool seenFirstChunk = false;
+        while (true) {
+            // Read in the next chunk.
+            for (chunkRead = 0; chunkRead < regChunkSize; chunkRead++) {
+                buf[chunkRead] = in.get();
+                if (in.eof())
+                    break;
+            }
+            if (chunkRead == 0)
+                break;
+
+            // Parse the chunk that has just been read.
+            // Interesting things (i.e., surgery) can only happen in the
+            // first chunk.
+            if (seenFirstChunk) {
+                parser.parse_chunk(std::string(buf, chunkRead));
+                continue;
+            }
+
+            seenFirstChunk = true;
+
+            // This is the first chunk in the data stream.  See if we
+            // need to perform surgery upon it.
+            //
+            // We will be fairly rigid in looking for substrings, since
+            // we are only trying to match old pre-utf8 data files.  If
+            // future versions of the calculation engine change the
+            // opening "signature" then this does not matter, since they
+            // will not need surgery anyway.
+            //
+            // If anything looks out of place, just abort the surgery
+            // and send everything straight through to the XML parser,
+            // in the hope that either the file is new enough to not
+            // need surgery (i.e., it uses utf8 already) or it's old but
+            // only uses plain ASCII.
+
+            char tmp = buf[regDataOpenBy];
+            buf[regDataOpenBy] = 0;
+
+            char* start = ::strstr(buf, "<reginadata engine=\"");
+            if (! start) {
+                buf[regDataOpenBy] = tmp;
+                parser.parse_chunk(std::string(buf, chunkRead));
+                continue;
+            }
+            start += 20; /* Length of "<reginadata engine=\"" */
+
+            char* finish = ::strchr(start, '"');
+            if (finish == 0 || finish == start) {
+                // Never found a closing quote, or else the engine version
+                // string is empty.
+                buf[regDataOpenBy] = tmp;
+                parser.parse_chunk(std::string(buf, chunkRead));
+                continue;
+            }
+
+            // We've found the engine version.
+            buf[regDataOpenBy] = tmp;
+
+            tmp = *finish;
+            *finish = 0;
+
+            if (versionUsesUTF8(start)) {
+                // No need for surgery.
+                *finish = tmp;
+                parser.parse_chunk(std::string(buf, chunkRead));
+                continue;
+            }
+
+            // Surgery is required.
+            // Make sure there is somewhere to squeeze in an encoding.
+            start = ::strstr(buf, "encoding=");
+            if (start) {
+                // Someone else seems to have done it already.. *shrug*
+                *finish = tmp;
+                parser.parse_chunk(std::string(buf, chunkRead));
+                continue;
+            }
+
+            start = ::strstr(buf, "?>");
+            if (! start) {
+                // Can't find the end of the <?xml ...?> opening sequence!
+                *finish = tmp;
+                parser.parse_chunk(std::string(buf, chunkRead));
+                continue;
+            }
+
+            // Good to go.  Parse the chunk with a new encoding squeezed in
+            // just before the "?>".
+            *finish = tmp;
+
+            parser.parse_chunk(std::string(buf, start - buf));
+            parser.parse_chunk(" encoding=\"ISO-8859-1\"");
+            parser.parse_chunk(std::string(start, chunkRead - (start - buf)));
+        }
+
+        parser.finish();
+        delete[] buf;
+    }
 
     // See if we read anything.
     // If so, break it away from the top-level container and return it.
