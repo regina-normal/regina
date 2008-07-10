6b3417c465167bd8f9bf8bdf4e528351b777f60b
Benjamin Burton <bab@debian.org>
2008-07-10 12:41:07 +0000

    A wee bit of streamlining.

--- engine/maths/nfastvector.h
+++ engine/maths/nfastvector.h
@@ -108,7 +108,7 @@ class NFastVector {
          * @param newVectorSize the number of elements in the new
          * vector; this must be strictly positive.
          */
-        NFastVector(unsigned newVectorSize) :
+        inline NFastVector(unsigned newVectorSize) :
                 elements(new T[newVectorSize]), vectorSize(newVectorSize) {
         }
         /**
@@ -120,7 +120,7 @@ class NFastVector {
          * @param initValue the value to assign to every element of the
          * vector.
          */
-        NFastVector(unsigned newVectorSize, const T& initValue) :
+        inline NFastVector(unsigned newVectorSize, const T& initValue) :
                 elements(new T[newVectorSize]), vectorSize(newVectorSize) {
             std::fill(elements, elements + vectorSize, initValue);
         }
@@ -129,7 +129,7 @@ class NFastVector {
          *
          * @param cloneMe the vector to clone.
          */
-        NFastVector(const NFastVector<T>& cloneMe) :
+        inline NFastVector(const NFastVector<T>& cloneMe) :
                 elements(new T[cloneMe.vectorSize]),
                 vectorSize(cloneMe.vectorSize) {
             std::copy(cloneMe.elements, cloneMe.elements + vectorSize,
@@ -138,7 +138,7 @@ class NFastVector {
         /**
          * Destroys this vector.
          */
-        ~NFastVector() {
+        inline ~NFastVector() {
             delete[] elements;
         }
         /**
@@ -146,7 +146,7 @@ class NFastVector {
          *
          * @return the vector size.
          */
-        unsigned size() const {
+        inline unsigned size() const {
             return vectorSize;
         }
         /**
@@ -159,7 +159,7 @@ class NFastVector {
          * @param index the vector index to examine.
          * @return the vector element at the given index.
          */
-        const T& operator[](unsigned index) const {
+        inline const T& operator[](unsigned index) const {
             return elements[index];
         }
         /**
@@ -172,7 +172,7 @@ class NFastVector {
          * @param value the new value to assign to the element.
          * @return the vector element at the given index.
          */
-        void setElement(unsigned index, const T& value) {
+        inline void setElement(unsigned index, const T& value) {
             elements[index] = value;
         }
 
@@ -185,7 +185,7 @@ class NFastVector {
          * @return \c true if and only if the this and the given vector
          * are equal.
          */
-        bool operator == (const NFastVector<T>& compare) const {
+        inline bool operator == (const NFastVector<T>& compare) const {
             return std::equal(elements, elements + vectorSize,
                 compare.elements);
         }
@@ -197,7 +197,7 @@ class NFastVector {
          * @param cloneMe the vector whose value shall be assigned to this
          * vector.
          */
-        void operator = (const NFastVector<T>& cloneMe) {
+        inline void operator = (const NFastVector<T>& cloneMe) {
             std::copy(cloneMe.elements, cloneMe.elements + vectorSize,
                 elements);
         }
@@ -208,9 +208,11 @@ class NFastVector {
          *
          * @param other the vector to add to this vector.
          */
-        void operator += (const NFastVector<T>& other) {
-            for (unsigned i=0; i<vectorSize; i++)
-                elements[i] += other.elements[i];
+        inline void operator += (const NFastVector<T>& other) {
+            T* e = elements;
+            const T* o = other.elements;
+            for ( ; e < elements + vectorSize; ++e, ++o)
+                *e += *o;
         }
         /**
          * Subtracts the given vector from this vector.
@@ -219,20 +221,22 @@ class NFastVector {
          *
          * @param other the vector to subtract from this vector.
          */
-        void operator -= (const NFastVector<T>& other) {
-            for (unsigned i=0; i<vectorSize; i++)
-                elements[i] -= other.elements[i];
+        inline void operator -= (const NFastVector<T>& other) {
+            T* e = elements;
+            const T* o = other.elements;
+            for ( ; e < elements + vectorSize; ++e, ++o)
+                *e -= *o;
         }
         /**
          * Multiplies this vector by the given scalar.
          *
          * @param factor the scalar with which this will be multiplied.
          */
-        void operator *= (const T& factor) {
+        inline void operator *= (const T& factor) {
             if (factor == NFastVector<T>::one)
                 return;
-            for (unsigned i=0; i<vectorSize; i++)
-                elements[i] *= factor;
+            for (T* e = elements; e < elements + vectorSize; ++e)
+                *e *= factor;
         }
         /**
          * Calculates the dot product of this vector and the given vector.
@@ -242,18 +246,22 @@ class NFastVector {
          * @param other the vector with which this will be multiplied.
          * @return the dot product of this and the given vector.
          */
-        T operator * (const NFastVector<T>& other) const {
+        inline T operator * (const NFastVector<T>& other) const {
             T ans(zero);
-            for (unsigned i=0; i<vectorSize; i++)
-                ans += elements[i] * other.elements[i];
+
+            const T* e = elements;
+            const T* o = other.elements;
+            for ( ; e < elements + vectorSize; ++e, ++o)
+                ans += (*e) * (*o);
+
             return ans;
         }
         /**
          * Negates every element of this vector.
          */
-        void negate() {
-            for (unsigned i=0; i<vectorSize; i++)
-                elements[i] = -elements[i];
+        inline void negate() {
+            for (T* e = elements; e < elements + vectorSize; ++e)
+                *e = -*e;
         }
         /**
          * Returns the norm of this vector.
@@ -261,10 +269,10 @@ class NFastVector {
          *
          * @return the norm of this vector.
          */
-        T norm() const {
+        inline T norm() const {
             T ans(zero);
-            for (unsigned i=0; i<vectorSize; i++)
-                ans += elements[i] * elements[i];
+            for (const T* e = elements; e < elements + vectorSize; ++e)
+                ans += (*e) * (*e);
             return ans;
         }
         /**
@@ -272,10 +280,10 @@ class NFastVector {
          *
          * @return the sum of the elements of this vector.
          */
-        T elementSum() const {
+        inline T elementSum() const {
             T ans(zero);
-            for (unsigned i=0; i<vectorSize; i++)
-                ans += elements[i];
+            for (const T* e = elements; e < elements + vectorSize; ++e)
+                ans += *e;
             return ans;
         }
         /**
@@ -299,8 +307,10 @@ class NFastVector {
                 (*this) -= other;
                 return;
             }
-            for (unsigned i=0; i<vectorSize; i++)
-                elements[i] += other[i] * multiple;
+            T* e = elements;
+            const T* o = other.elements;
+            for ( ; e < elements + vectorSize; ++e, ++o)
+                *e += *o * multiple;
         }
         /**
          * Subtracts the given multiple of the given vector to this vector.
@@ -323,8 +333,10 @@ class NFastVector {
                 (*this) += other;
                 return;
             }
-            for (unsigned i=0; i<vectorSize; i++)
-                elements[i] -= other[i] * multiple;
+            T* e = elements;
+            const T* o = other.elements;
+            for ( ; e < elements + vectorSize; ++e, ++o)
+                *e -= *o * multiple;
         }
 };
 
