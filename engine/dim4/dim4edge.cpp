e833e951038562d5d97d7e0fa66491cc616e37da
Benjamin Burton <bab@debian.org>
2008-11-22 02:54:01 +0000

    Added Dim4Edge.

---
+++ engine/dim4/dim4edge.cpp
@@ -0,0 +1,53 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2008, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+#include "dim4/dim4edge.h"
+
+namespace regina {
+
+const int Dim4Edge::edgeNumber[5][5] = {
+    { -1, 0, 1, 2, 3 },
+    { 0, -1, 4, 5, 6 },
+    { 1, 4, -1, 7, 8 },
+    { 2, 5, 7, -1, 9 },
+    { 3, 6, 8, 9, -1 }};
+
+const int Dim4Edge::edgeVertex[10][2] = {
+    { 0, 1 },
+    { 0, 2 },
+    { 0, 3 },
+    { 0, 4 },
+    { 1, 2 },
+    { 1, 3 },
+    { 1, 4 },
+    { 2, 3 },
+    { 2, 4 },
+    { 3, 4 }};
+
+} // namespace regina
+
---
+++ engine/dim4/dim4edge.h
@@ -0,0 +1,376 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2008, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+/*! \file dim4edge.h
+ *  \brief Deals with edges in a 4-manifold triangulation.
+ */
+
+#ifndef __DIM4EDGE_H
+#ifndef __DOXYGEN
+#define __DIM4EDGE_H
+#endif
+
+#include <deque>
+#include "shareableobject.h"
+#include "dim4/dim4pentachoron.h"
+#include "dim4/nperm5.h"
+
+namespace regina {
+
+class Dim4Component;
+class Dim4BoundaryComponent;
+
+/**
+ * \weakgroup dim4
+ * @{
+ */
+
+/**
+ * Details how an edge in the skeleton of a 4-manifold triangulation forms
+ * part of an individual pentachoron.
+ */
+class Dim4EdgeEmbedding {
+    private:
+        Dim4Pentachoron* pent_;
+            /**< The pentachoron in which this edge is contained. */
+        int edge_;
+            /**< The edge number of the pentachoron that is this edge. */
+
+    public:
+        /**
+         * Default constructor.  The embedding descriptor created is
+         * unusable until it has some data assigned to it using
+         * <tt>operator =</tt>.
+         *
+         * \ifacespython Not present.
+         */
+        Dim4EdgeEmbedding();
+
+        /**
+         * Creates an embedding descriptor containing the given data.
+         *
+         * @param pent the pentachoron in which this edge is contained.
+         * @param edge the edge number of \a pent that is this edge.
+         */
+        Dim4EdgeEmbedding(Dim4Pentachoron* pent, int edge);
+
+        /**
+         * Creates an embedding descriptor containing the same data as
+         * the given embedding descriptor.
+         *
+         * @param cloneMe the embedding descriptor to clone.
+         */
+        Dim4EdgeEmbedding(const Dim4EdgeEmbedding& cloneMe);
+
+        /**
+         * Assigns to this embedding descriptor the same data as is
+         * contained in the given embedding descriptor.
+         *
+         * @param cloneMe the embedding descriptor to clone.
+         */
+        Dim4EdgeEmbedding& operator =(const Dim4EdgeEmbedding& cloneMe);
+
+        /**
+         * Returns the pentachoron in which this edge is contained.
+         *
+         * @return the pentachoron.
+         */
+        Dim4Pentachoron* getPentachoron() const;
+
+        /**
+         * Returns the edge number within getPentachoron() that is
+         * this edge.
+         *
+         * @return the edge number that is this edge.
+         */
+        int getEdge() const;
+
+        /**
+         * Returns a mapping from vertices (0,1) of this edge to the
+         * corresponding vertex numbers in getPentachoron(), as described
+         * in Dim4Pentachoron::getEdgeMapping().
+         *
+         * @return a mapping from the vertices of this edge to the
+         * vertices of getPentachoron().
+         */
+        NPerm5 getVertices() const;
+};
+
+/**
+ * Represents an edge in the skeleton of a 4-manifold triangulation.
+ * Edges are highly temporary; once a triangulation changes, all its
+ * edge objects will be deleted and new ones will be created.
+ */
+class Dim4Edge : public ShareableObject, public NMarkedElement {
+    public:
+        /**
+         * A table that maps vertices of a pentachoron to edge numbers.
+         *
+         * Edges in a pentachoron are numbered 0,...,9.  This table
+         * converts vertices to edge number; in particular, the edge
+         * joining vertices \a i and \a j of a pentachoron is
+         * edge number <tt>edgeNumber[i][j]</tt>.  Here \a i and \a j
+         * must be distinct, must be between 0 and 4 inclusive, and may
+         * be given in any order.  The resulting edge number will be
+         * between 0 and 9 inclusive.
+         *
+         * Note that edge \i is always opposite face \i in a pentachoron.
+         *
+         * This the 4-manifold analogue to the global array
+         * regina::edgeNumber for 3-manifold triangulations.
+         */
+        static const int edgeNumber[5][5];
+
+        /**
+         * A table that maps edges of a pentachoron to vertex numbers.
+         *
+         * Edges in a pentachoron are numbered 0,...,9.  This table
+         * converts edge numbers to vertices; in particular, edge \a i
+         * in a pentachoron joins vertices <tt>edgeVertex[i][0]</tt> and
+         * <tt>edgeVertex[i][1]</tt>.  Here \a i must be between 0 and 9
+         * inclusive; the resulting vertex numbers will be between 0 and 4
+         * inclusive.
+         *
+         * Note that edge \i is always opposite face \i in a pentachoron.
+         * It is guaranteed that <tt>edgeVertex[i][0]</tt> will always
+         * be smaller than <tt>edgeVertex[i][1]</tt>.
+         *
+         * This the 4-manifold analogue to the global arrays
+         * regina::edgeStart and regina::edgeEnd for 3-manifold triangulations.
+         */
+        static const int edgeVertex[10][2];
+
+    private:
+        std::deque<Dim4EdgeEmbedding> emb_;
+            /**< A list of descriptors telling how this edge forms a part of
+                 each individual pentachoron that it belongs to. */
+        Dim4Component* component_;
+            /**< The component that this edge is a part of. */
+        Dim4BoundaryComponent* boundaryComponent_;
+            /**< The boundary component that this edge is a part of,
+                 or 0 if this edge is internal. */
+        bool valid_;
+            /**< Is this edge valid? */
+
+    public:
+        /**
+         * Creates a new edge and marks it as belonging to the
+         * given triangulation component.
+         *
+         * \ifacespython Not present.
+         *
+         * @param component the triangulation component to which this
+         * edge belongs.
+         */
+        Dim4Edge(Dim4Component* component);
+
+        /**
+         * Default destructor.
+         */
+        ~Dim4Edge();
+
+        /**
+         * Returns the list of descriptors detailing how this edge forms a
+         * part of various pentachora in the 4-manifold triangulation.
+         * Note that if this edge represents multiple edges of a
+         * particular pentachoron, then there will be multiple embedding
+         * descriptors in the list regarding that pentachoron.
+         *
+         * \ifacespython This routine returns a python list.
+         *
+         * @return the list of embedding descriptors.
+         * @see Dim4EdgeEmbedding
+         */
+        const std::deque<Dim4EdgeEmbedding>& getEmbeddings() const;
+
+        /**
+         * Returns the number of descriptors in the list returned by
+         * getEmbeddings().  Note that this is identical to getDegree().
+         *
+         * @return the number of embedding descriptors.
+         */
+        unsigned long getNumberOfEmbeddings() const;
+
+        /**
+         * Returns the requested descriptor from the list returned by
+         * getEmbeddings().
+         *
+         * @param index the index of the requested descriptor.  This
+         * should be between 0 and getNumberOfEmbeddings()-1 inclusive.
+         * @return the requested embedding descriptor.
+         */
+        const Dim4EdgeEmbedding& getEmbedding(unsigned long index) const;
+
+        /**
+         * Returns the component of the triangulation to which this
+         * edge belongs.
+         *
+         * @return the component containing this edge.
+         */
+        Dim4Component* getComponent() const;
+
+        /**
+         * Returns the boundary component of the triangulation to which
+         * this edge belongs.
+         *
+         * @return the boundary component containing this edge, or 0 if this
+         * edge does not lie entirely within the boundary of the triangulation.
+         */
+        Dim4BoundaryComponent* getBoundaryComponent() const;
+
+        /**
+         * Returns the vertex of the 4-manifold triangulation corresponding
+         * to the given vertex of this edge.
+         *
+         * @param vertex the vertex of this edge to examine.  This should
+         * be 0 or 1.
+         * @return the corresponding vertex of the 4-manifold triangulation.
+         */
+        Dim4Vertex* getVertex(int vertex) const;
+
+        /**
+         * Returns the degree of this edge.  Note that this is identical
+         * to getNumberOfEmbeddings().
+         *
+         * @return the degree of this edge.
+         */
+        unsigned long getDegree() const;
+
+        /**
+         * Determines if this edge lies entirely on the boundary of the
+         * triangulation.
+         *
+         * @return \c true if and only if this edge lies on the boundary.
+         */
+        bool isBoundary() const;
+
+        /**
+         * Determines if this edge is valid.
+         * An edge is valid if and only if it is not glued to itself
+         * in reverse.
+         *
+         * @return \c true if and only if this edge is valid.
+         */
+        bool isValid() const;
+
+        void writeTextShort(std::ostream& out) const;
+
+    friend class Dim4Triangulation;
+        /**< Allow access to private members. */
+};
+
+/*@}*/
+
+// Inline functions for Dim4EdgeEmbedding
+
+inline Dim4EdgeEmbedding::Dim4EdgeEmbedding() : pent_(0) {
+}
+
+inline Dim4EdgeEmbedding::Dim4EdgeEmbedding(Dim4Pentachoron* pent, int edge) :
+        pent_(pent), edge_(edge) {
+}
+
+inline Dim4EdgeEmbedding::Dim4EdgeEmbedding(const Dim4EdgeEmbedding& cloneMe) :
+        pent_(cloneMe.pent_), edge_(cloneMe.edge_) {
+}
+
+inline Dim4EdgeEmbedding& Dim4EdgeEmbedding::operator =
+        (const Dim4EdgeEmbedding& cloneMe) {
+    pent_ = cloneMe.pent_;
+    edge_ = cloneMe.edge_;
+    return *this;
+}
+
+inline Dim4Pentachoron* Dim4EdgeEmbedding::getPentachoron() const {
+    return pent_;
+}
+
+inline int Dim4EdgeEmbedding::getEdge() const {
+    return edge_;
+}
+
+inline NPerm5 Dim4EdgeEmbedding::getVertices() const {
+    return pent_->getEdgeMapping(edge_);
+}
+
+// Inline functions for Dim4Edge
+
+inline Dim4Edge::Dim4Edge(Dim4Component* component) :
+        component_(component), boundaryComponent_(0), valid_(true) {
+}
+
+inline Dim4Edge::~Dim4Edge() {
+}
+
+inline const std::deque<Dim4EdgeEmbedding>& Dim4Edge::getEmbeddings() const {
+    return emb_;
+}
+
+inline unsigned long Dim4Edge::getNumberOfEmbeddings() const {
+    return emb_.size();
+}
+
+inline const Dim4EdgeEmbedding& Dim4Edge::getEmbedding(unsigned long index)
+        const {
+    return emb_[index];
+}
+
+inline Dim4Component* Dim4Edge::getComponent() const {
+    return component_;
+}
+
+inline Dim4BoundaryComponent* Dim4Edge::getBoundaryComponent() const {
+    return boundaryComponent_;
+}
+
+inline Dim4Vertex* Dim4Edge::getVertex(int vertex) const {
+    const Dim4EdgeEmbedding& e(emb_.front());
+    return e.getPentachoron()->getVertex(e.getVertices()[vertex]);
+}
+
+inline unsigned long Dim4Edge::getDegree() const {
+    return emb_.size();
+}
+
+inline bool Dim4Edge::isBoundary() const {
+    return (boundaryComponent_ != 0);
+}
+
+inline bool Dim4Edge::isValid() const {
+    return valid_;
+}
+
+inline void Dim4Edge::writeTextShort(std::ostream& out) const {
+    out << (boundaryComponent_ ? "Boundary " : "Internal ")
+        << "edge of degree " << emb_.size();
+}
+
+} // namespace regina
+
+#endif
+
