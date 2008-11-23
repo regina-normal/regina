96a7af7933f53503868ca63d9c430adae25aff06
Benjamin Burton <bab@debian.org>
2008-11-23 19:59:33 +0000

    Be a little less aggressive -- we call NTetrahedron::getFace, not NTriangulation::getFace.  Calculate the skeleton.

--- engine/triangulation/simplify.cpp
+++ engine/triangulation/simplify.cpp
@@ -688,9 +688,10 @@ bool NTriangulation::openBook(NFace* f, bool check, bool perform) {
 bool NTriangulation::shellBoundary(NTetrahedron* t,
         bool check, bool perform) {
     // To perform the move we don't even need a skeleton.
-    // To check the move, the initial calls to getFace() will ensure
-    // that the skeleton is calculated.
     if (check) {
+        if (! calculatedSkeleton)
+            calculateSkeleton();
+
         int nBdry = 0;
         int face;
         int bdry[4];
