0cd4e66ae8fe516fac3a729fe3abc2809dfccd9e
Ryan Budney <rybu@uvic.ca>
2009-05-05 08:42:38 +0000

    Undo previous edit.

--- engine/dim4/dim4triangulation.h
+++ engine/dim4/dim4triangulation.h
@@ -55,7 +55,7 @@ class NXMLDim4TriangulationReader;
 class NXMLPacketReader;
 
 /**
- * \addtogroup dim4 4-Manifold
+ * \addtogroup dim4 4-Manifold Triangulation
  * Triangulations of 4-manifolds.
  * @{
  */
