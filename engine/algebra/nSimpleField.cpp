9435ec953c56e4b34045cb4ee54b9964f32ce4db
Ryan Budney <rybu@uvic.ca>
2009-05-07 21:25:50 +0000

    super-primitive NSimpleField class.  Much to do!

---
+++ engine/algebra/nSimpleField.cpp
@@ -0,0 +1,173 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2009, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+#include "algebra/nSimpleField.h"
+#include <iostream>
+#include "maths/nmatrix.h"
+
+namespace regina {
+
+/**
+ * Assignment 
+ */
+template <class T>
+NSimpleField<T> NSimpleField<T>::operator = (const NSimpleField<T>& other)
+ {
+ baseField = other.baseField; coeff = other.coeff; 
+ return (*this);
+ }
+
+/**
+  * Creates a clone of the given field with all 
+  *  elements initialized to zero if zero = true, otherwise initialized
+  *  to one.
+  *
+  * @param cloneMe the group to clone.
+  */
+template <class T>
+NSimpleField<T>::NSimpleField(const NSimpleField<T>& cloneMe, bool zero) :
+        ShareableObject(), baseField(cloneMe.baseField), coeff(cloneMe.coeff)
+{
+ for (int i=0; i<coeff.size(); i++) coeff[i]=0;
+}
+
+
+/**
+ * The zero element of the given field.
+ */
+template <class T>
+NSimpleField<T> NSimpleField<T>::zero()
+{
+}
+
+/**
+ * The identity element of the given field.
+ */
+template <class T>
+NSimpleField<T> NSimpleField<T>::one()
+{
+}
+
+/**
+ * Addition of field elts
+ */
+template <class T>
+NSimpleField<T> NSimpleField<T>::operator + (const NSimpleField<T>& other) const
+{
+ NSimpleField<T> temp( other );
+ for (int i=0; i<coeff.size(); i++) temp.coeff[i] += coeff[i];
+ return temp;
+}
+
+/**
+ * Multiplication of field elts.
+ */
+template <class T>
+NSimpleField<T> NSimpleField<T>::operator * (const NSimpleField<T>& other) const
+{
+ NSimpleField<T> temp(other, true); // zero element
+
+ NMatrix<T> redMat(baseField.size(),baseField.size()-1); // computed in step 1
+
+   // the column vectors will be representations of x^{n+j} wrt 1, x, ..., x^{n-1}
+   // n=baseField.size()
+
+  // step 1: compute reduction matrix
+
+  for (int i=0; i<redMat.rows(); i++) redMat.entry(i,0) = baseField[i];
+
+  for (int i=1; i<redMat.columns(); i++)
+   { // shift entry(j,i) <-- (j-1,i-1)...
+    for (int j=1; j<redMat.rows(); j++)
+	redMat.entry(j,i) = redMat.entry(j-1,i-1); 
+    for (int j=0; j<redMat.rows(); j++)
+	redMat.entry(j,i) += redMat.entry(baseField.size()-1, i-1)*redMat.entry(j,0);
+   }
+
+  // step 2: compute sum_k sum_{i+j=k} a_ib_j x^k
+  std::vector<T> polySum(redMat.columns()*2, 0);
+  for (int k=0; k<polySum.size(); k++)
+	for (int i=0; i<k+1; i++) polySum[k] += coeff[i]*other.coeff[k-i];
+
+  // step 3: reduce 
+  for (int k=baseField.size(); k<polySum.size(); k++)
+	{
+	for (int j=0; j<redMat.columns(); j++) // coeffs of x^{n+j} in the product
+	  for (int i=0; i<baseField.size(); i++) // component iterator
+		polySum[i] += polySum[baseField.size()+j]*redMat.entry(i,j);
+	}
+
+  return temp;
+}
+
+/**
+ * Multiplicative inverse
+ */
+template <class T>
+NSimpleField<T> NSimpleField<T>::inverse() const
+{
+ NSimpleField<T> temp(*this);
+
+ // not implemented yet
+
+ return temp;
+}
+
+/**
+ * Vector space ops
+ */
+template <class T>
+NSimpleField<T> NSimpleField<T>::operator * (const T& k) const
+{
+// not implemented yet
+}
+
+/**
+ * Vector space ops
+ */
+template <class TT>
+NSimpleField<TT> operator * (const TT& k, const NSimpleField<TT>& other)
+{
+// not implemented yet
+}
+
+
+template <class T>
+void NSimpleField<T>::writeTextShort(std::ostream& out) const 
+{
+ for (int i=0; i<coeff.size(); i++)
+	{
+	out<<coeff[i]<<"x^"<<i;
+	if (i<coeff.size()-1) out<<"+"; else out<<" ";
+	}
+}
+
+
+
+} // namespace regina
+
---
+++ engine/algebra/nSimpleField.h
@@ -0,0 +1,203 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2009, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+/*! \file nSimpleField.h
+ *  \brief Deals with simple field/ring extensions K[x]/p(x)
+ */
+
+#ifndef __NSIMPLEFIELD_H
+#ifndef __DOXYGEN
+#define __NSIMPLEFIELD_H
+#endif
+
+#include "shareableobject.h"
+#include <vector>
+#include <iostream>
+
+//#include "utilities/nmpi.h"
+
+namespace regina {
+
+/**
+ * \weakgroup algebra
+ * @{
+ */
+
+/**
+ * Represents elements in an elementary number field -- a polynomial ring
+ * K[x]/p(x) where p(x) is some monic polynomial in the polynomial ring K[x],
+ * and K is an arbitrary templated field. We assume that K
+ *
+ * @author Ryan Budney
+ *
+ */
+
+template <class T>
+class NSimpleField : public ShareableObject {
+    private:
+	/**
+	 * NsimpleField class -- represents elements in a simple extension field: K[x]/p(x).
+	 * 
+	 * Requirements: K must be a field, so it needs basic arithmatic plus
+	 *  an element called zero, and all non-zero elements need a multiplicative
+	 *  inverse.  K[x]/p(x) is always a ring, but if you want inverses to be
+	 *  defined, you need p(x) to be prime in K[x].  K needs to cast 0.
+	 *
+	 * Restrictions: we'll use unsigned long integers for the exponents in the
+	 *  polynomials, so the polynomial you initialize with can have degree at
+	 *  most the largest unsigned long.  The field is templated so you get
+	 *  whatever you give, there.  This class is designed to work in Regina
+	 *  minimally with the NRational class.
+	 *
+	 * we will have static members that keep track of 
+	 * relevant data for efficient multiplication and inversion of elements
+	 * in the number field.
+	 *
+	 * Initialize class with monic polynomial p(x) = x^n - a_{n-1}x^{n-1} - ... - a_0
+	 *  the polynomial will be stored as a vector (a_0, a_1, ..., a_{n-1}) ie, in
+	 *  K[x]/p(x) we have the rule that x^n = a_0 + a_1 x + ... + a_{n-1}x^{n-1}
+	 *
+	 * So on initialization there is some static data we need to compute, in order
+	 *  to make multiplication efficient.  x^n, x^{n+1}, ..., x^{2n-2}
+	 *  to be written in terms of 1, x, x^2, ..., x^{n-1}.
+	 */ 
+
+
+	//static NMatrixRing < T > redDat; // reduction data for multiplication
+	std::vector<T> baseField; // temp representation of the base field
+	std::vector<T> coeff; // rep of element in the field
+
+    public:
+
+        /**
+	 * General constructor.
+         */
+        NSimpleField(std::vector<T> bF, std::vector<T> cf);
+
+        /**
+         * Creates a clone of the given field/element.
+         *
+         * @param cloneMe the group to clone.
+         */
+        NSimpleField(const NSimpleField<T>& cloneMe);
+
+        /**
+         * Creates a clone of the given field with all 
+	 *  elements initialized to zero if zero = true, otherwise initialized
+	 *  to one.
+         *
+         * @param cloneMe the group to clone.
+         */
+        NSimpleField(const NSimpleField<T>& cloneMe, bool zero);
+
+        /**
+         * Destroys the field/element.
+         */
+        ~NSimpleField();
+
+	/**
+	 * The zero element of the given field.
+	 */
+	NSimpleField<T> zero();
+
+	/**
+	 * The identity element of the given field.
+	 */
+	NSimpleField<T> one();
+
+        /**
+         * Determines whether this and the given quaternion are equal.
+         */
+        bool operator == (const NSimpleField<T>& other) const;
+
+	/**
+	 * Assignment of quaternions.
+	 */
+	NSimpleField<T> operator = (const NSimpleField<T>& other);
+	/**
+	 * Addition of quaternions.
+	 */
+	NSimpleField<T> operator + (const NSimpleField<T>& other) const;
+	/**
+	 * Multiplication of quaternions.
+	 */
+	NSimpleField<T> operator * (const NSimpleField<T>& other) const;
+	/**
+	 * Multiplicative inverse
+	 */
+	NSimpleField<T> inverse() const;
+	/**
+	 * Vector space ops
+	 */
+	NSimpleField<T> operator * (const T& k) const;
+
+       /**
+	 * Writes the quaternionic integer to a stream in human-readable form.
+         *
+         * @param out the stream to write to.
+         */
+        void writeTextShort(std::ostream& out) const;
+
+	/**
+	 * Vector space ops
+	 */
+	template <class TT>
+	friend NSimpleField<TT> operator*(const TT& k, const NSimpleField<TT>& other);
+
+};
+
+
+/*@}*/
+
+// constructor
+template <class T>
+inline NSimpleField<T>::NSimpleField(std::vector<T> bF, std::vector<T> cf) :
+ ShareableObject(), baseField(bF), coeff(cf) {}
+
+// copy constructor
+template <class T>
+inline NSimpleField<T>::NSimpleField(const NSimpleField<T>& g) :
+        ShareableObject(), baseField(g.baseField), coeff(g.coeff) {}
+
+// destructor
+template <class T>
+inline NSimpleField<T>::~NSimpleField() {}
+
+// equality
+template <class T>
+inline bool NSimpleField<T>::operator == (
+        const NSimpleField<T>& other) const {
+    return ((baseField==other.baseField) && (coeff==other.coeff) );
+}
+
+
+
+} // namespace regina
+
+#endif
+
