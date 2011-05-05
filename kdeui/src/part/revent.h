5d2bf462f1748cb4254007e30cc382fc9af4ae85
William Pettersson <william.pettersson@gmail.com>
2011-05-05 03:39:05 +0000

    Added QEvent subclass, required for Qt4

---
+++ kdeui/src/part/revent.h
@@ -0,0 +1,64 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  KDE User Interface                                                    *
+ *                                                                        *
+ *  Copyright (c) 1999-2009, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+/*! \file revent.h
+ *  \brief Provides a required custom event.
+ */
+
+#ifndef __REVENT_H
+#define __REVENT_H
+
+#include <QEvent>
+
+
+class REvent;
+
+
+/**
+ * A single item in a Regina packet tree.
+ */
+class REvent : public QEvent {
+    private:
+
+        /**
+         * The packet item.
+         */
+        PacketTreeItem* item;
+
+
+    public:
+        PacketTreeItem* getItem();
+
+};
+
+
+inline PacketTreeItem* REvent::getItem() {
+    return item;
+}
+
+#endif
