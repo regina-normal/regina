0de70dc3d91deebe073f6a9905c878d90fd676b1
Benjamin Burton <bab@debian.org>
2011-05-15 21:35:57 +0000

    Build fixes for MacOSX/cmake.

--- python/maths/nperm5.cpp
+++ python/maths/nperm5.cpp
@@ -26,6 +26,10 @@
 
 /* end stub */
 
+// We need to see Python.h first to avoid a "portability fix" in pyport.h
+// that breaks boost.python on MacOSX.
+#include "Python.h"
+
 #include "maths/nperm5.h"
 #include "../globalarray.h"
 #include <boost/python.hpp>
