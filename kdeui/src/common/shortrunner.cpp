e977ee02a2fb5bb1f5cd56fae8a02322ba0f2930
Benjamin Burton <bab@debian.org>
2011-08-28 21:02:08 +0000

    Very careful go-over ShortRunner to make sure we are doing the right thing by QProcess/KProcess.  Complications arise because waitForFinished() does not distinguish between timeout and already-finished. :/

--- kdeui/src/common/shortrunner.cpp
+++ kdeui/src/common/shortrunner.cpp
@@ -32,23 +32,47 @@ QString ShortRunner::run(bool mergeStderr) {
     // Start the child process running.
     proc.setOutputChannelMode(mergeStderr ?
         KProcess::MergedChannels : KProcess::OnlyStdoutChannel);
+    connect(&proc, SIGNAL(started()), this, SLOT(processStarted()));
+    connect(&proc, SIGNAL(finished(int, QProcess::ExitStatus)),
+        this, SLOT(processFinished()));
+
     // Since we need to collect output, we must use start() and not
     // startDetached().
     proc.start();
 
     // Wait for it to finish, within a reasonable time limit.
-    if (proc.waitForFinished(timeout * 1000)) {
-        // All good.
-        return proc.readAll();
-    } else {
-        // Timed out.
-        // Attempt to terminate (SIGQUIT) ...
-        proc.terminate();
-        // ... and if the program doesn't respond then kill it (SIGKILL).
-        proc.waitForFinished(500);
-        proc.kill();
-        reachedTimeout = true;
-        return QString::null;
+    // Don't rely on the return value, since waitForFinished() can
+    // also return false if the program already finished before this call.
+    proc.waitForFinished(timeout * 1000);
+    {
+        QMutexLocker locker(&mutex);
+        if (finished) {
+            // All good.
+            return proc.readAll();
+        } else if (started)
+            reachedTimeout = true;
     }
+
+    // Bzzt.
+    // Either we timed out or we never even started.  The member
+    // reachedTimeout that we set above allows us to tell the difference.
+
+    // Attempt to terminate (SIGQUIT) ...
+    proc.terminate();
+    // ... and if the program doesn't respond then kill it (SIGKILL).
+    proc.waitForFinished(500);
+    proc.kill();
+
+    return QString::null;
+}
+
+void ShortRunner::processStarted() {
+    QMutexLocker locker(&mutex);
+    started = true;
+}
+
+void ShortRunner::processFinished() {
+    QMutexLocker locker(&mutex);
+    finished = true;
 }
 
