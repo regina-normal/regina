197b50db9e6652dadea645d6a4023fbf927dc1e1
Benjamin Burton <bab@debian.org>
2013-08-14 23:25:43 +0000

    Allow portions of the engine to be excised.

--- testsuite/runalltests.cpp
+++ testsuite/runalltests.cpp
@@ -26,6 +26,8 @@
 
 /* end stub */
 
+#include "regina-config.h" // For EXCLUDE_SNAPPEA
+
 #include <cctype>
 #include <iostream>
 #include <cppunit/Test.h>
@@ -35,7 +37,9 @@
 #include "testsuite/angle/testangle.h"
 #include "testsuite/census/testcensus.h"
 #include "testsuite/maths/testmaths.h"
+#ifndef EXCLUDE_SNAPPEA
 #include "testsuite/snappea/testsnappea.h"
+#endif
 #include "testsuite/subcomplex/testsubcomplex.h"
 #include "testsuite/surfaces/testsurfaces.h"
 #include "testsuite/triangulation/testtriangulation.h"
@@ -153,8 +157,10 @@ bool runAllTests() {
     addDim2Census(runner);
     addDim2EdgePairing(runner);
 
+#ifndef EXCLUDE_SNAPPEA
     // SnapPea:
     addNSnapPeaTriangulation(runner);
+#endif
 
     /**
      * END REGINA TEST SUITES
