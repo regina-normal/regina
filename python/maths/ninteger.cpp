d6547a171e6612f2bb6516d43b634ec0f4ee1cd4
Benjamin Burton <bab@debian.org>
2013-08-20 00:07:22 +0000

    Update python bindings for the two integer classes.

--- python/maths/ninteger.cpp
+++ python/maths/ninteger.cpp
@@ -33,25 +33,35 @@ using namespace boost::python;
 using regina::NInteger;
 
 namespace {
-    NInteger& (NInteger::*divByExact_lazy)(const NInteger&) =
+    NInteger& (NInteger::*divByExact_large)(const NInteger&) =
         &NInteger::divByExact;
     NInteger& (NInteger::*divByExact_long)(long) =
         &NInteger::divByExact;
-    NInteger (NInteger::*divExact_lazy)(const NInteger&)
-        const = &NInteger::divExact;
+    NInteger (NInteger::*divExact_large)(const NInteger&) const =
+        &NInteger::divExact;
     NInteger (NInteger::*divExact_long)(long) const =
         &NInteger::divExact;
 
     BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(OL_stringValue,
         NInteger::stringValue, 0, 1);
+
+    boost::python::tuple divisionAlg(const NInteger& n,
+            const NInteger& divisor) {
+        NInteger remainder;
+        NInteger quotient = n.divisionAlg(divisor, remainder);
+        return make_tuple(quotient, remainder);
+    }
 }
 
 void addNInteger() {
     scope s = class_<NInteger>("NInteger")
         .def(init<long>())
         .def(init<const NInteger&>())
+        .def(init<const regina::NLargeInteger&>())
         .def(init<const char*, optional<int> >())
         .def("isNative", &NInteger::isNative)
+        .def("isZero", &NInteger::isZero)
+        .def("isInfinite", &NInteger::isInfinite)
         .def("longValue", &NInteger::longValue)
         .def("stringValue", &NInteger::stringValue, OL_stringValue())
         .def("swap", &NInteger::swap)
@@ -75,10 +85,11 @@ void addNInteger() {
         .def(self * long())
         .def(self / self)
         .def(self / long())
-        .def("divExact", divExact_lazy)
+        .def("divExact", divExact_large)
         .def("divExact", divExact_long)
         .def(self % self)
         .def(self % long())
+        .def("divisionAlg", divisionAlg)
         .def(- self)
         .def(self += self)
         .def(self += long())
@@ -88,7 +99,7 @@ void addNInteger() {
         .def(self *= long())
         .def(self /= self)
         .def(self /= long())
-        .def("divByExact", divByExact_lazy, return_internal_reference<>())
+        .def("divByExact", divByExact_large, return_internal_reference<>())
         .def("divByExact", divByExact_long, return_internal_reference<>())
         .def(self %= self)
         .def(self %= long())
@@ -97,9 +108,18 @@ void addNInteger() {
         .def("abs", &NInteger::abs)
         .def("gcd", &NInteger::gcd)
         .def("lcm", &NInteger::lcm)
+        .def("gcdWithCoeffs", &NInteger::gcdWithCoeffs)
+        .def("legendre", &NInteger::legendre)
+        //.def("randomBoundedByThis", &NInteger::randomBoundedByThis)
+        //.def("randomBinary", &NInteger::randomBinary)
+        //.def("randomCornerBinary", &NInteger::randomCornerBinary)
         .def("makeLarge", &NInteger::makeLarge)
         .def("tryReduce", &NInteger::tryReduce)
+        .def(long() + self)
+        .def(long() * self)
         .def(self_ns::str(self))
+        //.staticmethod("randomBinary")
+        //.staticmethod("randomCornerBinary")
     ;
 
     // Apparently there is no way in python to make a module attribute
