7669b49c26d46a1a521dd57563592953fcd70ac3
Benjamin Burton <bab@debian.org>
2013-08-21 23:51:24 +0000

    Add a license clause to allow distribution through online application repositories, such as the App Store and Google Play.

--- python/triangulation/pytriangulation.cpp
+++ python/triangulation/pytriangulation.cpp
@@ -4,13 +4,19 @@
  *  Regina - A Normal Surface Theory Calculator                           *
  *  Python Interface                                                      *
  *                                                                        *
- *  Copyright (c) 1999-2011, Ben Burton                                   *
+ *  Copyright (c) 1999-2013, Ben Burton                                   *
  *  For further details contact Ben Burton (bab@debian.org).              *
  *                                                                        *
  *  This program is free software; you can redistribute it and/or         *
  *  modify it under the terms of the GNU General Public License as        *
  *  published by the Free Software Foundation; either version 2 of the    *
  *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  As an exception, when this program is distributed through (i) the     *
+ *  App Store by Apple Inc.; (ii) the Mac App Store by Apple Inc.; or     *
+ *  (iii) Google Play by Google Inc., then that store may impose any      *
+ *  digital rights management, device limits and/or redistribution        *
+ *  restrictions that are required by its terms of service.               *
  *                                                                        *
  *  This program is distributed in the hope that it will be useful, but   *
  *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
