6f14173961415abe19cc21b8838a5b6ed6e9c16a
Benjamin Burton <bab@debian.org>
2013-08-24 12:49:28 +0000

    Go over the new tree traversal code to incorporate into the source style / conventions used in Regina.

--- timing/tree.cc
+++ timing/tree.cc
@@ -31,6 +31,7 @@
  **************************************************************************/
 
 #include <enumerate/ntreetraversal.h>
+#include <triangulation/ntriangulation.h>
 
 using namespace regina;
 
