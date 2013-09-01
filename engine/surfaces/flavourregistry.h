4d8112a923302924259aad6ebe6c4cff10afe61f
Benjamin Burton <bab@debian.org>
2013-09-01 23:20:53 +0000

    Rework the flavour registery to use template-based mechanisms instead of preprocessor abuse.

--- engine/surfaces/flavourregistry.h
+++ engine/surfaces/flavourregistry.h
@@ -45,47 +45,178 @@
  *        system that can actually be used to create and store normal
  *        surfaces.
  *
- *    For each such flavour there should be a line of the form:
+ *    For each such flavour there should be:
  *
- *        REGISTER_FLAVOUR(id_name, class, name, almost_normal, spun, oriented)
+ *    (1) an #include line for the corresponding vector subclass;
  *
- *    where:
- *        id_name = the enum value from NormalCoords that represents this
- *            flavour.
+ *    (2) a template specialisation NormalFlavour<id>, where id is the
+ *        corresponding NormalCoords enum value;
+ *
+ *    (3) a corresponding case in both implementations of forFlavour();
+ *
+ *    (4) a macro REGISTER_FLAVOUR(id, class, name, an, spun, oriented), where:
+ *        id = the enum value from NormalCoords that represents this flavour;
  *        class = the C++ subclass of NNormalSurfaceVector that uses this
- *            coordinate system.
+ *            coordinate system;
  *        name = the string (English) name of the new flavour.
- *        almost_normal = true if this coordinate system allows almost
- *            normal surfaces or false otherwise.
+ *        an = true if this coordinate system allows almost normal surfaces or
+ *            false otherwise;
  *        spun = true if this coordinate system allows spun normal surfaces
- *            or false otherwise.
+ *            or false otherwise;
  *        oriented = true if this coordinate system supports transversely
  *            oriented normal surfaces or false otherwise.
  *
- *    The appropriate include files should also be placed in the
- *        appropriate include sections below.
+ *    There are two ways to use this flavour registry.
  *
- *    To use the flavour registry, simply #include this file.  If you
- *        have not defined __FLAVOUR_REGISTRY_BODY, only the include
- *        sections will be brought in.  If you have defined
- *        __FLAVOUR_REGISTRY_BODY, the include sections will be skipped
- *        and the REGISTER_FLAVOUR lines will be brought in instead.
- *        By suitably defining the macro REGISTER_FLAVOUR before including
- *        this file, you can have these lines do whatever you wish.
+ *    (1) The preferred way is to #include this file once, and then to use
+ *        the forFlavour() template functions to do any processing that
+ *        requires iterating through cases according to the flavour of
+ *        coordinate system.
  *
- *    If you do #include this file, be sure to #undef both
- *        REGISTER_FLAVOUR and __FLAVOUR_REGISTRY_BODY when you are
- *        finished, so that if --enable-final is being used then these
- *        macros are left undefined for the following files.
+ *    (2) The old way involves the REGISTER_FLAVOUR macros.  This old
+ *        way is now deprecated, and the REGISTER_FLAVOUR macros will be
+ *        abolished in some future verison of Regina.  To use the old way,
+ *        you must #include this file several times within your source code.
+ *        The first time, you shoud not have defined __FLAVOUR_REGISTRY_BODY;
+ *        this will bring in everything except for the REGISTER_FLAVOUR lines.
+ *        After this you should define __FLAVOUR_REGISTRY_BODY; each
+ *        subsequent #include will bring in only the REGISTER_FLAVOUR lines,
+ *        and you can define the REGISTER_FLAVOUR macro to do whatever
+ *        processing you like.  Be sure to #undef both REGISTER_FLAVOUR and
+ *        __FLAVOUR_REGISTRY_BODY when you are finished, so that if
+ *        --enable-final is being used then these macros are left undefined
+ *        for subsequent source files.
  */
 
 #ifndef __FLAVOUR_REGISTRY_BODY
+    #include "surfaces/normalcoords.h"
     #include "surfaces/nsstandard.h"
     #include "surfaces/nsanstandard.h"
     #include "surfaces/nsquad.h"
     #include "surfaces/nsquadoct.h"
     #include "surfaces/nsoriented.h"
     #include "surfaces/nsorientedquad.h"
+
+namespace regina {
+
+    template <int flavourType>
+    struct NormalFlavour;
+
+    template <>
+    struct NormalFlavour<NS_STANDARD> {
+        typedef NNormalSurfaceVectorStandard Vector;
+        inline static const char* name() {
+            return "Standard normal (tri-quad)";
+        }
+        enum {
+            almostNormal = 0,
+            spun = 0,
+            oriented = 0
+        };
+    };
+
+    template <>
+    struct NormalFlavour<NS_AN_STANDARD> {
+        typedef NNormalSurfaceVectorANStandard Vector;
+        inline static const char* name() {
+            return "Standard almost normal (tri-quad-oct)";
+        }
+        enum {
+            almostNormal = 1,
+            spun = 0,
+            oriented = 0
+        };
+    };
+
+    template <>
+    struct NormalFlavour<NS_QUAD> {
+        typedef NNormalSurfaceVectorQuad Vector;
+        inline static const char* name() {
+            return "Quad normal";
+        }
+        enum {
+            almostNormal = 0,
+            spun = 1,
+            oriented = 0
+        };
+    };
+
+    template <>
+    struct NormalFlavour<NS_AN_QUAD_OCT> {
+        typedef NNormalSurfaceVectorQuadOct Vector;
+        inline static const char* name() {
+            return "Quad-oct almost normal";
+        }
+        enum {
+            almostNormal = 1,
+            spun = 1,
+            oriented = 0
+        };
+    };
+
+    template <>
+    struct NormalFlavour<NS_ORIENTED> {
+        typedef NNormalSurfaceVectorOriented Vector;
+        inline static const char* name() {
+            return "Transversely oriented standard normal";
+        }
+        enum {
+            almostNormal = 0,
+            spun = 0,
+            oriented = 1
+        };
+    };
+
+    template <>
+    struct NormalFlavour<NS_ORIENTED_QUAD> {
+        typedef NNormalSurfaceVectorOrientedQuad Vector;
+        inline static const char* name() {
+            return "Transversely oriented quad normal";
+        }
+        enum {
+            almostNormal = 0,
+            spun = 1,
+            oriented = 1
+        };
+    };
+
+    template <typename ReturnType>
+    struct FlavourFunction {
+        typedef ReturnType Return;
+    };
+
+    template <typename ProcedureObject>
+    inline void forFlavour(NormalCoords flavour, ProcedureObject proc) {
+        switch (flavour) {
+            case NS_STANDARD : proc(NormalFlavour<NS_STANDARD>()); break;
+            case NS_AN_STANDARD : proc(NormalFlavour<NS_AN_STANDARD>()); break;
+            case NS_QUAD : proc(NormalFlavour<NS_QUAD>()); break;
+            case NS_AN_QUAD_OCT : proc(NormalFlavour<NS_AN_QUAD_OCT>()); break;
+            case NS_ORIENTED : proc(NormalFlavour<NS_ORIENTED>()); break;
+            case NS_ORIENTED_QUAD :
+                proc(NormalFlavour<NS_ORIENTED_QUAD>()); break;
+            default: break;
+        }
+    }
+
+    template <typename FunctionObject>
+    inline typename FunctionObject::Return forFlavour(
+            NormalCoords flavour, FunctionObject func,
+            typename FunctionObject::Return defaultReturn) {
+        switch (flavour) {
+            case NS_STANDARD : return func(NormalFlavour<NS_STANDARD>());
+            case NS_AN_STANDARD : return func(NormalFlavour<NS_AN_STANDARD>());
+            case NS_QUAD : return func(NormalFlavour<NS_QUAD>());
+            case NS_AN_QUAD_OCT : return func(NormalFlavour<NS_AN_QUAD_OCT>());
+            case NS_ORIENTED : return func(NormalFlavour<NS_ORIENTED>());
+            case NS_ORIENTED_QUAD :
+                return func(NormalFlavour<NS_ORIENTED_QUAD>());
+            default: return defaultReturn;
+        }
+    }
+
+} // namespace regina
+
 #else
     REGISTER_FLAVOUR(NS_STANDARD, NNormalSurfaceVectorStandard,
         "Standard normal (tri-quad)", false, false, false)
