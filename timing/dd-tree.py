02ea4da3f23cbfd9ae57f3edaa4c263909edf380
Benjamin Burton <bab@debian.org>
2013-09-07 23:07:55 +0000

    Support progress tracking.

--- timing/dd-tree.py
+++ timing/dd-tree.py
@@ -1,20 +1,30 @@
 import time
 
+progress = 1
+
 sigs = [ 'tLAwLvLLMvQQQkcbbdfkonmlnroqprqqsshqnhjpiapvsvnjrgfohk',
          'uLAMLvvLzLQMQQcbcbdegmlopoqnrprsssttxxjxxjlldarjggsckofxr',
          'CLAMzLvvPwzALQwvQQQkbcbdefhkmnorpvvtsrywzwzBABBAAhhjhhhkfldtgwxapcbwvwswihlili',
          'xvLvvvwMvQPPQQQQcehpjtqksntrtvoupwpsuwsvwcgacalvucahatbhapaggjgfk' ]
 
+def enum(t, coords, which, alg):
+    start = time.clock()
+    if progress:
+        m = NProgressManager()
+        s = NNormalSurfaceList.enumerate(t, coords, which, alg, m)
+        while not m.isStarted():
+            time.sleep(0.3)
+        while not m.isFinished():
+            print m.getProgress().getPercent(), '%'
+            time.sleep(0.3)
+    else:
+        s = NNormalSurfaceList.enumerate(t, coords, which, alg)
+    print s
+    print time.clock() - start
+
 for sig in sigs:
     print sig
     t = NTriangulation.fromIsoSig(sig)
 
-    start = time.clock()
-    s = NNormalSurfaceList.enumerate(t, NS_QUAD, NS_VERTEX)
-    print s
-    print time.clock() - start
-
-    start = time.clock()
-    u = NNormalSurfaceList.enumerate(t, NS_QUAD, NS_VERTEX, NS_VERTEX_DD)
-    print u
-    print time.clock() - start
+    enum(t, NS_QUAD, NS_VERTEX, NS_ALG_DEFAULT)
+    enum(t, NS_QUAD, NS_VERTEX, NS_VERTEX_DD)
