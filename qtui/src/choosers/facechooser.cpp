9830c4dcf3d3ff5262173ff0aeb657c28f5fa55a
Benjamin Burton <bab@debian.org>
2013-09-11 11:11:03 +0000

    More choosers.

---
+++ qtui/src/choosers/facechooser.cpp
@@ -0,0 +1,153 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  KDE User Interface                                                    *
+ *                                                                        *
+ *  Copyright (c) 1999-2013, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  As an exception, when this program is distributed through (i) the     *
+ *  App Store by Apple Inc.; (ii) the Mac App Store by Apple Inc.; or     *
+ *  (iii) Google Play by Google Inc., then that store may impose any      *
+ *  digital rights management, device limits and/or redistribution        *
+ *  restrictions that are required by its terms of service.               *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+// Regina core includes:
+#include "triangulation/nface.h"
+
+// UI includes:
+#include "facechooser.h"
+
+#include <algorithm>
+#include <QBoxLayout>
+#include <QDialogButtonBox>
+#include <QLabel>
+
+using regina::NFace;
+
+FaceChooser::FaceChooser(
+        regina::NTriangulation* tri,
+        FaceFilterFunc filter, QWidget* parent) :
+        QComboBox(parent), tri_(tri), filter_(filter) {
+    setMinimumContentsLength(30);
+    setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLength);
+    tri_->listen(this);
+    fill();
+}
+
+FaceChooser::~FaceChooser() {
+    tri_->unlisten(this);
+}
+
+NFace* FaceChooser::selected() {
+    if (count() == 0)
+        return 0;
+    int curr = currentIndex();
+    return (curr < 0 ? 0 : options_[curr]);
+}
+
+void FaceChooser::select(regina::NFace* option) {
+    int index = 0;
+    std::vector<regina::NFace*>::const_iterator it = options_.begin();
+    while (it != options_.end()) {
+        if ((*it) == option) {
+            setCurrentIndex(index);
+            return;
+        }
+        ++it;
+        ++index;
+    }
+
+    // Not found.
+    if (! options_.empty())
+        setCurrentIndex(0);
+    return;
+}
+
+QString FaceChooser::description(regina::NFace* option) {
+    if (option->getNumberOfEmbeddings() == 1) {
+        const regina::NFaceEmbedding& e0 = option->getEmbedding(0);
+        return trUtf8("Face %2 — %3 (%4)")
+            .arg(tri_->faceIndex(option))
+            .arg(tri_->tetrahedronIndex(e0.getTetrahedron()))
+            .arg(e0.getVertices().trunc3().c_str());
+    } else {
+        const regina::NFaceEmbedding& e0 = option->getEmbedding(0);
+        const regina::NFaceEmbedding& e1 = option->getEmbedding(1);
+        return trUtf8("Face %1 — %2 (%3), %4 (%5)")
+            .arg(tri_->faceIndex(option))
+            .arg(tri_->tetrahedronIndex(e0.getTetrahedron()))
+            .arg(e0.getVertices().trunc3().c_str())
+            .arg(tri_->tetrahedronIndex(e1.getTetrahedron()))
+            .arg(e1.getVertices().trunc3().c_str());
+    }
+}
+
+void FaceChooser::fill() {
+    regina::NTriangulation::FaceIterator it;
+    for (it = tri_->getFaces().begin();
+            it != tri_->getFaces().end(); ++it)
+        if ((! filter_) || (*filter_)(*it)) {
+            addItem(description(*it));
+            options_.push_back(*it);
+        }
+}
+
+FaceDialog::FaceDialog(QWidget* parent,
+        regina::NTriangulation* tri,
+        FaceFilterFunc filter,
+        const QString& title,
+        const QString& message,
+        const QString& whatsThis) :
+        QDialog(parent) {
+    setWindowTitle(title);
+    setWhatsThis(whatsThis);
+    QVBoxLayout* layout = new QVBoxLayout(this);
+
+    QLabel* label = new QLabel(message);
+    layout->addWidget(label);
+
+    chooser = new FaceChooser(tri, filter, this);
+    layout->addWidget(chooser);
+
+    QDialogButtonBox* buttonBox = new QDialogButtonBox(
+        QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    layout->addWidget(buttonBox);
+
+    connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+    connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+}
+
+regina::NFace* FaceDialog::choose(QWidget* parent,
+        regina::NTriangulation* tri,
+        FaceFilterFunc filter,
+        const QString& title,
+        const QString& message,
+        const QString& whatsThis) {
+    FaceDialog dlg(parent, tri, filter, title, message, whatsThis);
+    if (dlg.exec())
+        return dlg.chooser->selected();
+    else
+        return 0;
+}
+
---
+++ qtui/src/choosers/facechooser.h
@@ -0,0 +1,175 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Qt User Interface                                                     *
+ *                                                                        *
+ *  Copyright (c) 1999-2013, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  As an exception, when this program is distributed through (i) the     *
+ *  App Store by Apple Inc.; (ii) the Mac App Store by Apple Inc.; or     *
+ *  (iii) Google Play by Google Inc., then that store may impose any      *
+ *  digital rights management, device limits and/or redistribution        *
+ *  restrictions that are required by its terms of service.               *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+/*! \file facechooser.h
+ *  \brief Provides a widget for selecting a single face
+ *  of a 3-manifold triangulation.
+ */
+
+#ifndef __FACECHOOSER_H
+#define __FACECHOOSER_H
+
+#include "packet/npacketlistener.h"
+
+#include <QDialog>
+#include <QComboBox>
+#include <vector>
+
+namespace regina {
+    class NFace;
+    class NTriangulation;
+};
+
+/**
+ * A filter function, used to determine whether a given face
+ * should appear in the list.
+ */
+typedef bool (*FaceFilterFunc)(regina::NFace*);
+
+/**
+ * A widget through which a single face of some triangulation
+ * can be selected.  An optional filter may be applied to restrict the
+ * available selections.
+ *
+ * The contents of this chooser will be updated in real time if the
+ * triangulation is externally modified.
+ */
+class FaceChooser : public QComboBox, public regina::NPacketListener {
+    Q_OBJECT
+
+    private:
+        regina::NTriangulation* tri_;
+            /**< The triangulation whose faces we are
+                 choosing from. */
+        FaceFilterFunc filter_;
+            /**< A filter to restrict the available selections, or
+                 0 if no filter is necessary. */
+        std::vector<regina::NFace*> options_;
+            /**< A list of the available options to choose from. */
+
+    public:
+        /**
+         * Constructors that fills the chooser with available selections.
+         *
+         * This chooser will claim ownership of any filter that is
+         * passed.
+         */
+        FaceChooser(regina::NTriangulation* tri,
+                FaceFilterFunc filter, QWidget* parent);
+        ~FaceChooser();
+
+        /**
+         * Returns the currently selected face.
+         *
+         * If there are no available faces to choose from,
+         * this routine will return 0.
+         */
+        regina::NFace* selected();
+
+        /**
+         * Changes the selection to the given face.
+         *
+         * If the given face is not one of the options in this
+         * chooser, or if the given pointer is 0, then the first entry
+         * in the chooser will be selected.
+         *
+         * The activated() signal will \e not be emitted.
+         */
+        void select(regina::NFace* option);
+
+        /**
+         * NPacketListener overrides.
+         */
+        void packetToBeChanged(regina::NPacket*);
+        void packetWasChanged(regina::NPacket*);
+        void packetToBeDestroyed(regina::NPacket*);
+
+    private:
+        /**
+         * The text to be displayed for a given option.
+         */
+        QString description(regina::NFace* option);
+
+        /**
+         * Fills the chooser with the set of allowable options.
+         */
+        void fill();
+};
+
+/**
+ * A dialog used to select a single face of a given triangulation.
+ */
+class FaceDialog : public QDialog {
+    Q_OBJECT
+
+    private:
+        /**
+         * Internal components:
+         */
+        FaceChooser* chooser;
+
+    public:
+        /**
+         * Constructor and destructor.
+         */
+        FaceDialog(QWidget* parent,
+            regina::NTriangulation* tri,
+            FaceFilterFunc filter,
+            const QString& title,
+            const QString& message,
+            const QString& whatsThis);
+
+        static regina::NFace* choose(QWidget* parent,
+            regina::NTriangulation* tri,
+            FaceFilterFunc filter,
+            const QString& title,
+            const QString& message,
+            const QString& whatsThis);
+};
+
+inline void FaceChooser::packetToBeChanged(regina::NPacket*) {
+    clear();
+    options_.clear();
+}
+
+inline void FaceChooser::packetWasChanged(regina::NPacket*) {
+    fill();
+}
+
+inline void FaceChooser::packetToBeDestroyed(regina::NPacket*) {
+    clear();
+    options_.clear();
+}
+
+#endif
