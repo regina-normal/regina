74fe7e35216cdd55f16bc97aa3402952614692bd
Benjamin Burton <bab@debian.org>
2013-09-11 05:09:52 +0000

    Start on skeletal chooser code.

---
+++ qtui/src/choosers/boundarycomponentchooser.h
@@ -0,0 +1,139 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Qt User Interface                                                     *
+ *                                                                        *
+ *  Copyright (c) 1999-2013, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  As an exception, when this program is distributed through (i) the     *
+ *  App Store by Apple Inc.; (ii) the Mac App Store by Apple Inc.; or     *
+ *  (iii) Google Play by Google Inc., then that store may impose any      *
+ *  digital rights management, device limits and/or redistribution        *
+ *  restrictions that are required by its terms of service.               *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+/* end stub */
+
+/*! \file boundarycomponentchooser.h
+ *  \brief Provides a widget for selecting a single boundary component
+ *  of a 3-manifold triangulation.
+ */
+
+#ifndef __BOUNDARYCOMPONENTCHOOSER_H
+#define __BOUNDARYCOMPONENTCHOOSER_H
+
+#include "packet/npacketlistener.h"
+
+#include <QComboBox>
+#include <vector>
+
+namespace regina {
+    class NBoundaryComponent;
+    class NTriangulation;
+};
+
+/**
+ * A filter function, used to determine whether a given boundary component
+ * should appear in the list.
+ */
+typedef bool (*BoundaryComponentFilterFunc)(regina::NBoundaryComponent*);
+
+/**
+ * A widget through which a single boundary component of some triangulation
+ * can be selected.  An optional filter may be applied to restrict the
+ * available selections.
+ *
+ * The contents of this chooser will be updated in real time if the
+ * triangulation is externally modified.
+ */
+class BoundaryComponentChooser :
+        public QComboBox, public regina::NPacketListener {
+    Q_OBJECT
+
+    private:
+        regina::NTriangulation* tri_;
+            /**< The triangulation whose boundary components we are
+                 choosing from. */
+        BoundaryComponentFilterFunc* filter_;
+            /**< A filter to restrict the available selections, or
+                 0 if no filter is necessary. */
+        std::vector<regina::NBoundaryComponent*> options_;
+            /**< A list of the available options to choose from. */
+
+    public:
+        /**
+         * Constructors that fills the chooser with available selections.
+         *
+         * This chooser will claim ownership of any filter that is
+         * passed.
+         */
+        BoundaryComponentChooser(regina::NTriangulation* tri,
+                BoundaryComponentFilterFunc* filter, QWidget* parent);
+        ~BoundaryComponentChooser();
+
+        /**
+         * Returns the currently selected boundary component.
+         *
+         * If there are no available boundary components to choose from,
+         * this routine will return 0.
+         */
+        regina::NBoundaryComponent* selected();
+
+        /**
+         * Changes the selection to the given boundary component.
+         *
+         * If the given boundary component is not one of the options in this
+         * chooser, or if the given pointer is 0, then the first entry
+         * in the chooser will be selected.
+         *
+         * The activated() signal will \e not be emitted.
+         */
+        void select(regina::NBoundaryComponent* option);
+
+        /**
+         * NPacketListener overrides.
+         */
+        void packetToBeChanged(regina::NPacket*);
+        void packetWasChanged(regina::NPacket*);
+        void packetToBeDestroyed(regina::NPacket*);
+
+    private:
+        /**
+         * Fills the chooser with the set of allowable options.
+         */
+        void fill();
+};
+
+inline void BoundaryComponentChooser::packetToBeChanged(regina::NPacket*) {
+    clear();
+    options_.clear();
+}
+
+inline void BoundaryComponentChooser::packetWasChanged(regina::NPacket*) {
+    fill();
+}
+
+inline void BoundaryComponentChooser::packetToBeDestroyed(regina::NPacket*) {
+    clear();
+    options_.clear();
+}
+
+#endif
