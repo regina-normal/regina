6103c769f2866fd5d1d73a6aac51057656bf078d
Benjamin Burton <bab@maths.uq.edu.au>
2014-04-29 21:04:34 +1000

    Remove order() and isOrdered(), to be reinstated in a separate branch.

--- engine/dim4/dim4triangulation.h
+++ engine/dim4/dim4triangulation.h
@@ -929,46 +929,6 @@ class REGINA_API Dim4Triangulation : public NPacket,
          */
         bool isConnected() const;
 
-        /**
-         * Relabels pentachoron vertices in this triangulation to give
-         * an ordered triangulation, if possible.
-         *
-         * To be an ordered triangulation, all facet gluings (when restricted
-         * to the pentachoron facet) must be order preserving. In other words,
-         * it must be possible to orient all edges of the triangulation in
-         * such a fashion that they are consistent with the ordering of the
-         * vertices in each pentachoron.
-         *
-         * If it is possible to order this triangulation, the vertices
-         * of each pentachoron will be relabelled accordingly and this
-         * routine will return \c true.  Otherwise, this routine will
-         * return \c false and the triangulation will not be changed.
-         *
-         * \todo \opt This is more efficient than the NTriangulation analogue;
-         * perhaps merge this and/or make it dimension-agnostic.
-         *
-         * @return \c true if the triangulation has been successfully ordered
-         * as described above, or \c false if not.
-         */
-        bool order();
-
-        /**
-         * Determines if this triangulation is ordered; that is, if
-         * pentachoron vertices are labelled so that all gluing
-         * permutations are order-preserving on the pentachoron facets.
-         * Equivalently, this tests whether the edges of the triangulation
-         * can all be oriented such that they induce a consistent ordering
-         * on the vertices of each pentachoron.
-         *
-         * Triangulations are not ordered by default, and indeed some
-         * cannot be ordered at all.  The routine order() will attempt
-         * to relabel pentachoron vertices to give an ordered triangulation.
-         *
-         * @return \c true if and only if all gluing permutations are
-         * order preserving on the pentachoron facets.
-         */
-        bool isOrdered() const;
-
         /*@}*/
         /**
          * \name Algebraic Properties
