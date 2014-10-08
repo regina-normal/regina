8439dc48d97cad72a847ac316add8ba5d565cb40
Benjamin Burton <bab@maths.uq.edu.au>
2014-10-09 08:06:25 +1000

    Surfaces summary: first implementation.

--- ios/packets/SurfacesSummary.mm
+++ ios/packets/SurfacesSummary.mm
@@ -31,8 +31,128 @@
  **************************************************************************/
 
 #import "SurfacesSummary.h"
+#import "TextHelper.h"
+#import "surfaces/nnormalsurface.h"
+#import "surfaces/nnormalsurfacelist.h"
+#import <iterator>
+#import <map>
 
-@interface SurfacesSummary () <UITableViewDataSource>
+// TODO: Compress cell heights.
+// TODO: Experiment with changing cell style.
+
+#pragma mark - Surfaces summary table cell
+
+@interface SurfacesSummaryTableCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *euler;
+@property (weak, nonatomic) IBOutlet UILabel *count0;
+@property (weak, nonatomic) IBOutlet UILabel *count1;
+@property (weak, nonatomic) IBOutlet UILabel *count2;
+@property (weak, nonatomic) IBOutlet UILabel *count3;
+@end
+
+@implementation SurfacesSummaryTableCell
+@end
+
+#pragma mark - Surfaces summary
+
+struct CountSet {
+    unsigned long n1sOr, n2sOr, n1sNor, n2sNor, tot;
+    int mask;
+    
+    CountSet() : n1sOr(0), n2sOr(0), n1sNor(0), n2sNor(0), tot(0), mask(0) {
+    }
+    
+    void reset() {
+        n1sOr = n2sOr = n1sNor = n2sNor = tot = mask = 0;
+    }
+    
+    void append(const regina::NNormalSurface* s) {
+        if (s->isOrientable()) {
+            if (s->isTwoSided()) {
+                ++n2sOr;
+                mask |= 1;
+            } else {
+                ++n1sOr;
+                mask |= 2;
+            }
+        } else {
+            if (s->isTwoSided()) {
+                ++n2sNor;
+                mask |= 4;
+            } else {
+                ++n1sNor;
+                mask |= 8;
+            }
+        }
+        ++tot;
+    }
+    
+    unsigned long field(int usingMask, int pos) const {
+        if (usingMask & 1) {
+            if (pos == 0)
+                return n2sOr;
+            --pos;
+        }
+        if (usingMask & 2) {
+            if (pos == 0)
+                return n1sOr;
+            --pos;
+        }
+        if (usingMask & 4) {
+            if (pos == 0)
+                return n2sNor;
+            --pos;
+        }
+        if (usingMask & 8) {
+            if (pos == 0)
+                return n1sNor;
+            --pos;
+        }
+        return 0;
+    }
+    
+    NSString* fieldString(int usingMask, int pos) const {
+        unsigned long n = field(usingMask, pos);
+        if (n == 0)
+            return @" ";
+        else
+            return [NSString stringWithFormat:@"%ld", n];
+    }
+    
+    static NSString* fieldTitle(int usingMask, int pos) {
+        if (usingMask & 1) {
+            if (pos == 0)
+                return @"2-sided orbl";
+            --pos;
+        }
+        if (usingMask & 2) {
+            if (pos == 0)
+                return @"1-sided orbl";
+            --pos;
+        }
+        if (usingMask & 4) {
+            if (pos == 0)
+                return @"2-sided non-orbl";
+            --pos;
+        }
+        if (usingMask & 8) {
+            if (pos == 0)
+                return @"1-sided non-orbl";
+            --pos;
+        }
+        // Return an empty string for positions that are too large,
+        // so we can use this for extraneous labels.
+        return @" ";
+    }
+};
+
+@interface SurfacesSummary () <UITableViewDataSource, UITableViewDelegate> {
+    CGFloat headerHeight;
+    
+    CountSet nClosed, nBounded;
+    std::map<regina::NLargeInteger, CountSet> eulerClosed, eulerBounded;
+    unsigned long nSpun;
+}
 @property (weak, nonatomic) IBOutlet UITableView *table;
 @end
 
@@ -43,6 +163,7 @@
     [super viewWillAppear:animated];
 
     self.table.dataSource = self;
+    self.table.delegate = self;
 
     [self reloadPacket];
 }
@@ -50,34 +171,174 @@
 - (void)reloadPacket
 {
     [super reloadPacket];
+    
+    nClosed.reset();
+    nBounded.reset();
+    eulerClosed.clear();
+    eulerBounded.clear();
+    nSpun = 0;
+    
+    unsigned long n = self.packet->getNumberOfSurfaces();
+    const regina::NNormalSurface* s;
+    regina::NLargeInteger euler;
+    for (unsigned long i = 0; i < n; ++i) {
+        s = self.packet->getSurface(i);
+        if (! s->isCompact())
+            ++nSpun;
+        else {
+            euler = s->getEulerChar();
+            if (s->hasRealBoundary()) {
+                nBounded.append(s);
+                eulerBounded[euler].append(s);
+            } else {
+                nClosed.append(s);
+                eulerClosed[euler].append(s);
+            }
+        }
+    }
+    
     [self.table reloadData];
 }
 
+#pragma mark - Table view
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    if (nSpun) {
+        if (section == 0)
+            return 0;
+        --section;
+    }
+    
+    if (nClosed.tot) {
+        if (section == 0)
+            return 1 + eulerClosed.size();
+        --section;
+    }
+    
+    if (nBounded.tot) {
+        if (section == 0)
+            return 1 + eulerBounded.size();
+        --section;
+    }
+
+    return 0;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 3;
+    return (nSpun ? 1 : 0) + (nClosed.tot ? 1 : 0) + (nBounded.tot ? 1 : 0);
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    switch (section) {
-        case 0: return @"Closed surfaces";
-        case 1: return @"Bounded surfaces";
-        case 2: return @"Spun (non-compact) surfaces";
+    if (nSpun) {
+        if (section == 0)
+            return [TextHelper countString:nSpun
+                                  singular:"spun (non-compact) surface"
+                                    plural:"spun (non-compact) surfaces"];
+        --section;
     }
-    return @"";
+    
+    if (nClosed.tot) {
+        if (section == 0)
+            return [TextHelper countString:nClosed.tot
+                                  singular:"closed surface"
+                                    plural:"closed surfaces"];
+        --section;
+    }
+    
+    if (nBounded.tot) {
+        if (section == 0)
+            return [TextHelper countString:nBounded.tot
+                                  singular:"bounded surface"
+                                    plural:"bounded surfaces"];
+        --section;
+    }
+
+    return @"No surfaces";
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Count" forIndexPath:indexPath];
-    cell.textLabel.text = @"χ = TODO";
-    return cell;
+    int section = indexPath.section;
+    SurfacesSummaryTableCell* cell;
+    
+    if (nSpun) {
+        if (section == 0)
+            return nil;
+        --section;
+    }
+    
+    if (nClosed.tot) {
+        if (section == 0) {
+            if (indexPath.row == 0) {
+                cell = [tableView dequeueReusableCellWithIdentifier:@"Header" forIndexPath:indexPath];
+                cell.count0.text = nClosed.fieldTitle(nClosed.mask, 0);
+                cell.count1.text = nClosed.fieldTitle(nClosed.mask, 1);
+                cell.count2.text = nClosed.fieldTitle(nClosed.mask, 2);
+                cell.count3.text = nClosed.fieldTitle(nClosed.mask, 3);
+            } else {
+                cell = [tableView dequeueReusableCellWithIdentifier:@"Count" forIndexPath:indexPath];
+                std::map<regina::NLargeInteger, CountSet>::const_reverse_iterator it = eulerClosed.rbegin();
+                advance(it, indexPath.row - 1);
+                cell.euler.text = [NSString stringWithFormat:@"χ = %s", it->first.stringValue().c_str()];
+                cell.count0.text = it->second.fieldString(nClosed.mask, 0);
+                cell.count1.text = it->second.fieldString(nClosed.mask, 1);
+                cell.count2.text = it->second.fieldString(nClosed.mask, 2);
+                cell.count3.text = it->second.fieldString(nClosed.mask, 3);
+            }
+            return cell;
+        }
+        --section;
+    }
+    
+    if (nBounded.tot) {
+        if (section == 0) {
+            if (indexPath.row == 0) {
+                cell = [tableView dequeueReusableCellWithIdentifier:@"Header" forIndexPath:indexPath];
+                cell.count0.text = nBounded.fieldTitle(nBounded.mask, 0);
+                cell.count1.text = nBounded.fieldTitle(nBounded.mask, 1);
+                cell.count2.text = nBounded.fieldTitle(nBounded.mask, 2);
+                cell.count3.text = nBounded.fieldTitle(nBounded.mask, 3);
+            } else {
+                cell = [tableView dequeueReusableCellWithIdentifier:@"Count" forIndexPath:indexPath];
+                std::map<regina::NLargeInteger, CountSet>::const_reverse_iterator it = eulerBounded.rbegin();
+                advance(it, indexPath.row - 1);
+                cell.euler.text = [NSString stringWithFormat:@"χ = %s", it->first.stringValue().c_str()];
+                cell.count0.text = it->second.fieldString(nBounded.mask, 0);
+                cell.count1.text = it->second.fieldString(nBounded.mask, 1);
+                cell.count2.text = it->second.fieldString(nBounded.mask, 2);
+                cell.count3.text = it->second.fieldString(nBounded.mask, 3);
+            }
+            return cell;
+        }
+        --section;
+    }
+
+    // Should never reach here.
+    return nil;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return NO;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row > 0)
+        return self.table.rowHeight;
+    
+    // The header row is smaller.  Calculate it based on the cell contents, which include
+    // auto-layout constraints that pin the labels to the upper and lower boundaries.
+    if (headerHeight == 0) {
+        UITableViewCell* cell = [self.table dequeueReusableCellWithIdentifier:@"Header"];
+        [cell layoutIfNeeded];
+        CGSize size = [cell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+        headerHeight = size.height;
+    }
+    return headerHeight;
 }
 
 @end
