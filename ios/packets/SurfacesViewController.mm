719670c8afbfd1554cec3354403f4715870facea
Benjamin Burton <bab@maths.uq.edu.au>
2014-10-09 21:59:30 +1000

    Drop surfaces navbar button, which is no longer necessary.

--- ios/packets/SurfacesViewController.mm
+++ ios/packets/SurfacesViewController.mm
@@ -51,22 +51,6 @@
     [self registerDefaultKey:@"ViewSurfacesTab"];
 }
 
-- (NSString *)packetActionIcon
-{
-    return @"NavBar-Surfaces";
-}
-
-- (void)packetActionPressed
-{
-    // TODO: Implement actions.
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"To Be Implemented"
-                                                    message:nil
-                                                   delegate:nil
-                                          cancelButtonTitle:@"Close"
-                                          otherButtonTitles:nil];
-    [alert show];
-}
-
 - (void)updateHeader:(UILabel *)summary coords:(UILabel *)coords tri:(UIButton *)tri
 {
     regina::NormalList which = self.packet->which();
