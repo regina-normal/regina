d973b44b9f52cfe6a5963ef61ac8b8088d3f77c8
Benjamin Burton <bab@maths.uq.edu.au>
2014-10-15 08:23:01 +1000

    Bells and whistles for the compatibility matrix tab.

--- ios/packets/SurfacesCompat.mm
+++ ios/packets/SurfacesCompat.mm
@@ -36,6 +36,11 @@
 
 #define KEY_LAST_TYPE @"SurfacesCompatType"
 
+#define MARGIN 20
+#define TICK_LENGTH 5
+#define TICK_GAP_HORIZ 5
+#define TICK_GAP_VERT 2
+
 static NSArray* typeText = @[@"Do surfaces have compatible quadrilateral and/or octagon types?",
                              @"Can surfaces be made globally disjoint?"];
 
@@ -49,12 +54,16 @@ static NSArray* typeText = @[@"Do surfaces have compatible quadrilateral and/or 
 @property (assign, nonatomic) regina::NNormalSurfaceList* packet;
 @property (assign, nonatomic) BOOL showLocal;
 
+@property (assign, nonatomic) CGFloat minCellSize;
 @property (assign, nonatomic) CGFloat maxCellSize;
 @property (assign, nonatomic) CGFloat lineWidth;
 @property (assign, nonatomic) CGFloat lineOffset;
 @property (strong, nonatomic) UIColor *gridColour;
+@property (strong, nonatomic) UIColor *labelColour;
 @property (strong, nonatomic) UIColor *localCellColour;
 @property (strong, nonatomic) UIColor *globalCellColour;
+@property (strong, nonatomic) UIColor *strikeoutColour;
+@property (strong, nonatomic) UIFont *labelFont;
 @end
 
 @implementation CompatGrid
@@ -63,7 +72,7 @@ static NSArray* typeText = @[@"Do surfaces have compatible quadrilateral and/or 
 {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        // TODO: Fix all defaults.
+        self.minCellSize = 4;
         self.maxCellSize = 20;
         if (self.contentScaleFactor >= 2.0) {
             self.lineWidth = 0.5;
@@ -73,8 +82,15 @@ static NSArray* typeText = @[@"Do surfaces have compatible quadrilateral and/or 
             self.lineOffset = 0.5;
         }
         self.gridColour = [UIColor lightGrayColor];
-        self.localCellColour = [UIColor cyanColor];
-        self.globalCellColour = [UIColor greenColor];
+        self.labelColour = [UIColor darkGrayColor];
+        // Powder blue: #B0E0E6
+        self.localCellColour = [UIColor colorWithRed:(0xb0 / 256.0) green:(0xe0 / 256.0) blue:(0xe6 / 256.0) alpha:1];
+        // Medium sea green: #3CB371
+        // Yellow green: #9ACD32
+        self.globalCellColour = [UIColor colorWithRed:(0x9a / 256.0) green:(0xcd / 256.0) blue:(0x32 / 256.0) alpha:1];
+        // Dark red: #8B0000
+        self.strikeoutColour = [UIColor colorWithRed:(0x8b / 256.0) green:0 blue:0 alpha:1];
+        self.labelFont = [UIFont systemFontOfSize:14];
         
         self.backgroundColor = [UIColor whiteColor];
         self.opaque = YES;
@@ -133,91 +149,167 @@ static NSArray* typeText = @[@"Do surfaces have compatible quadrilateral and/or 
 - (void)reload
 {
     local = global = nil;
-    
-    if (! self.packet)
-        return;
-    
-    if (self.showLocal)
-        [self buildLocal];
-    else
-        [self buildGlobal];
-    
     [self setNeedsDisplay];
-}
-
-- (void)reloadShowingLocal:(BOOL)showLocal
-{
-    _showLocal = showLocal;
-    [self reload];
 }
 
 - (void)setShowLocal:(BOOL)showLocal
 {
-    if (showLocal && ! _showLocal) {
-        _showLocal = YES;
-        if (! local)
-            [self buildLocal];
-    } else if (_showLocal && ! showLocal) {
-        _showLocal = NO;
-        if (! global)
-            [self buildGlobal];
+    if (showLocal != _showLocal) {
+        _showLocal = showLocal;
+        [self setNeedsDisplay];
     }
-    [self setNeedsDisplay];
+}
+
+- (void)drawError:(NSString*)text
+{
+    UIFont* errorFont = [UIFont systemFontOfSize:[UIFont systemFontSize]];
+    CGSize textBounds = [text sizeWithAttributes:@{NSFontAttributeName: errorFont}];
+ 
+    [text drawAtPoint:CGPointMake((self.bounds.size.width - textBounds.width) / 2,
+                                  (self.bounds.size.height - textBounds.height) / 2)
+      withAttributes:@{NSFontAttributeName:errorFont,
+                       NSForegroundColorAttributeName:self.strikeoutColour}];
 }
 
 - (void)drawRect:(CGRect)rect
 {
+    if (! self.packet)
+        return;
+    
     unsigned long n = self.packet->getNumberOfSurfaces();
     
-    // TODO: Proper labels.
-    CGFloat margin = 20;
+    if (n == 0) {
+        [self drawError:@"No surfaces to display"];
+        return;
+    }
     
-    CGFloat size = MIN(self.bounds.size.width, self.bounds.size.height);
-    CGFloat cellSize = (size - 2 * margin) / n;
+    // Compute the cell size.
+    CGSize maxLabel = [[NSString stringWithFormat:@"%ld", n] sizeWithAttributes:@{NSFontAttributeName: self.labelFont}];
+    
+    CGFloat availableHeight = self.bounds.size.height - 2 * MARGIN - maxLabel.height - TICK_LENGTH - TICK_GAP_VERT;
+    CGFloat availableWidth = self.bounds.size.width - 2 * MARGIN - maxLabel.width - TICK_LENGTH - TICK_GAP_HORIZ;
+    
+    CGFloat cellSize = floor(MIN(availableHeight, availableWidth) / n);
     if (cellSize > self.maxCellSize)
         cellSize = self.maxCellSize;
     
+    if (cellSize < self.minCellSize) {
+        [self drawError:@"Too many surfaces to display"];
+        return;
+    }
+    
+    // Compute the compatibility values if necessary.
+    NSArray* show;
+    if (self.showLocal) {
+        if (! local)
+            [self buildLocal];
+        show = local;
+    } else {
+        if (! global)
+            [self buildGlobal];
+        show = global;
+    }
+    
+    // How often to draw ticks?
+    int tickPeriod;
+    CGFloat cellsForLabel = (maxLabel.width / cellSize);
+    if (cellsForLabel < 2.5)
+        tickPeriod = 5;
+    else if (cellsForLabel < 6)
+        tickPeriod = 10;
+    else if (cellsForLabel < 15)
+        tickPeriod = 20;
+    else if (cellsForLabel < 40)
+        tickPeriod = 50;
+    else
+        cellsForLabel = 100;
+
+    // Draw it!
     UIBezierPath *path = [UIBezierPath bezierPath];
     path.lineWidth = self.lineWidth;
     
-    CGFloat originX = round((self.bounds.size.width - n * cellSize) / 2) - self.lineOffset;
-    CGFloat originY = round((self.bounds.size.height - n * cellSize) / 2) - self.lineOffset;
+    CGSize displaySize = CGSizeMake(n * cellSize + self.lineWidth + maxLabel.width + TICK_LENGTH + TICK_GAP_HORIZ,
+                                    n * cellSize + self.lineWidth + maxLabel.height + TICK_LENGTH + TICK_GAP_VERT);
+    
+    CGFloat originX = round((self.bounds.size.width - displaySize.width) / 2 + maxLabel.width + TICK_LENGTH + TICK_GAP_HORIZ);
+    CGFloat originY = round((self.bounds.size.height - displaySize.height) / 2 + maxLabel.height + TICK_LENGTH + TICK_GAP_VERT);
     
     unsigned long i, j;
     CGFloat pos;
-    for (i = 0, pos = originX; i <= n; ++i, pos += cellSize) {
-        [path moveToPoint:CGPointMake(pos, originY)];
-        [path addLineToPoint:CGPointMake(pos, originY + n * cellSize)];
+    NSString* label;
+    NSMutableParagraphStyle* align = [[NSMutableParagraphStyle alloc] init];
+    for (i = 0, pos = originX + self.lineOffset; i <= n; ++i, pos += cellSize) {
+        [path moveToPoint:CGPointMake(pos, originY + self.lineOffset)];
+        [path addLineToPoint:CGPointMake(pos, originY + self.lineOffset + n * cellSize)];
+        
+        if (i % tickPeriod == 0) {
+            [path moveToPoint:CGPointMake(pos + (cellSize / 2), originY + self.lineOffset)];
+            [path addLineToPoint:CGPointMake(pos + (cellSize / 2), originY + self.lineOffset - TICK_LENGTH)];
+            
+            label = [NSString stringWithFormat:@"%ld", i];
+            align.alignment = NSTextAlignmentCenter;
+            [label drawInRect:CGRectMake(pos + (cellSize / 2) - maxLabel.width,
+                                         originY + self.lineOffset - TICK_LENGTH - TICK_GAP_VERT - maxLabel.height,
+                                         maxLabel.width * 2,
+                                         maxLabel.height)
+               withAttributes:@{NSFontAttributeName:self.labelFont,
+                                NSForegroundColorAttributeName:self.labelColour,
+                                NSParagraphStyleAttributeName:align}];
+        }
     }
-    for (i = 0, pos = originY; i <= n; ++i, pos += cellSize) {
-        [path moveToPoint:CGPointMake(originX, pos)];
-        [path addLineToPoint:CGPointMake(originX + n * cellSize, pos)];
+    for (i = 0, pos = originY + self.lineOffset; i <= n; ++i, pos += cellSize) {
+        [path moveToPoint:CGPointMake(originX + self.lineOffset, pos)];
+        [path addLineToPoint:CGPointMake(originX + self.lineOffset + n * cellSize, pos)];
+        
+        if (i % tickPeriod == 0) {
+            [path moveToPoint:CGPointMake(originX + self.lineOffset, pos + (cellSize / 2))];
+            [path addLineToPoint:CGPointMake(originX + self.lineOffset - TICK_LENGTH, pos + (cellSize / 2))];
+
+            
+            label = [NSString stringWithFormat:@"%ld", i];
+            align.alignment = NSTextAlignmentRight;
+            [label drawInRect:CGRectMake(originX + self.lineOffset - TICK_LENGTH - TICK_GAP_HORIZ - maxLabel.width * 2,
+                                         pos + (cellSize / 2) - (maxLabel.height / 2),
+                                         maxLabel.width * 2,
+                                         maxLabel.height)
+               withAttributes:@{NSFontAttributeName:self.labelFont,
+                                NSForegroundColorAttributeName:self.labelColour,
+                                NSParagraphStyleAttributeName:align}];
+        }
     }
     
     [self.gridColour setStroke];
     [path stroke];
-
-    NSArray* show;
-    if (self.showLocal) {
-        show = local;
+    
+    if (self.showLocal)
         [self.localCellColour setFill];
-    } else {
-        show = global;
+    else
         [self.globalCellColour setFill];
-    }
     
     for (i = 0; i < n; ++i) {
         for (j = 0; j < n; ++j) {
             if (show[i] == NSNull.null || show[i][j] == NSNull.null) {
-                // TODO: Cross out.
-            } else {
-                if ([show[i][j] boolValue]) {
-                    path = [UIBezierPath bezierPathWithRect:CGRectMake(originX + self.lineWidth + j * cellSize,
-                                                                       originY + self.lineWidth + i * cellSize,
-                                                                       cellSize - self.lineWidth,
-                                                                       cellSize - self.lineWidth)];
-                    [path fill];
-                }
+                path = [UIBezierPath bezierPath];
+                path.lineWidth = self.lineWidth;
+                
+                [path moveToPoint:CGPointMake(originX + self.lineOffset + self.lineWidth + j * cellSize,
+                                              originY + self.lineOffset + self.lineWidth + i * cellSize)];
+                [path addLineToPoint:CGPointMake(originX + self.lineOffset - self.lineWidth + (j+1) * cellSize,
+                                                 originY + self.lineOffset - self.lineWidth + (i+1) * cellSize)];
+                
+                [path moveToPoint:CGPointMake(originX + self.lineOffset + self.lineWidth + j * cellSize,
+                                              originY + self.lineOffset - self.lineWidth + (i+1) * cellSize)];
+                [path addLineToPoint:CGPointMake(originX + self.lineOffset - self.lineWidth + (j+1) * cellSize,
+                                                 originY + self.lineOffset + self.lineWidth + i * cellSize)];
+                
+                [self.strikeoutColour setStroke];
+                [path stroke];
+            } else if ([show[i][j] boolValue]) {
+                path = [UIBezierPath bezierPathWithRect:CGRectMake(originX + self.lineWidth + j * cellSize,
+                                                                   originY + self.lineWidth + i * cellSize,
+                                                                   cellSize - self.lineWidth,
+                                                                   cellSize - self.lineWidth)];
+                [path fill];
             }
         }
     }
@@ -238,10 +330,11 @@ static NSArray* typeText = @[@"Do surfaces have compatible quadrilateral and/or 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    self.grid.packet = self.packet;
 
     self.type.selectedSegmentIndex = [[NSUserDefaults standardUserDefaults] integerForKey:KEY_LAST_TYPE];
-    self.typeExpln.text = typeText[self.type.selectedSegmentIndex];
+    [self typeChanged:nil];
+
+    self.grid.packet = self.packet;
 
     [self reloadPacket];
 }
@@ -249,7 +342,7 @@ static NSArray* typeText = @[@"Do surfaces have compatible quadrilateral and/or 
 - (void)reloadPacket
 {
     [super reloadPacket];
-    [self.grid reloadShowingLocal:(self.type.selectedSegmentIndex == 0)];
+    [self.grid reload];
 }
 
 - (IBAction)typeChanged:(id)sender {
