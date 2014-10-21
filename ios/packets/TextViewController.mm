af12118437a52c072decda64c986efb4357407ec
Benjamin Burton <bab@maths.uq.edu.au>
2014-10-21 10:14:18 +1000

    Fix scrolling for text view when keyboard is shown.

--- ios/packets/TextViewController.mm
+++ ios/packets/TextViewController.mm
@@ -33,8 +33,6 @@
 #import "TextViewController.h"
 #import "packet/ntext.h"
 
-// TODO: Need to change insets to account for space taken by the keyboard.
-
 @interface TextViewController () <UITextViewDelegate> {
     BOOL myEdit;
 }
@@ -51,6 +49,19 @@
     _detail.delegate = self;
 
     [self reloadPacket];
+
+    NSNotificationCenter* nc = [NSNotificationCenter defaultCenter];
+    [nc addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
+    [nc addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+
+    NSNotificationCenter* nc = [NSNotificationCenter defaultCenter];
+    [nc removeObserver:self name:UIKeyboardDidShowNotification object:nil];
+    [nc removeObserver:self name:UIKeyboardWillHideNotification object:nil];
 }
 
 - (void)reloadPacket {
@@ -72,6 +83,40 @@
     myEdit = YES;
     self.packet->setText([textView.text UTF8String]);
     myEdit = NO;
+}
+
+- (void)keyboardDidShow:(NSNotification*)notification
+{
+    // Madly, the (width, height) for kbSize seem to switch positions between iOS 7 and iOS 8
+    // when working in landscape mode.
+    // What we want seems to be:
+    // - kbSize.width for (iOS 7, landscape);
+    // - kbSize.height for (iOS 7, portrait) and (iOS 8, all).
+    // Just cheat and take the min.
+    CGSize kbSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    CGFloat kbHeight = MIN(kbSize.width, kbSize.height);
+
+    self.detail.contentInset = UIEdgeInsetsMake(0, 0, kbHeight, 0);
+    self.detail.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, kbHeight, 0);
+
+    // Sigh.  If we call [self.detail scrollRangeToVisible:self.detail.selectedRange],
+    // the cursor does not move up above the keyboard.
+    // Do this manually instead.
+    CGRect caret = [self.detail caretRectForPosition:self.detail.selectedTextRange.end];
+    CGFloat overflow = caret.origin.y + caret.size.height - (self.detail.contentOffset.y + self.detail.bounds.size.height - kbHeight);
+    if (overflow > 0) {
+        // Add a few points more than the required offset.
+        [UIView animateWithDuration:.2 animations:^{
+            self.detail.contentOffset = CGPointMake(self.detail.contentOffset.x,
+                                                    self.detail.contentOffset.y + overflow + 7);
+        }];
+    }
+}
+
+- (void)keyboardWillHide:(NSNotification*)notification
+{
+    self.detail.contentInset = UIEdgeInsetsZero;
+    self.detail.scrollIndicatorInsets = UIEdgeInsetsZero;
 }
 
 @end
