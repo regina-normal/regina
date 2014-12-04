0860bf431ecb18e4e7b828936e37e4b30bbc900e
Benjamin Burton <bab@maths.uq.edu.au>
2014-12-04 11:19:48 +1000

    Begin thorough documentation for NPolynomial.

--- engine/maths/npolynomial.h
+++ engine/maths/npolynomial.h
@@ -51,77 +51,280 @@ namespace regina {
  * @{
  */
 
-// TODO: Make sure all operations work on one's self.
-
 /**
- * TODO.
+ * Represents a single-variable polynomial with coefficients of type \a T.
+ * All exponents in the polynomial must be non-negative (so you can
+ * represent <tt>2+3x</tt> but not <tt>1+1/x</tt>).
  *
- * Here, the zero polynomial is considered to have degree 0.
+ * The type \a T must represent a ring with no zero divisors.
+ * In particular, it must:
  *
- * Pre: T has a default constructor that initialises to zero.
+ * - support basic arithmetic operations;
+ * - support assignments of the form <tt>x = int</tt> and
+ *   tests of the form <tt>x == int</tt> and <tt>x < int</tt>;
+ * - have a default constructor that assigns an explicit value of zero.
  *
- * \ifacespython In Python, NPolynomial refers to the specific template
- * class NPolynomial<NRational>.
+ * This means that Regina's numerical types such as NInteger and NRational
+ * are supported, but native data types such as int and long are not
+ * (since they have no zero-initialising default constructor).
+ *
+ * The underlying storage method for this class is dense (i.e., all
+ * coefficients are explicitly stored, including zero coefficients).
+ *
+ * \ifacespython In Python, the class NPolynomial refers to the specific
+ * template class NPolynomial<NRational>.
  */
 template <typename T>
 class REGINA_API NPolynomial {
     private:
         size_t degree_;
+            /**< The degree of the polynomial.  Here the zero polynomial
+                 is considered to have degree zero. */
         T* coeff_;
+            /**< An array of length (\a degree_ + 1), storing the
+                 coefficients of the polynomial.  Specifically,
+                 coeff_[i] stores the coefficient of <tt>x^i</tt>. */
 
     public:
         /**
-         * Initialises to zero.
+         * Creates the zero polynomial.
          */
         NPolynomial();
+
         /**
-         * Initialises to x^degree.
+         * Creates the polynomial <tt>x^d</tt> for the given degree \a d.
+         *
+         * @param d the degree of the new polynomial.
          */
         explicit NPolynomial(size_t degree);
 
         /**
-         * Note for developers: even though this routine is identical to
+         * Creates a new copy of the given polynomial.
+         *
+         * A note for developers: even though this routine is identical to
          * the templated copy constructor, it must be declared and
          * implemented separately.  Otherwise the compiler might create
          * its own (incorrect) copy constructor automatically.
+         *
+         * @param value the polynomial to clone.
          */
         NPolynomial(const NPolynomial<T>& value);
 
-        // Not in Python (though the copy constructor is).
+        /**
+         * Creates a new copy of the given polynomial.
+         *
+         * \pre Objects of type \a T can be assigned values of type \a U.
+         *
+         * @param value the polynomial to clone.
+         */
         template <typename U>
         NPolynomial(const NPolynomial<U>& value);
 
-        // Not in Python.
+        /**
+         * Creates a new polynomial from the given sequence of coefficients.
+         * The coefficients should appear in order from the constant
+         * coefficient to the leading coefficient.
+         *
+         * There is no problem if the leading coefficient (i.e., the
+         * last coefficient in the sequence) is zero.
+         * An empty sequence will be treated as the zero polynomial.
+         *
+         * \pre Objects of type \a T can be assigned values from
+         * dereferenced iterators of type \a iterator.
+         *
+         * \ifacespython Not present.
+         *
+         * @param begin the beginning of the sequence of coefficients.
+         * @param end a past-the-end iterator indicating the end of the
+         * sequence of coefficients.
+         */
         template <typename iterator>
         NPolynomial(iterator begin, iterator end);
 
+        /**
+         * Destroys this polynomial.
+         */
         ~NPolynomial();
 
+        /**
+         * Sets this to become the zero polynomial.
+         */
         void init();
 
+        /**
+         * Sets this to become the polynomial <tt>x^d</tt> for the
+         * given degree \a d.
+         *
+         * @param d the new degree of this polynomial.
+         */
         void init(size_t degree);
 
-        // Not in Python.
+        /**
+         * Sets this to become the polynomial described by the given
+         * sequence of coefficients.
+         * The coefficients should appear in order from the constant
+         * coefficient to the leading coefficient.
+         *
+         * There is no problem if the leading coefficient (i.e., the
+         * last coefficient in the sequence) is zero.
+         * An empty sequence will be treated as the zero polynomial.
+         *
+         * \pre Objects of type \a T can be assigned values from
+         * dereferenced iterators of type \a iterator.
+         *
+         * \ifacespython Not present.
+         *
+         * @param begin the beginning of the sequence of coefficients.
+         * @param end a past-the-end iterator indicating the end of the
+         * sequence of coefficients.
+         */
         template <typename iterator>
         void init(iterator begin, iterator end);
 
+        /**
+         * Returns the degree of this polynomial.
+         * This is the largest exponent with a non-zero coefficient.
+         *
+         * For the purposes of this class, the zero polynomial is
+         * considered to have degree zero.
+         *
+         * @return the degree of this polynomial.
+         */
         size_t degree() const;
+
+        /**
+         * Returns the given coefficient of this polynomial.
+         *
+         * @param exp the exponent of the term whose coefficient should
+         * be returned.  This must be between 0 and degree() inclusive.
+         * @return the coefficient of the given term.
+         */
         const T& operator [] (size_t exp) const;
+
+        /**
+         * Changes the given coefficient of this polynomial.
+         *
+         * It is fine to set the leading coefficient to zero, though note
+         * that degree() will now return a smaller value as a result.
+         *
+         * It is also fine to set a coefficient whose exponent is larger
+         * than the current degree; this time degree() will now return a
+         * larger value (unless the given coefficient is zero).  Such an
+         * operation is expensive, however, since it will require
+         * deallocating and reallocating the full list of coefficients.
+         *
+         * \ifacespython This set() routine is available, but you can
+         * also set coefficients directly using syntax of the form
+         * <tt>p[exp] = value</tt>.
+         *
+         * @param exp the exponent of the term whose coefficient should
+         * be changed.
+         * @param value the new value of this coefficient.
+         */
         void set(size_t exp, const T& value);
+
+        /**
+         * Tests whether this and the given polynomial are equal.
+         *
+         * @rhs the polynomial to compare with this.
+         * @return \c true if and only if this and the given polynomial
+         * are equal.
+         */
         bool operator == (const NPolynomial<T>& rhs) const;
+
+        /**
+         * Tests whether this and the given polynomial are not equal.
+         *
+         * @rhs the polynomial to compare with this.
+         * @return \c true if and only if this and the given polynomial
+         * are not equal.
+         */
         bool operator != (const NPolynomial<T>& rhs) const;
+
+        /**
+         * Sets this to be a copy of the given polynomial.
+         *
+         * This and the given polynomial need not have the same degree
+         * (but if they do not, then the degree of this polynomial will
+         * of course change).
+         *
+         * A note to developers: although this is identical to the templated
+         * assignment operator, it must be declared and implemented separately.
+         * See the copy constructor for further details.
+         *
+         * @param value the polynomial to copy.
+         * @return a reference to this polynomial.
+         */
         NPolynomial& operator = (const NPolynomial<T>& value);
+
+        /**
+         * Sets this to be a copy of the given polynomial.
+         *
+         * This and the given polynomial need not have the same degree
+         * (but if they do not, then the degree of this polynomial will
+         * of course change).
+         *
+         * @param value the polynomial to copy.
+         * @return a reference to this polynomial.
+         */
         template <typename U>
         NPolynomial& operator = (const NPolynomial<U>& value);
+
+        /**
+         * Swaps the contents of this and the given polynomial.
+         * This is a fast (constant time) operation.
+         *
+         * This and the given polynomial do not need to have the same degree.
+         *
+         * @param other the polynomial whose contents should be swapped
+         * with this.
+         */
         void swap(NPolynomial<T>& other);
 
-
+        /**
+         * Multiplies this polynomial by the given constant.
+         *
+         * @param scalar the scalar factor to multiply by.
+         * @return a reference to this polynomial.
+         */
         NPolynomial& operator *= (const T& scalar);
-        // Assumes exact division of coefficients using /=.
+
+        /**
+         * Divides this polynomial by the given constant.
+         *
+         * This uses the division operator /= for the coefficient type \a T.
+         *
+         * \pre The argument \a scalar is non-zero.
+         *
+         * @param scalar the scalar factor to divide by.
+         * @return a reference to this polynomial.
+         */
         NPolynomial& operator /= (const T& scalar);
 
+        /**
+         * Adds the given polynomial to this.
+         *
+         * The given polynomial need not have the same degree as this.
+         * Note that the degree of this polynomial might change as a
+         * result of this operation.
+         *
+         * @param other the polynomial to add to this.
+         * @return a reference to this polynomial.
+         */
         NPolynomial& operator += (const NPolynomial<T>& other);
+
+        /**
+         * Subtracts the given polynomial from this.
+         *
+         * The given polynomial need not have the same degree as this.
+         * Note that the degree of this polynomial might change as a
+         * result of this operation.
+         *
+         * @param other the polynomial to subtract from this.
+         * @return a reference to this polynomial.
+         */
         NPolynomial& operator -= (const NPolynomial<T>& other);
+
         NPolynomial& operator *= (const NPolynomial<T>& other);
         /**
          * Currently requires T = NInteger or NLargeInteger.
@@ -149,6 +352,7 @@ REGINA_API std::ostream& operator << (std::ostream& out,
 
 template <typename T>
 inline NPolynomial<T>::NPolynomial() : degree_(0), coeff_(new T[1]) {
+    // The default constructor for T already initialises coeff_[0] to zero.
 }
 
 template <typename T>
@@ -188,6 +392,7 @@ inline void NPolynomial<T>::init() {
     delete[] coeff_;
     degree_ = 0;
     coeff_ = new T[1];
+    // coeff_[0] is initialised to 0 automatically.
 }
 
 template <typename T>
@@ -212,10 +417,11 @@ void NPolynomial<T>::init(iterator begin, iterator end) {
     degree_ = end - begin - 1;
     coeff_ = new T[degree_ + 1];
 
-    size_t i;
-    for (i = 0; begin != end; ++i, ++begin)
-        coeff_[i] = *begin;
+    size_t i = 0;
+    while (begin != end)
+        coeff_[i++] = *begin++;
 
+    // The leading coefficient might be zero.
     while (degree_ > 0 && coeff_[degree_] == 0)
         --degree_;
 }
@@ -243,6 +449,7 @@ void NPolynomial<T>::set(size_t exp, const T& value) {
             coeff_[exp] = value;
         }
     } else if (! (value == 0)) {
+        // The degree will increase.
         T* newCoeff = new T[exp + 1];
         size_t i;
         for (i = 0; i <= degree_; ++i)
@@ -277,6 +484,8 @@ inline bool NPolynomial<T>::operator != (const NPolynomial<T>& rhs) const {
 
 template <typename T>
 NPolynomial<T>& NPolynomial<T>::operator = (const NPolynomial<T>& other) {
+    // This works even if &other == this, since we don't reallocate if
+    // the degrees are equal.
     if (degree_ < other.degree_) {
         delete[] coeff_;
         coeff_ = new T[other.degree_ + 1];
@@ -290,6 +499,8 @@ NPolynomial<T>& NPolynomial<T>::operator = (const NPolynomial<T>& other) {
 template <typename T>
 template <typename U>
 NPolynomial<T>& NPolynomial<T>::operator = (const NPolynomial<U>& other) {
+    // This works even if &other == this, since we don't reallocate if
+    // the degrees are equal.
     if (degree_ < other.degree_) {
         delete[] coeff_;
         coeff_ = new T[other.degree_ + 1];
@@ -326,6 +537,8 @@ inline NPolynomial<T>& NPolynomial<T>::operator /= (const T& scalar) {
 
 template <typename T>
 NPolynomial<T>& NPolynomial<T>::operator += (const NPolynomial<T>& other) {
+    // This works even if &other == this, since we don't reallocate if
+    // the degrees are equal.
     size_t i;
     if (degree_ < other.degree_) {
         T* copy = new T[other.degree_ + 1];
@@ -337,13 +550,18 @@ NPolynomial<T>& NPolynomial<T>::operator += (const NPolynomial<T>& other) {
     }
     for (i = 0; i <= other.degree_; ++i)
         coeff_[i] += other.coeff_[i];
+
+    // We might have zeroed out the leading coefficient.
     while (degree_ > 0 && coeff_[degree_] == 0)
         --degree_;
+
     return *this;
 }
 
 template <typename T>
 NPolynomial<T>& NPolynomial<T>::operator -= (const NPolynomial<T>& other) {
+    // This works even if &other == this, since we don't reallocate if
+    // the degrees are equal.
     size_t i;
     if (degree_ < other.degree_) {
         T* copy = new T[other.degree_ + 1];
@@ -356,13 +574,17 @@ NPolynomial<T>& NPolynomial<T>::operator -= (const NPolynomial<T>& other) {
     for (i = 0; i <= other.degree_; ++i)
         coeff_[i] -= other.coeff_[i];
 
+    // We might have zeroed out the leading coefficient.
     while (degree_ > 0 && coeff_[degree_] == 0)
         --degree_;
+
     return *this;
 }
 
 template <typename T>
 NPolynomial<T>& NPolynomial<T>::operator *= (const NPolynomial<T>& other) {
+    // This works even if &other == this, since we construct the
+    // coefficients of the product in a separate section of memory.
     size_t i, j;
     T* ans = new T[degree_ + other.degree_ + 1];
     for (i = 0; i <= degree_; ++i)
@@ -382,6 +604,7 @@ template <typename T>
 NPolynomial<T>& NPolynomial<T>::operator /= (const NPolynomial<T>& other) {
     size_t i, j;
     if (other.degree_ == 0) {
+        // The following code is correct even if &other == this.
         for (i = 0; i <= degree_; ++i)
             coeff_[i] /= other.coeff_[0];
         return *this;
@@ -389,6 +612,7 @@ NPolynomial<T>& NPolynomial<T>::operator /= (const NPolynomial<T>& other) {
     if (degree_ == 0)
         return *this;
 
+    // TODO: Make self /= self work from here down.
     T* remainder = coeff_;
     coeff_ = new T[degree_ - other.degree_ + 1];
     for (i = degree_; i >= other.degree_; --i) {
