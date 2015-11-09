50a726e36b1018b198b001335febfcecb7fb8f0d
Benjamin Burton <bab@maths.uq.edu.au>
2015-11-10 02:03:12 +1000

    A couple more spots for REGINA_INLINE_REQUIRED.

--- engine/dim2/dim2triangle.h
+++ engine/dim2/dim2triangle.h
@@ -304,6 +304,7 @@ class REGINA_API Dim2Triangle :
          * @return the vertex of the skeleton corresponding to the
          * requested triangle vertex.
          */
+        REGINA_INLINE_REQUIRED
         Dim2Vertex* getVertex(int vertex) const;
         /**
          * Returns the edge in the 2-manifold triangulation skeleton
@@ -374,6 +375,7 @@ class REGINA_API Dim2Triangle :
          * @return a mapping from vertices (0,1) of the requested
          * edge to the corresponding vertices of this triangle.
          */
+        REGINA_INLINE_REQUIRED
         NPerm3 getEdgeMapping(int edge) const;
         /**
          * Returns the orientation of this triangle in the 2-manifold
