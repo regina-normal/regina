16e73303966b95ecbac3607f399bc192c43f3afa
William Pettersson <william.pettersson@gmail.com>
2016-01-25 19:23:36 +1000

    Create metadata when creating database

--- engine/data/census/mkenumerationdb.cpp
+++ engine/data/census/mkenumerationdb.cpp
@@ -33,8 +33,9 @@
 /* end stub */
 
 #include "regina-config.h" // For QDBM-related macros
+#include "census/enumerationdb.h"
 #include "triangulation/ntriangulation.h"
-#include "census/nfacepairing.h"
+#include "triangulation/nfacepairing.h"
 
 #include <cctype>
 #include <cstdlib>
@@ -65,13 +66,27 @@ void usage(const char* progName, const std::string& error = std::string()) {
         std::cerr << error << "\n\n";
 
     std::cerr << "Usage:\n";
-    std::cerr << "    " << progName << " <input-file> <output-file>\n";
+    std::cerr << "    " << progName << " <nTet> <orientations> "
+        "<hyperbolic> <input-file> <output-file>\n";
     std::cerr << std::endl;
+    std::cerr << "<nTet> is an integer such that this database contains all "
+        "triangulations on <= nTet tetrahedra" << std::endl;
+    std::cerr << "<orientations> is one of:" << std::endl;
+    std::cerr << "\t\"o\" if this database only contains all orientable "
+        "triangulations," << std::endl;
+    std::cerr << "\t\"n\" if this database only contains all non-orientable "
+        "triangulations, or " << std::endl;
+    std::cerr << "\t\"a\" if this database contains all orientable and "
+        "non-orientable triangulations." << std::endl;
+    std::cerr << "<hyperbolic> should be \"y\" if this census is of hyperbolic "
+        "triangulations, \"n\" otherwise." << std::endl;
     std::cerr << "Key-value data will be read from the input file:\n";
     std::cerr << "<isosig> <name>\n";
     std::cerr << "<isosig> <name>\n";
     std::cerr << "...\n";
-    std::cerr << "Note that even though the name must be present, it will not "
+    std::cerr << "Note that all options must be passed to " << progName <<
+        " in order to properly initialise the database" << std::endl;
+    std::cerr << "Also note that even though the name must be present, it will not "
         "be used at all. This simply maintains compatability with mkcensusdb" <<
         std::endl;
     exit(1);
@@ -79,13 +94,13 @@ void usage(const char* progName, const std::string& error = std::string()) {
 
 int main(int argc, char* argv[]) {
     // Parse the command line.
-    if (argc != 3)
+    if (argc != 6)
         usage(argv[0]);
-    std::string outputFile = argv[2];
+    std::string outputFile = argv[5];
 
     // Open the input file.
-    std::cout << "Processing: " << argv[1] << std::endl;
-    std::ifstream file(argv[1], std::ios_base::in | std::ios_base::binary);
+    std::cout << "Processing: " << argv[4] << std::endl;
+    std::ifstream file(argv[4], std::ios_base::in | std::ios_base::binary);
     if (! file) {
         std::cerr << "ERROR: Could not open input file: " << argv[1]
             << std::endl;
@@ -114,6 +129,21 @@ int main(int argc, char* argv[]) {
             << outputFile << std::endl;
         std::exit(1);
     }
+#endif
+    // Fill some metadata
+#ifdef QDBM_AS_TOKYOCABINET
+    vlput(db, regina::EnumerationDB::tetTag.c_str(),
+            regina::EnumerationDB::tetTag.length(), argv[1], -1, VL_DDUP);
+    vlput(db, regina::EnumerationDB::orientationTag.c_str(),
+            regina::EnumerationDB::orientationTag.length(),
+            argv[2], -1 /*strlen*/, VL_DDUP);
+    vlput(db, regina::EnumerationDB::hyperbolicTag.c_str(),
+            regina::EnumerationDB::hyperbolicTag.length(),
+            argv[3], -1 /*strlen*/, VL_DDUP);
+#else
+    tcbdbputdup2(db, regina::EnumerationDB::tetTag.c_str(), argv[1]);
+    tcbdbputdup2(db, regina::EnumerationDB::orientationTag.c_str(), argv[2]);
+    tcbdbputdup2(db, regina::EnumerationDB::hyperbolicTag.c_str(), argv[3]);
 #endif
 
     // Fill the database with the key-value pairs generated from the
