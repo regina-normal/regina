c95ce6334d565b56416b1803c9428ab4b421b1bf
Benjamin Burton <bab@maths.uq.edu.au>
2016-01-26 08:18:17 +1000

    Oops, put the bracket in the right place for adorned labels.

--- ios/packets/SurfacesCoords.mm
+++ ios/packets/SurfacesCoords.mm
@@ -262,7 +262,7 @@ static NSArray* nonEmbProps = @[@PROP_EULER, @PROP_BDRY, @PROP_LINK];
     
     regina::NTriangulation* ans = s->cutAlong();
     ans->intelligentSimplify();
-    ans->setLabel(self.packet->triangulation()->adornedLabel("Cut #") + std::to_string(selectedRow - 1));
+    ans->setLabel(self.packet->triangulation()->adornedLabel("Cut #" + std::to_string(selectedRow - 1)));
     self.packet->insertChildLast(ans);
     [ReginaHelper viewPacket:ans];
 }
@@ -309,7 +309,7 @@ static NSArray* nonEmbProps = @[@PROP_EULER, @PROP_BDRY, @PROP_LINK];
 
     regina::NTriangulation* ans = s->crush();
     ans->intelligentSimplify();
-    ans->setLabel(self.packet->triangulation()->adornedLabel("Crushed #") + std::to_string(selectedRow - 1));
+    ans->setLabel(self.packet->triangulation()->adornedLabel("Crushed #" + std::to_string(selectedRow - 1)));
     self.packet->insertChildLast(ans);
     [ReginaHelper viewPacket:ans];
 }
