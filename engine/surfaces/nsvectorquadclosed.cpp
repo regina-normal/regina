3da80a39bfa3c61a125aee2a78ef6df78b445bd5
Benjamin Burton <bab@maths.uq.edu.au>
2017-07-18 11:59:40 +0200

    Make NSVectorQuadClosed derive from NSVectorQuad, to avoid reimplementing some of the core quad coordinate machinery.

--- engine/surfaces/nsvectorquadclosed.cpp
+++ engine/surfaces/nsvectorquadclosed.cpp
@@ -48,8 +48,28 @@ NormalSurfaceVector* NSVectorQuadClosed::makeZeroVector(
 
 MatrixInt* NSVectorQuadClosed::makeMatchingEquations(
         const Triangulation<3>* triangulation) {
+    // Enforce our basic preconditions.
+    if (! (triangulation->isOriented() && triangulation->isIdeal() &&
+            triangulation->countBoundaryComponents() == 1 &&
+            triangulation->countVertices() == 1 &&
+            triangulation->vertex(0)->link() == Vertex<3>::TORUS))
+        return 0;
+
+    // We will use SnapPea to build the additional constraint that
+    // enforce closed surfaces.  Before doing anything else, see whether
+    // SnapPea is going to play along.
+    SnapPeaTriangulation snapPea(*triangulation, false);
+    MatrixInt* coeffs = snapPea.slopeEquations();
+    if (! (coeffs && snapPea.isIdenticalTo(*triangulation))) {
+        // SnapPea either couldn't handle it, or else it retriangulated on us.
+        delete coeffs;
+        return 0;
+    }
+
     unsigned long nCoords = 3 * triangulation->size();
-    // One equation per non-boundary edge, plus two per ideal vertex.
+    // One equation per edge, plus two per ideal vertex.
+    // (This code is written a little more generically, in order to
+    // support multiple ideal vertices at some later date.)
     long nEquations = long(triangulation->countEdges() +
         2 * triangulation->countBoundaryComponents());
 
@@ -74,18 +94,7 @@ MatrixInt* NSVectorQuadClosed::makeMatchingEquations(
 
     // Run through each ideal vertex and add the corresponding meridian
     // and longitude equations.
-    SnapPeaTriangulation snapPea(*triangulation, false);
-    MatrixInt* coeffs = snapPea.slopeEquations();
     int i, j;
-    if (! coeffs)
-        return 0; // TODO
-
-    if (! snapPea.isIdenticalTo(*triangulation)) {
-        // SnapPea seems to have changed the triangulation on us.
-        delete coeffs;
-        return 0;
-    }
-
     for (i = 0; i < triangulation->countBoundaryComponents(); ++i) {
         // The coefficients here are differencdes of terms from SnapPy's
         // get_cusp_equation(), which works in native integers; therefore we
@@ -99,225 +108,6 @@ MatrixInt* NSVectorQuadClosed::makeMatchingEquations(
     }
 
     delete coeffs;
-    return ans;
-}
-
-EnumConstraints* NSVectorQuadClosed::makeEmbeddedConstraints(
-        const Triangulation<3>* triangulation) {
-    EnumConstraints* ans = new EnumConstraints(triangulation->size());
-
-    unsigned long base = 0;
-    for (unsigned c = 0; c < ans->size(); ++c) {
-        (*ans)[c].insert((*ans)[c].end(), base);
-        (*ans)[c].insert((*ans)[c].end(), base + 1);
-        (*ans)[c].insert((*ans)[c].end(), base + 2);
-        base += 3;
-    }
-
-    return ans;
-}
-
-namespace {
-    /**
-     * A structure representing a particular end of an edge.
-     */
-    struct EdgeEnd {
-        Edge<3>* edge;
-            /**< The edge under consideration. */
-        int end;
-            /**< The end of the edge under consideration; this is 0 or 1. */
-
-        EdgeEnd() {}
-        EdgeEnd(Edge<3>* newEdge, int newEnd) : edge(newEdge), end(newEnd) {}
-        EdgeEnd(const EdgeEnd& cloneMe) : edge(cloneMe.edge),
-                end(cloneMe.end) {}
-        void operator = (const EdgeEnd& cloneMe) {
-            edge = cloneMe.edge; end = cloneMe.end;
-        }
-    };
-}
-
-NormalSurfaceVector* NSVectorQuadClosed::makeMirror(
-        const Ray& original, const Triangulation<3>* triang) {
-    // We're going to do this by wrapping around each edge and seeing
-    // what comes.
-    unsigned long nRows = 7 * triang->size();
-    NSVectorStandard* ans = new NSVectorStandard(nRows);
-
-    // Set every triangular coordinate in the answer to infinity.
-    // For coordinates about vertices not enjoying infinitely many discs,
-    // infinity will mean "unknown".
-    unsigned long row;
-    int i;
-    for (row = 0; row < nRows; row+=7)
-        for (i = 0; i < 4; i++)
-            ans->setElement(row + i, LargeInteger::infinity);
-    for (row = 0; 7 * row < nRows; ++row)
-        for (i = 0; i < 3; i++)
-            ans->setElement(7 * row + 4 + i, original[3 * row + i]);
-
-    // Run through the vertices and work out the triangular coordinates
-    // about each vertex in turn.
-    std::set<Edge<3>*> usedEdges[2];
-        // usedEdges[i] contains the edges for which we have already
-        // examined end i.
-    LargeInteger min;
-        // The minimum coordinate that has been assigned about this
-        // vertex.
-    std::deque<EdgeEnd> examine;
-    bool broken;
-        // Are the matching equations broken about this edge end?
-    int end;
-    Edge<3>* edge;
-    EdgeEnd current;
-    std::deque<EdgeEmbedding<3>>::const_iterator eembit, backupit,
-        endit, beginit;
-    Tetrahedron<3>* tet;
-    Perm<4> tetPerm, adjPerm;
-    unsigned long tetIndex, adjIndex;
-    LargeInteger expect;
-    for (Triangulation<3>::VertexIterator vit = triang->vertices().begin();
-            vit != triang->vertices().end(); vit++) {
-        usedEdges[0].clear(); usedEdges[1].clear();
-        examine.clear();
-        broken = false;
-
-        // Pick some triangular disc and set it to zero.
-        const VertexEmbedding<3>& vemb = (*vit)->front();
-        row = 7 * vemb.tetrahedron()->index() + vemb.vertex();
-        ans->setElement(row, LargeInteger::zero);
-
-        min = LargeInteger::zero;
-
-        // Mark the three surrounding edge ends for examination.
-        for (i=0; i<4; i++) {
-            if (i == vemb.vertex())
-                continue;
-            edge = vemb.tetrahedron()->edge(
-                Edge<3>::edgeNumber[vemb.vertex()][i]);
-            end = vemb.tetrahedron()->edgeMapping(
-                Edge<3>::edgeNumber[vemb.vertex()][i])[0] == i ? 1 : 0;
-            if (usedEdges[end].insert(edge).second)
-                examine.push_back(EdgeEnd(edge, end));
-        }
-
-        // Cycle through edge ends until we are finished or until the
-        // matching equations are broken.  Each time we pick a value for
-        // a coordinate, add the corresponding nearby edge ends to the
-        // list of edge ends to examine.
-        while ((! broken) && (! examine.empty())) {
-            current = examine.front();
-            examine.pop_front();
-
-            // Run around this edge end.
-            // We know there is a pre-chosen coordinate somewhere; run
-            // forwards and find this.
-            beginit = current.edge->begin();
-            endit = current.edge->end();
-            for (eembit = beginit; eembit != endit; eembit++)
-                if (! (*ans)[7 * (*eembit).tetrahedron()->index() +
-                        (*eembit).vertices()[current.end]].isInfinite())
-                    break;
-
-            // We are now at the first pre-chosen coordinate about this
-            // vertex.  Run backwards from here and fill in all the
-            // holes.
-            backupit = eembit;
-            adjPerm = (*eembit).vertices();
-            adjIndex = (*eembit).tetrahedron()->index();
-            while (eembit != beginit) {
-                eembit--;
-
-                // Work out the coordinate for the disc type at eembit.
-                tet = (*eembit).tetrahedron();
-                tetPerm = (*eembit).vertices();
-                tetIndex = tet->index();
-
-                expect =
-                    (*ans)[7 * adjIndex + adjPerm[current.end]] +
-                    (*ans)[7 * adjIndex + 4 +
-                        quadSeparating[adjPerm[3]][adjPerm[current.end]]] -
-                    (*ans)[7 * tetIndex + 4 +
-                        quadSeparating[tetPerm[2]][tetPerm[current.end]]];
-                ans->setElement(7 * tetIndex + tetPerm[current.end], expect);
-                if (expect < min)
-                    min = expect;
-
-                // Remember to examine the new edge end if appropriate.
-                edge = tet->edge(
-                    Edge<3>::edgeNumber[tetPerm[2]][tetPerm[current.end]]);
-                end = tet->edgeMapping(
-                    Edge<3>::edgeNumber[tetPerm[2]][tetPerm[current.end]])[0]
-                    == tetPerm[2] ? 1 : 0;
-                if (usedEdges[end].insert(edge).second)
-                    examine.push_back(EdgeEnd(edge, end));
-
-                adjPerm = tetPerm;
-                adjIndex = tetIndex;
-            }
-
-            // Now move forwards from the original first pre-chosen
-            // coordinate and fill in the holes from here onwards,
-            // always checking to ensure the
-            // matching equations have not been broken.
-            eembit = backupit;
-            adjPerm = (*eembit).vertices();
-            adjIndex = (*eembit).tetrahedron()->index();
-            for (eembit++; eembit != endit; eembit++) {
-                // Work out the coordinate for the disc type at eembit.
-                tet = (*eembit).tetrahedron();
-                tetPerm = (*eembit).vertices();
-                tetIndex = tet->index();
-
-                expect =
-                    (*ans)[7 * adjIndex + adjPerm[current.end]] +
-                    (*ans)[7 * adjIndex + 4 +
-                        quadSeparating[adjPerm[2]][adjPerm[current.end]]] -
-                    (*ans)[7 * tetIndex + 4 +
-                        quadSeparating[tetPerm[3]][tetPerm[current.end]]];
-                row = 7 * tetIndex + tetPerm[current.end];
-                if ((*ans)[row].isInfinite()) {
-                    ans->setElement(row, expect);
-                    if (expect < min)
-                        min = expect;
-
-                    // Remember to examine the new edge end if appropriate.
-                    edge = tet->edge(
-                        Edge<3>::edgeNumber[tetPerm[3]][tetPerm[current.end]]);
-                    end = tet->edgeMapping(
-                        Edge<3>::edgeNumber[tetPerm[3]][tetPerm[current.end]])[0]
-                        == tetPerm[3] ? 1 : 0;
-                    if (usedEdges[end].insert(edge).second)
-                        examine.push_back(EdgeEnd(edge, end));
-                } else {
-                    // This coordinate has already been set.
-                    // Make sure it's the same value!
-                    if ((*ans)[row] != expect) {
-                        broken = true;
-                        break;
-                    }
-                }
-
-                adjPerm = tetPerm;
-                adjIndex = tetIndex;
-            }
-        }
-
-        // If the matching equations were broken, set every coordinate
-        // to infinity.  Otherwise subtract min from every coordinate to
-        // make the values as small as possible.
-        for (auto& emb : **vit) {
-            row = 7 * emb.tetrahedron()->index() + emb.vertex();
-            if (broken)
-                ans->setElement(row, LargeInteger::infinity);
-            else
-                ans->setElement(row, (*ans)[row] - min);
-        }
-    }
-
-    // Note that there should be no need to remove common factors since
-    // the quad coordinates have not changed and in theory they already
-    // had gcd=1.
     return ans;
 }
 
--- engine/surfaces/nsvectorquadclosed.h
+++ engine/surfaces/nsvectorquadclosed.h
@@ -41,7 +41,7 @@
 #endif
 
 #include "regina-core.h"
-#include "surfaces/nsvectormirrored.h"
+#include "surfaces/nsvectorquad.h"
 
 namespace regina {
 
@@ -59,7 +59,7 @@ struct NormalInfo<NS_QUAD_CLOSED> {
     typedef NormalInfo<NS_STANDARD> Standard;
     typedef NormalInfo<NS_QUAD_CLOSED> Reduced;
     inline static const char* name() {
-        return "Quad closed";
+        return "Closed quad";
     }
     enum {
         almostNormal = 0,
@@ -70,21 +70,44 @@ struct NormalInfo<NS_QUAD_CLOSED> {
 #endif
 
 /**
- * TODO: Document this.  A normal surface vector using quad coordinates.
+ * The vector for a normal surface in an ideal triangulation, expressed using
+ * quad coordinates and enumerated to include closed surfaces only.
  *
- * If there are \a t tetrahedra in the underlying
- * triangulation, there must be precisely 3<i>t</i> coordinates.
+ * The underlying coordinate system is identical to "plain" quad coordinates,
+ * as represented by the parent class NSVectorQuad.  The difference with
+ * NSVectorQuadClosed is that, when \e enumerating surfaces in this class,
+ * it adds linear constraints to ensure that only closed normal surfaces
+ * are obtained (as opposed to spun-normal surfaces).  The resulting
+ * solution space is the space Q_0 as described in "Computing closed
+ * essential surfaces in knot complements", by Burton, Coward and Tillmann,
+ * in SCG ’13: Proceedings of the 29th Annual Symposium on Computational
+ * Geometry, ACM, 2013, pp. 405–414.
+ *
+ * Note that, if a vertex surface in quad coordinates is closed, then it
+ * will also be a vertex surface in this system of "closed quad coordinates".
+ * However, the converse is not true: a vertex surface in closed quad
+ * coordinates need not be a vertex in "plain" quad coordinates.
+ *
+ * Recall that, in quad coordinates, if there are \a t tetrahedra in the
+ * underlying triangulation then there are precisely 3<i>t</i> coordinates.
  * The first three coordinates will be for the first tetrahedron, the
  * next three for the second tetrahedron and so on.  For each
  * tetrahedron, the three individual coordinates represent the
  * number of quadrilateral discs of type 0, 1 and 2
  * (see NormalSurface::quads()).
  *
+ * \pre This coordinate system can only be used with a limited class of
+ * triangulations, as documented in the coordinate constant NS_QUAD_CLOSED.
+ * Currently, such triangulations \e must be oriented and ideal, with precisely
+ * one torus cusp and no other boundary components or internal vertices.
+ * Moreover, SnapPea must be able to work with them without retriangulating
+ * (though this should follow from the other constraints).
+ *
  * \ifacespython Not present.
  */
-class REGINA_API NSVectorQuadClosed : public NSVectorMirrored {
+class REGINA_API NSVectorQuadClosed : public NSVectorQuad {
     REGINA_NORMAL_SURFACE_FLAVOUR(NSVectorQuadClosed, NS_QUAD_CLOSED,
-        NSVectorMirrored);
+        NSVectorQuad);
 
     public:
         /**
@@ -101,21 +124,9 @@ class REGINA_API NSVectorQuadClosed : public NSVectorMirrored {
          */
         NSVectorQuadClosed(const Vector<LargeInteger>& cloneMe);
 
-        static NormalSurfaceVector* makeMirror(const Ray& original,
-            const Triangulation<3>* triang);
-        virtual NormalSurfaceVector* makeMirror(const Triangulation<3>* triang)
-            const;
-
-        virtual const Vertex<3>* isVertexLink(const Triangulation<3>* triang) const;
-
-        virtual LargeInteger octs(size_t tetIndex,
-            int octType, const Triangulation<3>* triang) const;
-
         static NormalSurfaceVector* makeZeroVector(
             const Triangulation<3>* triangulation);
         static MatrixInt* makeMatchingEquations(
-            const Triangulation<3>* triangulation);
-        static EnumConstraints* makeEmbeddedConstraints(
             const Triangulation<3>* triangulation);
 };
 
@@ -123,28 +134,13 @@ class REGINA_API NSVectorQuadClosed : public NSVectorMirrored {
 
 // Inline functions for NSVectorQuadClosed
 
-inline NSVectorQuadClosed::NSVectorQuadClosed(
-        size_t length) : NSVectorMirrored(length) {
+inline NSVectorQuadClosed::NSVectorQuadClosed(size_t length) :
+        NSVectorQuad(length) {
 }
+
 inline NSVectorQuadClosed::NSVectorQuadClosed(
         const Vector<LargeInteger>& cloneMe) :
-        NSVectorMirrored(cloneMe) {
-}
-
-inline NormalSurfaceVector* NSVectorQuadClosed::makeMirror(
-        const Triangulation<3>* triang) const {
-    return makeMirror(coords(), triang);
-}
-
-inline const Vertex<3>* NSVectorQuadClosed::isVertexLink(
-        const Triangulation<3>*) const {
-    // Quad space does not contain vertex links at all.
-    return 0;
-}
-
-inline LargeInteger NSVectorQuadClosed::octs(
-        size_t, int, const Triangulation<3>*) const {
-    return Ray::zero;
+        NSVectorQuad(cloneMe) {
 }
 
 } // namespace regina
