69a53812430e67f2973abc04810f5cad1afdd3e1
Benjamin Burton <bab@maths.uq.edu.au>
2017-09-28 12:48:33 +1000

    Add basic boost.graph routines to test suite.

---
+++ testsuite/dim3/graph.cpp
@@ -0,0 +1,107 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Computational Engine                                                  *
+ *                                                                        *
+ *  Copyright (c) 1999-2017, Ben Burton                                   *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  As an exception, when this program is distributed through (i) the     *
+ *  App Store by Apple Inc.; (ii) the Mac App Store by Apple Inc.; or     *
+ *  (iii) Google Play by Google Inc., then that store may impose any      *
+ *  digital rights management, device limits and/or redistribution        *
+ *  restrictions that are required by its terms of service.               *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public             *
+ *  License along with this program; if not, write to the Free            *
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,       *
+ *  MA 02110-1301, USA.                                                   *
+ *                                                                        *
+ **************************************************************************/
+
+#include <sstream>
+#include <cppunit/extensions/HelperMacros.h>
+#include "triangulation/dim3.h"
+#include "triangulation/graph.h"
+#include "testsuite/dim3/testtriangulation.h"
+#include <boost/graph/graph_concepts.hpp>
+#include <boost/graph/bandwidth.hpp>
+
+using regina::Triangulation;
+
+void testConcepts() {
+    // Compile-time test.
+    boost::function_requires<boost::GraphConcept<regina::Triangulation<3>>>();
+    boost::function_requires<boost::VertexListGraphConcept<regina::Triangulation<3>>>();
+    boost::function_requires<boost::EdgeListGraphConcept<regina::Triangulation<3>>>();
+    boost::function_requires<boost::AdjacencyGraphConcept<regina::Triangulation<3>>>();
+    boost::function_requires<boost::IncidenceGraphConcept<regina::Triangulation<3>>>();
+    boost::function_requires<boost::BidirectionalGraphConcept<regina::Triangulation<3>>>();
+}
+
+class DualGraph3Test : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(DualGraph3Test);
+
+    CPPUNIT_TEST(bandwidth);
+
+    CPPUNIT_TEST_SUITE_END();
+
+    private:
+        Triangulation<3> lens13_3;
+            /**< A layered lens space.
+                 The dual graph is a double-ended chain through vertices
+                 0,1,2,3 in order. */
+        Triangulation<3> aug;
+            /**< A triangulation with no non-trivial symmetries whatsoever.
+                 The dual graph has triangles (4,5,6), (4,5,7) and (5,6,8),
+                 a double edge (8,9), and a loop at 9. */
+        Triangulation<3> ball;
+            /**< A standalone tetrahedron. */
+
+    public:
+        void setUp() {
+            lens13_3.insertLayeredLensSpace(13, 3);
+            lens13_3.setLabel("L(13,3)");
+
+            aug.insertAugTriSolidTorus(3, -1, 5, -3, 2, -1);
+            aug.setLabel("Aug((3,-1), (5,-3), (2,-1))");
+
+            ball.newTetrahedron();
+            ball.setLabel("Ball");
+        }
+
+        void tearDown() {
+        }
+
+        void testBandwidth(const Triangulation<3>& t, long bandwidth) {
+            long result = boost::bandwidth(t);
+            if (result != bandwidth) {
+                std::ostringstream msg;
+                msg << "Bandwidth of " << t.label() <<
+                    " gives " << result << ", not " << bandwidth << ".";
+                CPPUNIT_FAIL(msg.str());
+            }
+        }
+
+        void bandwidth() {
+            testBandwidth(lens13_3, 1);
+            testBandwidth(aug, 3);
+            testBandwidth(ball, 0);
+        }
+};
+
+void addDualGraph3(CppUnit::TextUi::TestRunner& runner) {
+    runner.addTest(DualGraph3Test::suite());
+}
+
--- testsuite/testsuite.cpp
+++ testsuite/testsuite.cpp
@@ -131,6 +131,7 @@ void populateTests(CppUnit::TextTestRunner& runner) {
     addElementaryMoves(runner);
     addConnectedSumDecomp(runner);
     addIsomorphism3(runner);
+    addDualGraph3(runner);
     addHomologicalData(runner);
 
     // 4-manifold triangulations:
