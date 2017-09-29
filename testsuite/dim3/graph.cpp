9f7d69f3b5d5bc77f3244adcbac4856fcfec0796
Benjamin Burton <bab@maths.uq.edu.au>
2017-09-29 14:34:28 +1000

    Some more graph tests.

--- testsuite/dim3/graph.cpp
+++ testsuite/dim3/graph.cpp
@@ -40,6 +40,7 @@
 #include <sstream>
 #include <cppunit/extensions/HelperMacros.h>
 #include "triangulation/dim3.h"
+#include "triangulation/example3.h"
 #include "triangulation/graph.h"
 #include "testsuite/dim3/testtriangulation.h"
 
@@ -64,6 +65,8 @@ class DualGraph3Test : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE_END();
 
     private:
+        Triangulation<3> empty;
+            /**< An empty triangulation. */
         Triangulation<3> lens13_3;
             /**< A layered lens space.
                  The dual graph is a double-ended chain through vertices
@@ -72,19 +75,39 @@ class DualGraph3Test : public CppUnit::TestFixture {
             /**< A triangulation with no non-trivial symmetries whatsoever.
                  The dual graph has triangles (4,5,6), (4,5,7) and (5,6,8),
                  a double edge (8,9), and a loop at 9. */
+        Triangulation<3> weeks;
+            /**< The ready-made triangulation of the Weeks manifold. */
         Triangulation<3> ball;
             /**< A standalone tetrahedron. */
+        Triangulation<3> disconnected;
+            /**< A disjoint union of a 6-tetrahedron loop and a
+                 4-tetrahedron chain. */
 
     public:
+        void copyAndDelete(Triangulation<3>& dest, Triangulation<3>* source) {
+            dest.insertTriangulation(*source);
+            dest.setLabel(source->label());
+            delete source;
+        }
+
         void setUp() {
+            empty.setLabel("Empty");
+
             lens13_3.insertLayeredLensSpace(13, 3);
             lens13_3.setLabel("L(13,3)");
 
             aug.insertAugTriSolidTorus(3, -1, 5, -3, 2, -1);
             aug.setLabel("Aug((3,-1), (5,-3), (2,-1))");
 
+            copyAndDelete(weeks, regina::Example<3>::weeks());
+
             ball.newTetrahedron();
             ball.setLabel("Ball");
+
+            disconnected.insertLayeredSolidTorus(4, 7);
+            disconnected.tetrahedron(disconnected.size() - 1)->unjoin(0);
+            disconnected.insertLayeredLoop(6, true);
+            disconnected.setLabel("Loop(6) U Chain(4)");
         }
 
         void tearDown() {
@@ -102,9 +125,12 @@ class DualGraph3Test : public CppUnit::TestFixture {
         }
 
         void bandwidth() {
+            testBandwidth(empty, 0);
             testBandwidth(lens13_3, 1);
             testBandwidth(aug, 3);
+            testBandwidth(weeks, 4);
             testBandwidth(ball, 0);
+            testBandwidth(disconnected, 5);
         }
 
         void testBipartite(const Triangulation<3>& t, bool expected) {
@@ -119,9 +145,12 @@ class DualGraph3Test : public CppUnit::TestFixture {
         }
 
         void bipartite() {
+            testBipartite(empty, true);
             testBipartite(lens13_3, false);
             testBipartite(aug, false);
+            testBipartite(weeks, false);
             testBipartite(ball, true);
+            testBipartite(disconnected, true);
         }
 };
 
