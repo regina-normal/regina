96bda7a581bb8b190acf79c1ccaa4023b8f4a5eb
Benjamin Burton <bab@maths.uq.edu.au>
2018-03-06 18:12:58 +1000

    Oops, thanks Ryan. Fixed typo.

--- engine/triangulation/dim2/triangle2.h
+++ engine/triangulation/dim2/triangle2.h
@@ -55,7 +55,7 @@ namespace regina {
  */
 
 /**
- * Represents a tetrahedron within a 2-manifold triangulation.
+ * Represents a triangle within a 2-manifold triangulation.
  *
  * This class is typically referred to by the aliases Simplex<2> or
  * Triangle<2> (or, for Python users, \c Simplex2 and \c Triangle2).
