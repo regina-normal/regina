27fb62d45e84f1495021f99c2c365c19c5670b93
Benjamin Burton <bab@maths.uq.edu.au>
2018-04-04 18:55:22 +1000

    Copyright update 2018, stage 2.

--- python/modulemain.cpp
+++ python/modulemain.cpp
@@ -68,7 +68,7 @@ namespace {
     std::string welcome() {
         return std::string(PACKAGE_STRING) +
             "\nSoftware for low-dimensional topology" +
-            "\nCopyright (c) 1999-2017, The Regina development team";
+            "\nCopyright (c) 1999-2018, The Regina development team";
     }
 }
 
