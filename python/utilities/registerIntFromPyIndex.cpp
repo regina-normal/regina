ecdc8181c1059ed5e40306dade107cb69d2d472b
Matthias Goerner <enischte@gmail.com>
2018-05-09 21:21:24 -0700

    Some tweaks to from_python conversion of objects providing __index__. Check that the object has an __index__ method. Use size_t instead of unsigned long to mirror the declaration of methods such as simplex(size_t). Add comments.

--- python/utilities/registerIntFromPyIndex.cpp
+++ python/utilities/registerIntFromPyIndex.cpp
@@ -30,6 +30,20 @@
  *                                                                        *
  **************************************************************************/
 
+// Without the from-python conversion we register here, a user could
+// not type a command like "T.simplex(0)" into Sage. The reason is
+// that the Sage preparser turns it into "T.simplex(Integer(0))" where
+// Integer is a sage.rings.integer.Integer.
+//
+// Luckily, such an Integer follows PEP 357 and provides an __index__
+// method. This file implements a from-python conversion for integer types
+// checking whether the python object has an __index__ method returning an
+// integer.
+//
+// This conversion is not registered by default. It needs to be registered
+// explicitly with engine._registerIntFromPyIndex() in __init__.py when
+// we detect regina is run from sage.
+
 #include <boost/python.hpp>
 #include <boost/optional.hpp>
 
@@ -38,11 +52,22 @@ using namespace boost::python;
 namespace regina {
 namespace python {
 
+// from_python conversion of python objects implementing PEP 357 to integer
+// type T. Calling the c'tor will register the conversion with boost::python.
 template<typename T>
 struct register_int_from_py_index {
 
+    // Checks whether the given value can be held by the integer type T.
     static
     bool _in_range(const long value) {
+        // Note that value < std::numeric_limits<T>::min() is not working
+        // correctly when T is unsigned and has the same number of bits
+        // than value.
+        // This is because when comparing an unsigned and signed number with
+        // the same number of bits, C will interpret the signed number as
+        // unsigned number with the same number of bits, so the negative
+        // number becomes a really large unsigned number.
+        // Thus, we are branching here:
         if (not std::numeric_limits<T>::is_signed) {
             if (value < 0) {
                 return false;
@@ -59,10 +84,14 @@ struct register_int_from_py_index {
         return true;
     }
 
+    // Try to convert a python object to an integer of type T.
+    // Return empty boost::optional if python object is not holding
+    // the right type or the number is too big.
     static
     boost::optional<T> _convert_py_int(PyObject * obj) {
+        // Python 2 has two integer types, one holding a C long and
+        // one holding arbitrary precision.
         if (PyInt_Check(obj)) {
-            // No error checking
             const long value = PyInt_AS_LONG(obj);
             if (_in_range(value)) {
                 return value;
@@ -81,23 +110,32 @@ struct register_int_from_py_index {
         
         return boost::none;
     }
-    
+
+    // Get an integer from a python object by calling its __index__ method
     static
     boost::optional<T> _convert_py_index(PyObject * const obj) {
         if (not obj) {
             return boost::none;
         }
-        
+
+        // Check whether it has __index__
+        if (not PyObject_HasAttrString(obj, "__index__")) {
+            return boost::none;
+        }
+
+        // Get __index__
         PyObject * const attr = PyObject_GetAttrString(obj, "__index__");
         if (not PyCallable_Check(attr)) {
             return boost::none;
         }
-        
+
+        // Arguments for the call to __index__
         PyObject * const args = PyTuple_New(0);
         if (not args) {
             return boost::none;
         }
-        
+
+        // Call __index__
         PyObject * const result = PyObject_CallObject(attr, args);
         Py_DECREF(args);
         
@@ -105,13 +143,23 @@ struct register_int_from_py_index {
             return boost::none;
         }
 
+        // Extract integer from result of call
         const boost::optional<T> value = _convert_py_int(result);
         
         Py_DECREF(result);
         
         return value;
     }
-    
+
+    // Implements boost::python convertible concept.
+    //
+    // Note that we say it is not convertible if __index__ does not return
+    // an integer or the integer is too large. Thus, the user will see in
+    // these cases the typical boost message that the signature did not
+    // match. It might be better, if we actually threw an exception and
+    // gave a python error similar to what, e.g., range(10)[f] does if
+    // f.__index__ returned a non-int:
+    //    TypeError: __index__ returned non-(int,long) (type str)
     static
     void * convertible(PyObject * const obj) {
         if (_convert_py_index(obj)) {
@@ -123,7 +171,8 @@ struct register_int_from_py_index {
 
     using converter_data =
         boost::python::converter::rvalue_from_python_stage1_data;
-    
+
+    // Implements boost::python constructing a C++ int from a python object
     static
     void construct(PyObject * const obj,
                    converter_data * const data) {
@@ -142,6 +191,7 @@ struct register_int_from_py_index {
         data->convertible = storage;
     }
 
+    // Registers the from_python conversion
     register_int_from_py_index() {
         boost::python::converter::registry::push_back(
             convertible,
@@ -155,7 +205,7 @@ struct register_int_from_py_index {
 static
 void _registerIntFromPyIndex()
 {
-    regina::python::register_int_from_py_index<unsigned long>();
+    regina::python::register_int_from_py_index<size_t>();
 }
 
 void addRegisterIntFromPyIndex() {
