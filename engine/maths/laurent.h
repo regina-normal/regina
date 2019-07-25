18764e3f985e64112ad4ec7d8c085a26dde2311d
Benjamin Burton <bab@maths.uq.edu.au>
2019-07-25 18:31:19 +1000

    Doc fix.

--- engine/maths/laurent.h
+++ engine/maths/laurent.h
@@ -54,7 +54,7 @@ namespace regina {
 /**
  * Represents a single-variable Laurent polynomial with coefficients of
  * type \a T.  A Laurent polynomial differs from an ordinary polynomial
- * in that it allows negative exponents (so, unlike the NPolynomial class,
+ * in that it allows negative exponents (so, unlike the Polynomial class,
  * you can represent both <tt>2+3x</tt> and <tt>1+1/x</tt>).
  *
  * The type \a T must represent a ring with no zero divisors.
