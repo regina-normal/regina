59e56977b4ffbd658da05894a65eb170515c4694
Benjamin Burton <bab@maths.uq.edu.au>
2021-03-23 11:01:19 +1000

    Move "make env" into admin/, so that it can run even if cppunit is not available.

---
+++ admin/arch.cpp
@@ -0,0 +1,17 @@
+
+/**
+ * Regina - Software for low-dimensional topology
+ * Administrative utilities
+ *
+ * This program is built and run by "make env".
+ * It displays basic information about the underlying OS and architecture.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "sizeof(int) = " << sizeof(int) << std::endl;
+    std::cout << "sizeof(long) = " << sizeof(long) << std::endl;
+    std::cout << "sizeof(size_t) = " << sizeof(size_t) << std::endl;
+    return 0;
+}
