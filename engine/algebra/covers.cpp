0ce30fd8bf2ae909a93c2d2020ec8334798383c6
Benjamin Burton <bab@maths.uq.edu.au>
2021-07-31 00:11:38 +1000

    Flesh out the documentation for the new RelationScheme machinery.

--- engine/algebra/covers.cpp
+++ engine/algebra/covers.cpp
@@ -212,6 +212,48 @@ namespace {
         };
     };
 
+    /**
+     * This is a helper class for enumerateCovers(), whose purpose is to
+     * speed up the tests for whether a candidate representation of the
+     * generators in the symmetric group S_n respects the group relations.
+     *
+     * The idea is the following:
+     *
+     * - The members rep[0..(nGen-1)] are the representatives of the
+     *   group generators in S_n.
+     *
+     * - The members computed[nGen...] are additional elements of S_n
+     *   that correpsond to formulae (i.e., group expressions) involving
+     *   the generators.  These formulae typically appear as contiguous
+     *   subexpressions of the group relations.
+     *
+     * - In particular, for compCount[d] <= i < compCount[d+1], the expressions
+     *   formulae[i] can all be written in terms of the generators 0..d only.
+     *   We refer to these as the formulae "at depth d".  We compute the
+     *   corresponding permutations as soon as we have chosen representatives
+     *   for generators 0..d, and cache them in computed[i].
+     *
+     * - We allow ourselves to write formulae[j] in terms of formulae[i]
+     *   for i < j (as well as the original group generators appropriate
+     *   for the depth).  This means that we can reuse the computations
+     *   for these subexpressions, which in practice saves significant
+     *   time over the alternative strategy of testing all group relations
+     *   from scratch for every choice of representatives for all generators.
+     *
+     * - In particular, every group relation appears as one of our formulae.
+     *   This means that we can effectively check the group relations as
+     *   we perform the various computations for formulae[i].
+     *
+     * The RelationScheme() constructor is responsible for taking the
+     * group relations and deciding what additional formulae to use, and
+     * in what order.  It assumes that minimaxGenerators() has already
+     * been called on the group presentation.
+     *
+     * Once you have chosen generator d, the function computeFor(d) will
+     * compute the formulae at depth d.  It is assumed that the formulae
+     * at depths < d have already been computed, and that the representatives
+     * for all generators 0..d have already been chosen.
+     */
     template <int index>
     struct RelationScheme {
         size_t nGen;
@@ -220,7 +262,7 @@ namespace {
         Perm<index>* rep;
         Perm<index>* computed;
 
-        // Do we want to use precomputed permutation product tables that are
+        // Do we want to compose permutations using precomputed tables that are
         // generated at runtime?
         //
         // Note that for index <= 5 the Perm<index> class already uses lookup
@@ -238,21 +280,29 @@ namespace {
             nGen = g.countGenerators();
             compCount = new size_t[nGen + 1];
 
+            // nSeen will be the total number of formulae that we have
+            // available to work with, including the group generators as
+            // well as all additional subexpressions that are stored in
+            // formulae[].  In particular, we should always have
+            // nSeen == formulae.size() + nGen.
             long nSeen = nGen;
 
-            // Work out all the pieces we need to compute.
-            // Initially we will give these temporary indices;
-            // everything will be reindexed later when we have decided
-            // on an order of computation.
-            //
-            // As we walk through each relation, currExp[i] holds the
+            // Work out all the additonal formulae we will want to compute.
+            // Initially we will give these temporary indices, which we
+            // store as the values in the maps foundExp[depth].
+            // We will reindex all our formulae later, once we have a
+            // complete set.
+
+            // As we walk through each relation, currExp[i] will hold the
             // maximum length sub-expression ending at the current
             // position, using only generators of index <= i, and
             // *excluding* all trailing terms with generators of index < i.
-            //
             std::vector<GroupExpressionTerm>* currExp =
                 new std::vector<GroupExpressionTerm>[nGen];
 
+            // The formulae that we will compute at depth d are stored
+            // as keys in the map foundExp[d].  The corresponding values
+            // (as noted earlier) are the temporary indices for each formulae.
             typedef std::map<Formula, long, Formula::Compare> ExpressionMap;
             ExpressionMap* foundExp = new ExpressionMap[nGen];
 
@@ -263,14 +313,18 @@ namespace {
 
                 for (const auto& t : r.terms()) {
                     if (t.generator < depth) {
+                        // Start a new subexpression at a smaller depth.
                         depth = t.generator;
                         currExp[depth].push_back({ depth, t.exponent });
                     } else {
+                        // Finish off all subexpressions at depths
+                        // below the newly-seen generator.
                         while (depth < t.generator) {
                             if (currExp[depth].size() == 1 &&
                                     currExp[depth].front().exponent == 1) {
                                 // This expression is just a single symbol.
-                                // Reuse that symbol.
+                                // Reuse that symbol instead of creating
+                                // a new one.
                                 prev = currExp[depth].front().generator;
                                 currExp[depth].clear();
                             } else {
@@ -287,15 +341,21 @@ namespace {
                                     // hadn't seen before.
                                     prev = nSeen++;
                                 } else {
-                                    // We already have plans to compute
-                                    // this same expression.
-                                    // Fetch its corresponding index.
+                                    // We already have this same expression
+                                    // stashed away as a formula from earlier.
+                                    // Reuse its index.
                                     prev = result.first->second;
                                 }
                             }
+                            // Append the term prev^1 to the expression at
+                            // the next higher depth, where prev is the
+                            // index of the formula that we just closed off.
                             ++depth;
                             currExp[depth].push_back({ prev, 1 });
                         }
+                        // Finally, actually append the newly-seen term
+                        // that we are looking at now.
+                        // Note that depth == t.generator at this point.
                         currExp[depth].push_back({ depth, t.exponent });
                     }
                 }
@@ -303,7 +363,9 @@ namespace {
                 // We are guaranteed that the last term in the relation uses
                 // the highest generator index that appears in the relation.
                 //
-                // This means that currExp[depth] is the entire relation.
+                // This means that currExp[depth] is the entire relation,
+                // and the relation does not use any generators with index
+                // greater than depth.
                 //
                 // Again we use a swap and a move to avoid a deep copy
                 // of currExp[depth], and this also clears currExp[depth].
@@ -311,33 +373,41 @@ namespace {
                 tmp.terms.swap(currExp[depth]);
                 auto result = foundExp[depth].emplace(std::move(tmp), nSeen);
                 if (result.second) {
+                    // We have not seen this formula before.
                     ++nSeen;
-
-                    // It's conceivable that this same expression also
-                    // appears in non-relation form.  Currently this
-                    // would mean we are computing it twice.
-                    //
-                    // However: if this *does* happen that it means that
-                    // one relation (this) is a strict subexpression of
-                    // another.  Assuming the group presentation has
-                    // been simplified, this should never happen.  So
-                    // just leave the inefficient here, under the
-                    // assumption that it will never be triggered (but
-                    // it's harmless if it is).
-                } else {
-                    // We already have plans to compute this same relation.
-                    // This can only mean that we have a duplicate relation.
-                    // Just throw this second copy away.
                 }
+
+                // It's conceivable that this same expression also appears in
+                // non-relation form.  Currently this would mean we are
+                // computing it twice, once with isRelation == true, and once
+                // with isRelation == false (which will be treated as different
+                // keys in the foundExp[...] maps).  This is inefficient, but
+                // otherwise harmless.
+                //
+                // However: if this *does* happen that it means that one group
+                // relation is a strict subexpression of another.  Assuming
+                // the group presentation has been simplified, this should not
+                // happen.  So just leave the inefficieny here, under the
+                // assumption that it will never be triggered (but it's
+                // harmless if it is).
             }
 
-            // Reindex all the expressions, using the order that has been
-            // generated by the maps, in order of increasing depth.
-            // This ordering put all relations first at each depth level
+            // Now we have a full set of formulae.
+            // Reindex them, using the order induced by the foundExp[...] maps,
+            // in order of increasing depth.
+            // This ordering will put all relations first at each depth level
             // (so we can backtrack sooner if the relation does not hold).
             //
             // Note that, by construction, each expression only uses other
-            // expressions at a lower depth, which means with smaller indices.
+            // expressions at a lower depth, which means that an
+            // expression with final index i will only every use terms
+            // with indices j < i.  So it will be safe to compute them
+            // in the order formulae[0], formulae[1], ....
+            //
+            // The reindexing is a two-stage process: (1) work out how
+            // the original indices map to the final indices; and then
+            // (2) fix all the terms in all the formulae that *use* these
+            // indices.
             unsigned long* reindex = new unsigned long[nSeen];
             long newIndex = nGen;
             for (depth = 0; depth < nGen; ++depth) {
@@ -358,15 +428,21 @@ namespace {
                 }
             }
 
+            // Finally, record out how many formulae we actually have at
+            // each depth.
             compCount[0] = 0;
             for (depth = 0; depth < nGen; ++depth)
                 compCount[depth + 1] = compCount[depth] +
                     foundExp[depth].size();
 
-            // See if we can use earlier relations in the computations
-            // of later ones.  Work from longer relations down to shorter,
-            // since we want to make sure we can do big substitutions if
-            // any are possible.
+            // At this point we are done, and we could happily finish.
+            // However, we make one more pass in an attempt to simplify
+            // our formulae a little more.
+            //
+            // We see now if it is possible to use the results from earlier
+            // formulae in the computations of later ones.  We work backwards
+            // from the longer relations to the shorter ones, since we want to
+            // prioritise large substitutions if any are possible.
             long outer, inner;
             for (outer = static_cast<long>(formulae.size()) - 1; outer >= 0;
                     --outer) {
@@ -379,18 +455,28 @@ namespace {
             delete[] foundExp;
             delete[] currExp;
 
+            // Now everything else is done: prepare for the big search
+            // for representatives, which is where the *real* work happens.
             rep = new Perm<index>[nGen];
             computed = new Perm<index>[compCount[nGen]];
         }
+
         ~RelationScheme() {
             delete[] compCount;
             delete[] rep;
             delete[] computed;
         }
+
+        /**
+         * Compute the representative in S_n for formulae[piece].
+         *
+         * Returns false if this formulae is one of the group relations
+         * and the resulting computation is not the identity (i.e., the
+         * group relation is not being respected by our current choice
+         * of rep[...]).  In this case we do *not* store the result of
+         * the computation, since we will be backtracking immediately.
+         */
         bool computePiece(size_t piece) {
-            // 0 <= piece < compCount[nGen]
-            // Compute and cache one individual piece.
-            // Return false if it is a relation and not identity.
             Perm<index> comb;
             for (const auto& t : formulae[piece].terms) {
                 Perm<index> gen = (t.generator < nGen ?
@@ -431,18 +517,32 @@ namespace {
             }
         }
 
-        bool computeFor(size_t gen) {
-            // 0 <= n < nGen
-            for (size_t i = compCount[gen]; i < compCount[gen + 1]; ++i)
+        /**
+         * Compute the representative in S_n for all formulae at the
+         * given depth (where 0 <= depth < nGen).
+         *
+         * Returns false if *any* of the corresponding formulae is one of the
+         * group relations and the resulting computation is not the identity
+         * (i.e., the group relations are not being respected).
+         */
+        bool computeFor(size_t depth) {
+            for (size_t i = compCount[depth]; i < compCount[depth + 1]; ++i)
                 if (! computePiece(i))
                     return false;
             return true;
         }
+
+        /**
+         * Dumps the details of this data structure to the given output stream.
+         *
+         * If the total number of generators and formulae exceeds 26,
+         * this routine will start to output junk (since it uses
+         * lower-case letters to denote generators and formulae).
+         *
+         * Since this is a private routine for diagnostic purposes that is
+         * never actually called, we will leave it like this for now.
+         */
         void dump(std::ostream& out) {
-            // If the total number of generators and computed pieces exceeds 26,
-            // this routine will (in its current form) output junk (since it
-            // outputs alphabetic generator symbols).  Since this is a private
-            // routine that is never called, we will leave it like this for now.
             out << "#gen: " << nGen << std::endl;
             out << "compCount:";
             for (int i = 0; i <= nGen; ++i)
