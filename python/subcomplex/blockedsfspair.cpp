593bdc0e2f3fc3d293ecd459eb588adda0b00dc6
Benjamin Burton <bab@maths.uq.edu.au>
2021-08-22 09:46:06 +1000

    Ensure all the subcomplex classes that will need std::optional can support it.

--- python/subcomplex/blockedsfspair.cpp
+++ python/subcomplex/blockedsfspair.cpp
@@ -31,6 +31,7 @@
  **************************************************************************/
 
 #include "../pybind11/pybind11.h"
+#include "../pybind11/stl.h"
 #include "subcomplex/blockedsfspair.h"
 #include "subcomplex/satregion.h"
 #include "triangulation/dim3.h"
--- python/subcomplex/layeredtorusbundle.cpp
+++ python/subcomplex/layeredtorusbundle.cpp
@@ -31,6 +31,7 @@
  **************************************************************************/
 
 #include "../pybind11/pybind11.h"
+#include "../pybind11/stl.h"
 #include "subcomplex/layeredtorusbundle.h"
 #include "subcomplex/txicore.h"
 #include "../helpers.h"
