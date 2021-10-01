c872d4a098533b44d45ec0664e9bd96bf1567fa0
Benjamin Burton <bab@maths.uq.edu.au>
2021-10-01 18:45:24 +1000

    Remove old workaround.

--- engine/file/xml/xmltrireader.h
+++ engine/file/xml/xmltrireader.h
@@ -434,8 +434,7 @@ inline XMLTriangulationReader<dim>::XMLTriangulationReader(
         XMLTreeResolver& res, Packet* parent, bool anon,
         std::string label, std::string id, size_t size, bool permIndex) :
         XMLPacketReader(res, parent, anon, std::move(label), std::move(id)),
-        // tri_(new PacketOf<Triangulation<dim>>()),
-        tri_(new std::remove_pointer_t<decltype(tri_)>()),
+        tri_(new PacketOf<Triangulation<dim>>()),
         permIndex_(permIndex), readSimplices_(0) {
     for ( ; size > 0; --size)
         tri_->newSimplex();
