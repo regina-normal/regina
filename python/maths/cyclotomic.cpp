dea7a0ffe25acb0087a8eb23c085098cdd53b1e4
Benjamin Burton <bab@maths.uq.edu.au>
2021-11-15 08:43:44 +1000

    Make it build.

--- python/maths/cyclotomic.cpp
+++ python/maths/cyclotomic.cpp
@@ -37,6 +37,7 @@
 
 using pybind11::overload_cast;
 using regina::Cyclotomic;
+using regina::Rational;
 
 void addCyclotomic(pybind11::module_& m) {
     auto c = pybind11::class_<Cyclotomic>(m, "Cyclotomic")
