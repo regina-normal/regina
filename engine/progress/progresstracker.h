8c017e9715825353e913be0c96836960a3d9572a
Benjamin Burton <bab@maths.uq.edu.au>
2021-11-22 10:02:20 +1000

    Unify string functions to take a string by value and move it into desc_.

--- engine/progress/progresstracker.h
+++ engine/progress/progresstracker.h
@@ -303,23 +303,7 @@ class ProgressTracker : public ProgressTrackerBase {
          * inclusive, and the weights of \e all stages must sum to 1
          * in total.
          */
-        void newStage(const char* desc, double weight = 1);
-        /**
-         * Used by the writing thread to indicate that it has moved on
-         * to a new stage of processing.  The percentage progress through
-         * the current stage will automatically be set to 100.
-         *
-         * This is typically called by the writing thread.
-         *
-         * @param desc a human-readable description of the new stage.
-         * Typically this begins with a capital and does not include a
-         * final period (full stop).
-         * @param weight the relative weight of this stage as a fraction
-         * of the entire operation.  This weight must be between 0 and 1
-         * inclusive, and the weights of \e all stages must sum to 1
-         * in total.
-         */
-        void newStage(const std::string& desc, double weight = 1);
+        void newStage(std::string desc, double weight = 1);
         /**
          * Used by the writing thread to indicate the level of progress
          * through the current stage.
@@ -426,19 +410,7 @@ class ProgressTrackerOpen : public ProgressTrackerBase {
          * Typically this begins with a capital and does not include a
          * final period (full stop).
          */
-        void newStage(const char* desc);
-        /**
-         * Used by the writing thread to indicate that it has moved on
-         * to a new stage of processing.  The number of steps completed
-         * will be left unchanged.
-         *
-         * This is typically called by the writing thread.
-         *
-         * @param desc a human-readable description of the new stage.
-         * Typically this begins with a capital and does not include a
-         * final period (full stop).
-         */
-        void newStage(const std::string& desc);
+        void newStage(std::string desc);
         /**
          * Used by the writing thread to indicate that one more step has
          * been completed.
@@ -530,18 +502,9 @@ inline double ProgressTracker::percent() {
     return prevPercent_ + currWeight_ * percent_;
 }
 
-inline void ProgressTracker::newStage(const char* desc, double weight) {
+inline void ProgressTracker::newStage(std::string desc, double weight) {
     std::lock_guard<std::mutex> lock(lock_);
-    desc_ = desc;
-    percent_ = 0;
-    prevPercent_ += 100 * currWeight_;
-    currWeight_ = weight;
-    percentChanged_ = descChanged_ = true;
-}
-
-inline void ProgressTracker::newStage(const std::string& desc, double weight) {
-    std::lock_guard<std::mutex> lock(lock_);
-    desc_ = desc;
+    desc_ = std::move(desc);
     percent_ = 0;
     prevPercent_ += 100 * currWeight_;
     currWeight_ = weight;
@@ -585,15 +548,9 @@ inline unsigned long ProgressTrackerOpen::steps() {
     return steps_;
 }
 
-inline void ProgressTrackerOpen::newStage(const char* desc) {
+inline void ProgressTrackerOpen::newStage(std::string desc) {
     std::lock_guard<std::mutex> lock(lock_);
-    desc_ = desc;
-    descChanged_ = true;
-}
-
-inline void ProgressTrackerOpen::newStage(const std::string& desc) {
-    std::lock_guard<std::mutex> lock(lock_);
-    desc_ = desc;
+    desc_ = std::move(desc);
     descChanged_ = true;
 }
 
