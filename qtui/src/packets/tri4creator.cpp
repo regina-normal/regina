33351fdf278d056ffe8e15e7756a14fa6dd71566
Benjamin Burton <bab@maths.uq.edu.au>
2022-05-11 09:58:25 +1000

    Include the K3 surface in the list of examples.

--- qtui/src/packets/tri4creator.cpp
+++ qtui/src/packets/tri4creator.cpp
@@ -76,6 +76,7 @@ namespace {
         ExampleCreator<4>(QObject::tr("4-sphere (simplex boundary)"), &regina::Example<4>::simplicialFourSphere),
         ExampleCreator<4>(QObject::tr("Cappell-Shaneson knot complement"), &regina::Example<4>::cappellShaneson),
         ExampleCreator<4>(QObject::tr("ℂP²"), &regina::Example<4>::cp2),
+        ExampleCreator<4>(QObject::tr("K3 surface"), &regina::Example<4>::k3),
         ExampleCreator<4>(QObject::tr("Product B³ × S¹"), &regina::Example<4>::ballBundle),
         ExampleCreator<4>(QObject::tr("Product S³ × S¹"), &regina::Example<4>::sphereBundle),
         ExampleCreator<4>(QObject::tr("Product S² × S²"), &regina::Example<4>::s2xs2),
