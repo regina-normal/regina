ae4ea0d28ea7d4a97a31f1f746f55a7bbfd99bd6
RAB <raburke_1@outlook.com>
2022-05-12 14:59:55 +1000

    new build + uds changes

--- utils/up-down-simplify.cpp
+++ utils/up-down-simplify.cpp
@@ -8,38 +8,65 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 #include <time.h>
 #include <triangulation/dim4.h>
 
-int main(){
+int main(int argc, char* argv[]){
     
-    int twoFourCap, edgeLowerbound, threeThreeCap;
-
-    std::string initIsoSig;
-    std::cout << "Iso Sig: ";
-    std::cin >> initIsoSig;
+    int twoFourCap,threeThreeCap;
     
-//    std::cout << "Lower bound goal: ";
-//    std::cin >> edgeLowerbound;
-
     std::cout << "Max. number of attempts per run: ";
     std::cin >> twoFourCap;
 
     std::cout << "Max. number of 3-3 moves: ";
     std::cin >> threeThreeCap;
+
+    int numberOfRuns;
+    std::cout << "Number of runs: ";
+    std::cin >> numberOfRuns;
+
+    std::string initIsoSig;
+    std::cout << "Iso Sig: ";
+    std::cin >> initIsoSig;
     
+    std::string isoSigFile = "nilSigsFile.tmp";
+    std::string simplificationDataFile = "nilDataFile.tmp";
+    std::ofstream isoSigs, simplificationData;
+    
+    bool verbose = true;
+
+//    for (int i=1; i<argc; ++i) {
+//        if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--verbose")) {
+//            verbose = true;
+//        }
+//        else {
+            isoSigFile = argv[1];
+            std::string argString = std::string(argv[1]);
+            simplificationDataFile = "Simplification Data - " + argString;
+//        }
+//    }
+    isoSigs.open(isoSigFile);
+    simplificationData.open(simplificationDataFile);
+    
+    int currentRun = 1;
+    
+    while (currentRun <= numberOfRuns) {
+        
     regina::Triangulation<4> init_mfld = regina::Triangulation<4>::fromIsoSig(initIsoSig);
     
     size_t ogEdges, numEdges;
     ogEdges = numEdges = init_mfld.countEdges();
+    auto ogIsoSig = init_mfld.isoSig();
 
-    std::cout << "Original number of edges: " << ogEdges << "\n";
+//    std::cout << "Original number of edges: " << ogEdges << "\n";
+        size_t runOrigEdges = ogEdges;
     
     bool runaway = false;
     
     clock_t time = clock();
-    
-    while (numEdges > ogEdges-1) {
+    size_t edgeLowerbound = ogEdges - 1;
+    while ((numEdges > edgeLowerbound) || (init_mfld.countPentachora() != 2)) {
         bool doIT = true;
 
         int attempts = 1;
@@ -57,7 +84,17 @@ int main(){
             init_mfld.intelligentSimplify();
             numEdges = init_mfld.countEdges();
         
-            std::cout << attempts << ", " << numEdges << std::endl;
+//            if ((init_mfld.isoSig() != ogIsoSig) && (init_mfld.countPentachora() <= 6)) {
+            if (verbose) {
+                std::cout << "\r" << std::setw(3) << attempts << ", " << std::setw(3) << numEdges << std::flush;
+//                std::cout << attempts << ", " << numEdges << std::endl;
+            }
+//                std::cout << init_mfld.isoSig() << "\n";
+//            }
+            
+//            if (std::cin.get() == 's') {
+//                std::cout << init_mfld.isoSig() << std::endl;
+//            }
             
             attempts+=1;
 
@@ -67,28 +104,19 @@ int main(){
                 break;
             }
             
-                for (int i=0; i<threeThreeCap; i++) {
-                    for (regina::Triangle<4>* tri : init_mfld.triangles()) {
-                        if (init_mfld.pachner(tri,true,true)) {
-                            break;
-                        }
+            for (int i=0; i<threeThreeCap; i++) {
+                for (regina::Triangle<4>* tri : init_mfld.triangles()) {
+                    if (init_mfld.pachner(tri,true,true)) {
+                        break;
                     }
                 }
-            
-            if (attempts > twoFourCap) {
-                doIT = false;
-//                for (int i=0; i<threeThreeCap; i++) {
-//                    //std::cout << "3"<<i<< " Trying some 3-3 moves...\n";
-//                    for (regina::Triangle<4>* tri : init_mfld->triangles()) {
-//                        if (init_mfld->pachner(tri,true,true)) {
-//                            break;
-//                        }
-//                    }
-//                }
-//                std::cout << "Perfomed " << threeThreeCap << " 3-3 moves.\n";
-                break;
             }
 
+            if (attempts > twoFourCap) {
+                doIT = false;
+                break;
+            }
+            
         }
     }
 
@@ -96,14 +124,38 @@ int main(){
     double time_taken = ((double)time)/CLOCKS_PER_SEC;
     
     if (!runaway) {
-        std::cout << "Resultant isomorphism signature: " << init_mfld.isoSig() << "\n";
+        std::cout << std::endl;
+        std::cout << init_mfld.isoSig() << "\n";
+        std::cout <<
+        //"s-vector: " <<
+        currentRun << " | " << init_mfld.countVertices() << ", " << runOrigEdges << " -> " << init_mfld.countEdges() << ", " << init_mfld.countPentachora() << " | " << time_taken << "\n";
+        
+        if (argc == 2) {
+            isoSigs << init_mfld.isoSig() << std::endl;
+            
+            simplificationData << currentRun << "," << init_mfld.countVertices() << "," << runOrigEdges << "," << init_mfld.countEdges() << "," << init_mfld.countPentachora() << "," << time_taken << std::endl;
+        }
     }
     else {
         std::cout << "Original isomorphism signature: " << initIsoSig << "\n";
     }
     
-    std::cout << "Time taken: " << time_taken << std::endl;
+//    std::cout << "Time taken (run): " << time_taken << std::endl;
     
+    initIsoSig = init_mfld.isoSig();
+        
+        currentRun+=1;
+    }
+    
+    std::cout << "\007";
+    
+    isoSigs.close();
+    simplificationData.close();
+    
+    if (argc == 1) {
+        remove("nilSigsFile.tmp");
+        remove("nilDataFile.tmp");
+    }
     
     return 0;
 }
