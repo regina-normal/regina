46cda5e22ec2671562c59f85269c77321845e9bf
Benjamin Burton <bab@maths.uq.edu.au>
2022-05-31 20:28:00 +1000

    Remove unused variables.

--- qtui/src/packets/surfacescoordinateui.cpp
+++ qtui/src/packets/surfacescoordinateui.cpp
@@ -172,7 +172,6 @@ QVariant SurfaceModel::data(const QModelIndex& index, int role) const {
         } else if ((surfaces_->isEmbeddedOnly() && index.column() == 6) ||
                 ((! surfaces_->isEmbeddedOnly()) && index.column() == 4)) {
             const regina::Vertex<3>* v;
-            std::pair<const regina::Edge<3>*, const regina::Edge<3>*> e;
 
             if ((v = s.isVertexLink()))
                 return tr("Vertex %1").arg(v->index());
