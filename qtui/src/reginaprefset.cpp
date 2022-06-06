77b8d344e59d62ad421f4809be1b92bd5bf37a9e
Benjamin Burton <bab@maths.uq.edu.au>
2022-06-06 17:10:13 +1000

    Make it build for Qt5.

--- qtui/src/reginaprefset.cpp
+++ qtui/src/reginaprefset.cpp
@@ -51,6 +51,9 @@
 #include <QDesktopServices>
 #include <QSettings>
 #include <QUrl>
+#if QT_VERSION < QT_VERSION_CHECK(6,0,0)
+#include <QTextCodec>
+#endif
 
 namespace {
     const QString INACTIVE("## INACTIVE ##");
