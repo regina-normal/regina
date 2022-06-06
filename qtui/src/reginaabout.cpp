b78eaa763dfd79d3019d0d5c921b8db7d605f3e0
Benjamin Burton <bab@maths.uq.edu.au>
2022-06-06 16:01:42 +1000

    Drop setMargin(), which is gone from Qt6; the default margin of 11px is fine here.

--- qtui/src/reginaabout.cpp
+++ qtui/src/reginaabout.cpp
@@ -219,7 +219,6 @@ ReginaAbout::ReginaAbout(QWidget* parent) :
 
     auto* titleGrid = new QGridLayout(title);
     titleGrid->setColumnStretch(1, 1);
-    titleGrid->setMargin(6);
 
     auto* titleIcon = new QLabel(this);
     titleIcon->setPixmap(IconCache::icon(IconCache::regina).pixmap(64));
