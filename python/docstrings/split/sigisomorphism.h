fd3e9fdfd1d42f137fd977f9f44dfa0d11b539f6
Benjamin Burton <bab@maths.uq.edu.au>
2022-10-11 22:35:27 +1000

    Oops, add missing files.

---
+++ python/docstrings/split/sigisomorphism.h
@@ -0,0 +1,200 @@
+/*
+  This file contains docstrings for use in the Python bindings.
+  Do not edit! They were automatically extracted by pybind11_mkdoc.
+ */
+
+#if defined(__GNUG__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#endif
+
+namespace regina::python::doc {
+
+
+// Docstring regina::python::doc::swap
+static const char *swap =
+R"doc(Swaps the contents of the given partial isomorphisms.
+
+This global routine simply calls SigPartialIsomorphism::swap(); it is
+provided so that SigPartialIsomorphism meets the C++ Swappable
+requirements.
+
+Parameter ``a``:
+    the first partial isomorphism whose contents should be swapped.
+
+Parameter ``b``:
+    the second partial isomorphism whose contents should be swapped.)doc";
+
+namespace SigPartialIsomorphism_ {
+
+// Docstring regina::python::doc::SigPartialIsomorphism_::SigPartialIsomorphism
+static const char *SigPartialIsomorphism =
+R"doc(Creates a new partial isomorphism that maps no cycles or symbols. This
+empty isomorphism is designed to be extended at some later point.
+
+Parameter ``newDir``:
+    positive if this isomorphism specifies that all cycles are
+    reversed, or negative if this isomorphism specifies that all
+    cycles keep their original direction.)doc";
+
+// Docstring regina::python::doc::SigPartialIsomorphism_::SigPartialIsomorphism_2
+static const char *SigPartialIsomorphism_2 =
+R"doc(Creates a copy of the given partial isomorphism.
+
+Parameter ``iso``:
+    the partial isomorphism to copy.)doc";
+
+// Docstring regina::python::doc::SigPartialIsomorphism_::__eq
+static const char *__eq =
+R"doc(Determines whether this and the given partial isomorphism are
+identical.
+
+Parameter ``other``:
+    the partial isomorphism to compare with this.
+
+Returns:
+    ``True`` if and only if this and *other* are identical.)doc";
+
+// Docstring regina::python::doc::SigPartialIsomorphism_::__ne
+static const char *__ne =
+R"doc(Determines whether this and the given partial isomorphism are not
+identical.
+
+Parameter ``other``:
+    the partial isomorphism to compare with this.
+
+Returns:
+    ``True`` if and only if this and *other* are not identical.)doc";
+
+// Docstring regina::python::doc::SigPartialIsomorphism_::compareWith
+static const char *compareWith =
+R"doc(Lexicographically compares the results of applying this and the given
+isomorphism to the given signature.
+
+Comparisons are done on a cycle-by-cycle basis; comparisons within a
+cycle are done as described by Signature::cycleCmp(). Comparison will
+not proceed beyond the cycles mapped by this partial isomorphism.
+
+Precondition:
+    the given partial isomorphism maps at least as many cycles and
+    symbols as this partial isomorphism.
+
+Parameter ``sig``:
+    the signature to which both this and the given isomorphism will be
+    applied.
+
+Parameter ``other``:
+    the isomorphism to compare with this isomorphism.
+
+Parameter ``fromCycleGroup``:
+    the first cycle group whose images should be examined. If it is
+    already known that the cycle images for the first *k* cycle groups
+    are identical under both isomorphisms, *k* should be passed in
+    this parameter. This parameter should not exceed the number of
+    cycle groups whose cycles are mapped by this partial isomorphism.
+
+Returns:
+    -1, 1 or 0 if the image of the given signature under this
+    isomorphism is lexicographically less than, greater than or equal
+    to its image under the given isomorphism respectively.)doc";
+
+// Docstring regina::python::doc::SigPartialIsomorphism_::compareWithIdentity
+static const char *compareWithIdentity =
+R"doc(Lexicographically compares the results of applying this and the
+identity isomorphism to the given signature.
+
+This routine behaves identically to compareWith(), except that it does
+not take a second isomorphism to compare against. See compareWith()
+for further details.
+
+Parameter ``sig``:
+    the signature to which this isomorphism will be applied.
+
+Parameter ``fromCycleGroup``:
+    the first cycle group whose images should be examined. If it is
+    already known that the cycle images for the first *k* cycle groups
+    are identical under both this and the identity isomorphism, *k*
+    should be passed in this parameter. This parameter should not
+    exceed the number of cycle groups whose cycles are mapped by this
+    partial isomorphism.
+
+Returns:
+    -1, 1 or 0 if the image of the given signature under this
+    isomorphism is lexicographically less than, greater than or equal
+    to its image under the identity isomorphism respectively.)doc";
+
+// Docstring regina::python::doc::SigPartialIsomorphism_::makeCanonical
+static const char *makeCanonical =
+R"doc(Rearranges the cycle images so that this isomorphism when applied to
+the given signature produces a new signature that is in canonical
+form.
+
+The result of this routine is dependent upon the symbol map defined by
+this isomorphism (this symbol map will not be changed).
+
+Parameter ``sig``:
+    the signature to which this isomorphism will be applied.
+
+Parameter ``fromCycleGroup``:
+    the first cycle group whose images may be rearranged. If it is
+    already known that the cycle images for the first *k* cycle groups
+    are correct, *k* should be passed in this parameter. This
+    parameter should not exceed the number of cycle groups whose
+    cycles are mapped by this partial isomorphism.)doc";
+
+// Docstring regina::python::doc::SigPartialIsomorphism_::swap
+static const char *swap =
+R"doc(Swaps the contents of this and the given partial isomorphism.
+
+It does not matter if this and the given partial isomorphism work with
+different numbers of symbols or different cycle structures; if they do
+then they will both be adjusted accordingly.
+
+Parameter ``other``:
+    the partial isomorphism whose contents are to be swapped with
+    this.)doc";
+
+}
+
+namespace SigPartialIsomorphism_::ShorterCycle_ {
+
+// Docstring regina::python::doc::SigPartialIsomorphism_::ShorterCycle_::ShorterCycle
+static const char *ShorterCycle =
+R"doc(Creates a new comparison function.
+
+Parameter ``newSig``:
+    the signature containing the cycles that this function will
+    examine.
+
+Parameter ``newIso``:
+    the partial isomorphism to apply to the cycles before they are
+    compared.)doc";
+
+// Docstring regina::python::doc::SigPartialIsomorphism_::ShorterCycle_::__call
+static const char *__call =
+R"doc(Determines whether the image of one cycle is lexicographically less
+than the image of another. See the class notes for further details on
+how this comparison is done.
+
+Parameter ``cycle1``:
+    the index of the first cycle to examine; this must be less than
+    the total number of cycles mapped by the isomorphism concerned and
+    less than the total number of cycles in the signature concerned.
+
+Parameter ``cycle2``:
+    the index of the second cycle to examine; this must be less than
+    the total number of cycles mapped by the isomorphism concerned and
+    less than the total number of cycles in the signature concerned.
+
+Returns:
+    ``True`` if and only if the image of the first cycle is less than
+    the image of the second cycle.)doc";
+
+}
+
+} // namespace regina::python::doc
+
+#if defined(__GNUG__)
+#pragma GCC diagnostic pop
+#endif
+
---
+++ python/docstrings/split/signature.h
@@ -0,0 +1,186 @@
+/*
+  This file contains docstrings for use in the Python bindings.
+  Do not edit! They were automatically extracted by pybind11_mkdoc.
+ */
+
+#if defined(__GNUG__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#endif
+
+namespace regina::python::doc {
+
+
+// Docstring regina::python::doc::Signature
+static const char *Signature =
+R"doc(Represents a signature of a splitting surface in a closed 3-manifold
+triangulation.
+
+A *splitting surface* is (for these purposes) a compact normal surface
+consisting of precisely one quad per tetrahedron and no other normal
+(or almost normal) discs.
+
+A *signature* of order *n* is a string consisting of 2*n* letters
+arranged into cycles, where *n* is the number of quads in the
+splitting surface. From a signature, the corresponding splitting
+surface and then the entire 3-manifold triangulation can be recreated.
+
+A signature of order *n* uses the first *n* letters of the alphabet,
+each precisely twice. Case is important; the meaning of a letter
+changes according to whether it appears in upper-case or lower-case.
+
+Each letter represents an individual quadrilateral (the two
+occurrences of the letter representing the quadrilateral's two sides).
+Each cycle represents a chain of quadrilaterals joined together in the
+splitting surface. The case of a letter represents in which direction
+a quadrilateral is traversed within a cycle.
+
+Cycles are arranged into *cycle groups*, where a cycle group consists
+of a series of consecutive cycles all of the same length.
+
+An example of a signature is ``(abc)(a)(b)(c)``. This signature is of
+order 3 and contains two cycle groups, the first being ``(abc)`` and
+the second being ``(a)(b)(c)``.
+
+A signature cannot represent a splitting surface with more than 26
+quadrilaterals.
+
+For further details on splitting surfaces and their signatures,
+consult *Minimal triangulations and normal surfaces*, Burton, PhD
+thesis, available from the Regina website.
+
+This class implements C++ move semantics and adheres to the C++
+Swappable requirement. It is designed to avoid deep copies wherever
+possible, even when passing or returning objects by value.)doc";
+
+// Docstring regina::python::doc::swap
+static const char *swap =
+R"doc(Swaps the contents of the given signatures.
+
+This global routine simply calls Signature::swap(); it is provided so
+that Signature meets the C++ Swappable requirements.
+
+Parameter ``a``:
+    the first signature whose contents should be swapped.
+
+Parameter ``b``:
+    the second signature whose contents should be swapped.)doc";
+
+namespace Signature_ {
+
+// Docstring regina::python::doc::Signature_::Signature
+static const char *Signature =
+R"doc(Creates a new signature by parsing the given signature string.
+
+Punctuation characters in the given string will be interpreted as
+separating cycles. All whitespace will be ignored.
+
+Examples of valid signatures are ``"(ab)(bC)(Ca)"`` and ``"AAb-
+bc-C"``. See the class notes for further details on what constitutes a
+valid signature.
+
+Exception ``InvalidArgument``:
+    The given string was not a valid signature with a positive number
+    of letters.
+
+Parameter ``str``:
+    a string representation of a splitting surface signature.)doc";
+
+// Docstring regina::python::doc::Signature_::Signature_2
+static const char *Signature_2 =
+R"doc(Creates a new copy of the given signature.
+
+Parameter ``sig``:
+    the signature to copy.)doc";
+
+// Docstring regina::python::doc::Signature_::__eq
+static const char *__eq =
+R"doc(Determines whether this and the given signature are identical.
+
+To be considered identical, it is not enough for two signatures to be
+isomorphic: their cycles and cycle groups must be presented in the
+same order, using the same symbols which must likewise be presented in
+the same order.
+
+If either signature was parsed from a string, any choice of formatting
+(e.g., punctuation and/or whitespace) is irrelevant; only the
+mathematical content of the signatures is important here.
+
+Parameter ``other``:
+    the signature to compare with this.
+
+Returns:
+    ``True`` if and only if this and *other* are identical.)doc";
+
+// Docstring regina::python::doc::Signature_::__ne
+static const char *__ne =
+R"doc(Determines whether this and the given signature are not identical.
+
+To be considered identical, it is not enough for two signatures to be
+isomorphic: their cycles and cycle groups must be presented in the
+same order, using the same symbols which must likewise be presented in
+the same order.
+
+If either signature was parsed from a string, any choice of formatting
+(e.g., punctuation and/or whitespace) is irrelevant; only the
+mathematical content of the signatures is important here.
+
+Parameter ``other``:
+    the signature to compare with this.
+
+Returns:
+    ``True`` if and only if this and *other* are not identical.)doc";
+
+// Docstring regina::python::doc::Signature_::order
+static const char *order =
+R"doc(Returns the order of this signature. The order is the number of quads
+in the corresponding splitting surface.
+
+Returns:
+    the order of this signature.)doc";
+
+// Docstring regina::python::doc::Signature_::str
+static const char *str =
+R"doc(Returns a customised string representation of this signature.
+
+Note that there is also a zero-argument version of str(), inherited
+through the ShortOutput base class. This zero-argument str() make
+sensible default choices for the three arguments required here.
+
+Parameter ``cycleOpen``:
+    the text to write at the beginning of each cycle (such as
+    ``"("``).
+
+Parameter ``cycleClose``:
+    the text to write at the end of each cycle (such as ``")"``).
+
+Parameter ``cycleJoin``:
+    the text to write between each pair of consecutive cycles.)doc";
+
+// Docstring regina::python::doc::Signature_::swap
+static const char *swap =
+R"doc(Swaps the contents of this and the given signature.
+
+It does not matter if this and the given signature use different
+number of symbols, cycles and/or cycle groups; if so then both
+signatures will be adjusted accordingly.
+
+Parameter ``other``:
+    the signature whose contents are to be swapped with this.)doc";
+
+// Docstring regina::python::doc::Signature_::triangulate
+static const char *triangulate =
+R"doc(Returns the 3-manifold triangulation corresponding to this splitting
+surface signature.
+
+Returns:
+    the corresponding triangulation.)doc";
+
+}
+
+} // namespace regina::python::doc
+
+#if defined(__GNUG__)
+#pragma GCC diagnostic pop
+#endif
+
