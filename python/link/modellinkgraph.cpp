339c160e0dc948633e689a1a22a8f78d9f4cb680
Benjamin Burton <bab@maths.uq.edu.au>
2022-10-18 22:04:33 +1000

    Finish docstrings for ModelLinkGraph and related classes, which finishes off link/.

--- python/link/modellinkgraph.cpp
+++ python/link/modellinkgraph.cpp
@@ -34,6 +34,7 @@
 #include "../pybind11/functional.h"
 #include "link/modellinkgraph.h"
 #include "../helpers.h"
+#include "../docstrings/link/modellinkgraph.h"
 
 using pybind11::overload_cast;
 using regina::ModelLinkGraphNode;
@@ -42,81 +43,97 @@ using regina::ModelLinkGraph;
 using regina::ModelLinkGraphCells;
 
 void addModelLinkGraph(pybind11::module_& m) {
-    auto a = pybind11::class_<ModelLinkGraphArc>(m, "ModelLinkGraphArc")
-        .def(pybind11::init<>())
-        .def(pybind11::init<ModelLinkGraphNode*, int>())
-        .def(pybind11::init<const ModelLinkGraphArc&>())
+    RDOC_SCOPE_BEGIN(ModelLinkGraphArc)
+
+    auto a = pybind11::class_<ModelLinkGraphArc>(m, "ModelLinkGraphArc",
+            rdoc_scope)
+        .def(pybind11::init<>(), rdoc::__default)
+        .def(pybind11::init<ModelLinkGraphNode*, int>(), rdoc::__init)
+        .def(pybind11::init<const ModelLinkGraphArc&>(), rdoc::__copy)
         .def("node", &ModelLinkGraphArc::node,
-            pybind11::return_value_policy::reference)
-        .def("arc", &ModelLinkGraphArc::arc)
-        .def("opposite", &ModelLinkGraphArc::opposite)
-        .def("traverse", &ModelLinkGraphArc::traverse)
-        .def("next", &ModelLinkGraphArc::next)
-        .def("prev", &ModelLinkGraphArc::prev)
+            pybind11::return_value_policy::reference, rdoc::node)
+        .def("arc", &ModelLinkGraphArc::arc, rdoc::arc)
+        .def("opposite", &ModelLinkGraphArc::opposite, rdoc::opposite)
+        .def("traverse", &ModelLinkGraphArc::traverse, rdoc::traverse)
+        .def("next", &ModelLinkGraphArc::next, rdoc::next)
+        .def("prev", &ModelLinkGraphArc::prev, rdoc::prev)
         .def("inc", [](ModelLinkGraphArc& a) {
            return a++;
-        })
+        }, rdoc::__inc)
         .def("dec", [](ModelLinkGraphArc& a) {
            return a--;
-        })
-        .def("__bool__", &ModelLinkGraphArc::operator bool)
+        }, rdoc::__dec)
+        .def("__bool__", &ModelLinkGraphArc::operator bool, rdoc::__bool)
     ;
     regina::python::add_output_ostream(a);
-    regina::python::add_eq_operators(a);
+    regina::python::add_eq_operators(a, rdoc::__eq, rdoc::__ne);
 
-    auto n = pybind11::class_<ModelLinkGraphNode>(m, "ModelLinkGraphNode")
-        .def("index", &ModelLinkGraphNode::index)
-        .def("arc", &ModelLinkGraphNode::arc)
-        .def("adj", &ModelLinkGraphNode::adj)
+    RDOC_SCOPE_SWITCH(ModelLinkGraphNode)
+
+    auto n = pybind11::class_<ModelLinkGraphNode>(m, "ModelLinkGraphNode",
+            rdoc_scope)
+        .def("index", &ModelLinkGraphNode::index, rdoc::index)
+        .def("arc", &ModelLinkGraphNode::arc, rdoc::arc)
+        .def("adj", &ModelLinkGraphNode::adj, rdoc::adj)
     ;
     regina::python::add_output(n);
     regina::python::add_eq_operators(n);
 
-    auto g = pybind11::class_<ModelLinkGraph>(m, "ModelLinkGraph")
-        .def(pybind11::init<>())
-        .def(pybind11::init<const regina::Link&>())
-        .def(pybind11::init<const ModelLinkGraph&>())
-        .def("size", &ModelLinkGraph::size)
+    RDOC_SCOPE_SWITCH(ModelLinkGraph)
+
+    auto g = pybind11::class_<ModelLinkGraph>(m, "ModelLinkGraph", rdoc_scope)
+        .def(pybind11::init<>(), rdoc::__default)
+        .def(pybind11::init<const regina::Link&>(), rdoc::__init)
+        .def(pybind11::init<const ModelLinkGraph&>(), rdoc::__copy)
+        .def("size", &ModelLinkGraph::size, rdoc::size)
         .def("node", &ModelLinkGraph::node,
-            pybind11::return_value_policy::reference_internal)
+            pybind11::return_value_policy::reference_internal, rdoc::node)
         .def("nodes", &ModelLinkGraph::nodes,
-            pybind11::keep_alive<0, 1>())
-        .def("swap", &ModelLinkGraph::swap)
-        .def("reflect", &ModelLinkGraph::reflect)
+            pybind11::keep_alive<0, 1>(), rdoc::nodes)
+        .def("swap", &ModelLinkGraph::swap, rdoc::swap)
+        .def("reflect", &ModelLinkGraph::reflect, rdoc::reflect)
         .def("cells", &ModelLinkGraph::cells,
-            pybind11::return_value_policy::reference_internal)
-        .def("findFlype", &ModelLinkGraph::findFlype)
+            pybind11::return_value_policy::reference_internal, rdoc::cells)
+        .def("findFlype", &ModelLinkGraph::findFlype, rdoc::findFlype)
         .def("flype", overload_cast<const ModelLinkGraphArc&,
                 const ModelLinkGraphArc&, const ModelLinkGraphArc&>(
-            &ModelLinkGraph::flype, pybind11::const_))
+            &ModelLinkGraph::flype, pybind11::const_), rdoc::flype)
         .def("flype", overload_cast<const ModelLinkGraphArc&>(
-            &ModelLinkGraph::flype, pybind11::const_))
-        .def("plantri", &ModelLinkGraph::plantri)
+            &ModelLinkGraph::flype, pybind11::const_), rdoc::flype_2)
+        .def("plantri", &ModelLinkGraph::plantri, rdoc::plantri)
         .def("canonicalPlantri", &ModelLinkGraph::canonicalPlantri,
             pybind11::arg("useReflection") = true,
-            pybind11::arg("tight") = false)
-        .def_static("fromPlantri", &ModelLinkGraph::fromPlantri)
+            pybind11::arg("tight") = false,
+            rdoc::canonicalPlantri)
+        .def_static("fromPlantri", &ModelLinkGraph::fromPlantri,
+            rdoc::fromPlantri)
         .def("generateMinimalLinks", &ModelLinkGraph::generateMinimalLinks<
-            const std::function<void(regina::Link&&)>&>)
+            const std::function<void(regina::Link&&)>&>,
+            rdoc::generateMinimalLinks)
     ;
     regina::python::add_output(g);
-    regina::python::add_eq_operators(g);
+    regina::python::add_eq_operators(g, rdoc::__eq, rdoc::__ne);
+
+    regina::python::add_global_swap<ModelLinkGraph>(m, rdoc::global_swap);
 
     regina::python::addListView<decltype(ModelLinkGraph().nodes())>(m);
 
-    auto c = pybind11::class_<ModelLinkGraphCells>(m, "ModelLinkGraphCells")
-        .def("isValid", &ModelLinkGraphCells::isValid)
-        .def("countCells", &ModelLinkGraphCells::countCells)
-        .def("size", &ModelLinkGraphCells::size)
-        .def("arc", &ModelLinkGraphCells::arc)
-        .def("arcs", &ModelLinkGraphCells::arcs)
-        .def("cell", &ModelLinkGraphCells::cell)
-        .def("cellPos", &ModelLinkGraphCells::cellPos)
+    RDOC_SCOPE_SWITCH(ModelLinkGraphCells)
+
+    auto c = pybind11::class_<ModelLinkGraphCells>(m, "ModelLinkGraphCells",
+            rdoc_scope)
+        .def("isValid", &ModelLinkGraphCells::isValid, rdoc::isValid)
+        .def("countCells", &ModelLinkGraphCells::countCells, rdoc::countCells)
+        .def("size", &ModelLinkGraphCells::size, rdoc::size)
+        .def("arc", &ModelLinkGraphCells::arc, rdoc::arc)
+        .def("arcs", &ModelLinkGraphCells::arcs, rdoc::arcs)
+        .def("cell", &ModelLinkGraphCells::cell, rdoc::cell)
+        .def("cellPos", &ModelLinkGraphCells::cellPos, rdoc::cellPos)
     ;
     regina::python::add_output(c);
-    regina::python::add_eq_operators(c);
+    regina::python::add_eq_operators(c, rdoc::__eq, rdoc::__ne);
 
     regina::python::addListView<decltype(ModelLinkGraph().cells().arcs(0))>(m);
 
-    regina::python::add_global_swap<ModelLinkGraph>(m);
+    RDOC_SCOPE_END
 }
