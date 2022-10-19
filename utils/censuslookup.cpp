5b1468803b2108f616c2f8ac6846098e64e99854
Benjamin Burton <bab@maths.uq.edu.au>
2022-10-19 13:11:39 +1000

    Add missing include.

--- utils/censuslookup.cpp
+++ utils/censuslookup.cpp
@@ -30,6 +30,7 @@
  *                                                                        *
  **************************************************************************/
 
+#include <cstring>
 #include <iostream>
 #include "regina-config.h"
 #include "census/census.h"
