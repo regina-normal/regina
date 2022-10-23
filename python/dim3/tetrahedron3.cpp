0788d22ea9aaaacd78f3d15046befbbc6c801593
Benjamin Burton <bab@maths.uq.edu.au>
2022-10-23 16:04:59 +1000

    Docstrings for all dimensions of Simplex.

--- python/dim3/tetrahedron3.cpp
+++ python/dim3/tetrahedron3.cpp
@@ -34,52 +34,67 @@
 #include "triangulation/dim3.h"
 #include "../helpers.h"
 #include "../generic/facehelper.h"
+#include "../docstrings/triangulation/dim3/tetrahedron3.h"
+#include "../docstrings/triangulation/detail/simplex.h"
 
 using pybind11::overload_cast;
 using regina::Tetrahedron;
 
 void addTetrahedron3(pybind11::module_& m) {
-    auto c = pybind11::class_<regina::Simplex<3>>(m, "Simplex3")
-        .def("description", &Tetrahedron<3>::description)
-        .def("setDescription", &Tetrahedron<3>::setDescription)
-        .def("index", &Tetrahedron<3>::index)
+    RDOC_SCOPE_BEGIN(Face)
+    RDOC_SCOPE_BASE(detail::SimplexBase)
+
+    auto c = pybind11::class_<regina::Simplex<3>>(m, "Simplex3", rdoc_scope)
+        .def("description", &Tetrahedron<3>::description, rbase::description)
+        .def("setDescription", &Tetrahedron<3>::setDescription,
+            rbase::setDescription)
+        .def("index", &Tetrahedron<3>::index, rbase::index)
         .def("adjacentTetrahedron", &Tetrahedron<3>::adjacentTetrahedron,
-            pybind11::return_value_policy::reference)
+            pybind11::return_value_policy::reference, rdoc::adjacentTetrahedron)
         .def("adjacentSimplex", &Tetrahedron<3>::adjacentSimplex,
-            pybind11::return_value_policy::reference)
-        .def("adjacentGluing", &Tetrahedron<3>::adjacentGluing)
-        .def("adjacentFace", &Tetrahedron<3>::adjacentFace)
-        .def("adjacentFacet", &Tetrahedron<3>::adjacentFacet)
-        .def("hasBoundary", &Tetrahedron<3>::hasBoundary)
-        .def("join", &Tetrahedron<3>::join)
+            pybind11::return_value_policy::reference, rbase::adjacentSimplex)
+        .def("adjacentGluing", &Tetrahedron<3>::adjacentGluing,
+            rbase::adjacentGluing)
+        .def("adjacentFace", &Tetrahedron<3>::adjacentFace, rdoc::adjacentFace)
+        .def("adjacentFacet", &Tetrahedron<3>::adjacentFacet,
+            rbase::adjacentFacet)
+        .def("hasBoundary", &Tetrahedron<3>::hasBoundary, rbase::hasBoundary)
+        .def("join", &Tetrahedron<3>::join, rbase::join)
         .def("unjoin", &Tetrahedron<3>::unjoin,
-            pybind11::return_value_policy::reference)
-        .def("isolate", &Tetrahedron<3>::isolate)
-        .def("triangulation", &Tetrahedron<3>::triangulation)
+            pybind11::return_value_policy::reference, rbase::unjoin)
+        .def("isolate", &Tetrahedron<3>::isolate, rbase::isolate)
+        .def("triangulation", &Tetrahedron<3>::triangulation,
+            rbase::triangulation)
         .def("component", &Tetrahedron<3>::component,
-            pybind11::return_value_policy::reference)
-        .def("face", &regina::python::face<Tetrahedron<3>, 3, int>)
+            pybind11::return_value_policy::reference, rbase::component)
+        .def("face", &regina::python::face<Tetrahedron<3>, 3, int>, rbase::face)
         .def("vertex", &Tetrahedron<3>::vertex,
-            pybind11::return_value_policy::reference)
+            pybind11::return_value_policy::reference, rbase::vertex)
         .def("edge",
             overload_cast<int>(&Tetrahedron<3>::edge, pybind11::const_),
-            pybind11::return_value_policy::reference)
+            pybind11::return_value_policy::reference, rbase::edge)
         .def("edge",
             overload_cast<int, int>(&Tetrahedron<3>::edge, pybind11::const_),
-            pybind11::return_value_policy::reference)
+            pybind11::return_value_policy::reference, rbase::edge_2)
         .def("triangle", &Tetrahedron<3>::triangle,
-            pybind11::return_value_policy::reference)
-        .def("faceMapping", &regina::python::faceMapping<Tetrahedron<3>, 3>)
-        .def("vertexMapping", &Tetrahedron<3>::vertexMapping)
-        .def("edgeMapping", &Tetrahedron<3>::edgeMapping)
-        .def("triangleMapping", &Tetrahedron<3>::triangleMapping)
-        .def("orientation", &Tetrahedron<3>::orientation)
-        .def("facetInMaximalForest", &Tetrahedron<3>::facetInMaximalForest)
+            pybind11::return_value_policy::reference, rbase::triangle)
+        .def("faceMapping", &regina::python::faceMapping<Tetrahedron<3>, 3>,
+            rbase::faceMapping)
+        .def("vertexMapping", &Tetrahedron<3>::vertexMapping,
+            rbase::vertexMapping)
+        .def("edgeMapping", &Tetrahedron<3>::edgeMapping, rbase::edgeMapping)
+        .def("triangleMapping", &Tetrahedron<3>::triangleMapping,
+            rbase::triangleMapping)
+        .def("orientation", &Tetrahedron<3>::orientation, rbase::orientation)
+        .def("facetInMaximalForest", &Tetrahedron<3>::facetInMaximalForest,
+            rbase::facetInMaximalForest)
         .def_readonly_static("dimension", &Tetrahedron<3>::dimension)
         .def_readonly_static("subdimension", &Tetrahedron<3>::subdimension)
     ;
     regina::python::add_output(c);
     regina::python::add_eq_operators(c);
+
+    RDOC_SCOPE_END
 
     m.attr("Tetrahedron3") = m.attr("Simplex3");
     m.attr("Face3_3") = m.attr("Simplex3");
--- python/dim4/pentachoron4.cpp
+++ python/dim4/pentachoron4.cpp
@@ -34,54 +34,70 @@
 #include "triangulation/dim4.h"
 #include "../helpers.h"
 #include "../generic/facehelper.h"
+#include "../docstrings/triangulation/dim4/pentachoron4.h"
+#include "../docstrings/triangulation/detail/simplex.h"
 
 using pybind11::overload_cast;
 using regina::Pentachoron;
 
 void addPentachoron4(pybind11::module_& m) {
-    auto c = pybind11::class_<regina::Simplex<4>>(m, "Simplex4")
-        .def("description", &Pentachoron<4>::description)
-        .def("setDescription", &Pentachoron<4>::setDescription)
-        .def("index", &Pentachoron<4>::index)
+    RDOC_SCOPE_BEGIN(Face)
+    RDOC_SCOPE_BASE(detail::SimplexBase)
+
+    auto c = pybind11::class_<regina::Simplex<4>>(m, "Simplex4", rdoc_scope)
+        .def("description", &Pentachoron<4>::description, rbase::description)
+        .def("setDescription", &Pentachoron<4>::setDescription,
+            rbase::setDescription)
+        .def("index", &Pentachoron<4>::index, rbase::index)
         .def("adjacentPentachoron", &Pentachoron<4>::adjacentPentachoron,
-            pybind11::return_value_policy::reference)
+            pybind11::return_value_policy::reference, rdoc::adjacentPentachoron)
         .def("adjacentSimplex", &Pentachoron<4>::adjacentSimplex,
-            pybind11::return_value_policy::reference)
-        .def("adjacentGluing", &Pentachoron<4>::adjacentGluing)
-        .def("adjacentFacet", &Pentachoron<4>::adjacentFacet)
-        .def("hasBoundary", &Pentachoron<4>::hasBoundary)
-        .def("join", &Pentachoron<4>::join)
+            pybind11::return_value_policy::reference, rbase::adjacentSimplex)
+        .def("adjacentGluing", &Pentachoron<4>::adjacentGluing,
+            rbase::adjacentGluing)
+        .def("adjacentFacet", &Pentachoron<4>::adjacentFacet,
+            rbase::adjacentFacet)
+        .def("hasBoundary", &Pentachoron<4>::hasBoundary, rbase::hasBoundary)
+        .def("join", &Pentachoron<4>::join, rbase::join)
         .def("unjoin", &Pentachoron<4>::unjoin,
-            pybind11::return_value_policy::reference)
-        .def("isolate", &Pentachoron<4>::isolate)
-        .def("triangulation", &Pentachoron<4>::triangulation)
+            pybind11::return_value_policy::reference, rbase::unjoin)
+        .def("isolate", &Pentachoron<4>::isolate, rbase::isolate)
+        .def("triangulation", &Pentachoron<4>::triangulation,
+            rbase::triangulation)
         .def("component", &Pentachoron<4>::component,
-            pybind11::return_value_policy::reference)
-        .def("face", &regina::python::face<Pentachoron<4>, 4, int>)
+            pybind11::return_value_policy::reference, rbase::component)
+        .def("face", &regina::python::face<Pentachoron<4>, 4, int>, rbase::face)
         .def("vertex", &Pentachoron<4>::vertex,
-            pybind11::return_value_policy::reference)
+            pybind11::return_value_policy::reference, rbase::vertex)
         .def("edge",
             overload_cast<int>(&Pentachoron<4>::edge, pybind11::const_),
-            pybind11::return_value_policy::reference)
+            pybind11::return_value_policy::reference, rbase::edge)
         .def("edge",
             overload_cast<int, int>(&Pentachoron<4>::edge, pybind11::const_),
-            pybind11::return_value_policy::reference)
+            pybind11::return_value_policy::reference, rbase::edge_2)
         .def("triangle", &Pentachoron<4>::triangle,
-            pybind11::return_value_policy::reference)
+            pybind11::return_value_policy::reference, rbase::triangle)
         .def("tetrahedron", &Pentachoron<4>::tetrahedron,
-            pybind11::return_value_policy::reference)
-        .def("faceMapping", &regina::python::faceMapping<Pentachoron<4>, 4>)
-        .def("vertexMapping", &Pentachoron<4>::vertexMapping)
-        .def("edgeMapping", &Pentachoron<4>::edgeMapping)
-        .def("triangleMapping", &Pentachoron<4>::triangleMapping)
-        .def("tetrahedronMapping", &Pentachoron<4>::tetrahedronMapping)
-        .def("orientation", &Pentachoron<4>::orientation)
-        .def("facetInMaximalForest", &Pentachoron<4>::facetInMaximalForest)
+            pybind11::return_value_policy::reference, rbase::tetrahedron)
+        .def("faceMapping", &regina::python::faceMapping<Pentachoron<4>, 4>,
+            rbase::faceMapping)
+        .def("vertexMapping", &Pentachoron<4>::vertexMapping,
+            rbase::vertexMapping)
+        .def("edgeMapping", &Pentachoron<4>::edgeMapping, rbase::edgeMapping)
+        .def("triangleMapping", &Pentachoron<4>::triangleMapping,
+            rbase::triangleMapping)
+        .def("tetrahedronMapping", &Pentachoron<4>::tetrahedronMapping,
+            rbase::tetrahedronMapping)
+        .def("orientation", &Pentachoron<4>::orientation, rbase::orientation)
+        .def("facetInMaximalForest", &Pentachoron<4>::facetInMaximalForest,
+            rbase::facetInMaximalForest)
         .def_readonly_static("dimension", &Pentachoron<4>::dimension)
         .def_readonly_static("subdimension", &Pentachoron<4>::subdimension)
     ;
     regina::python::add_output(c);
     regina::python::add_eq_operators(c);
+
+    RDOC_SCOPE_END
 
     m.attr("Pentachoron4") = m.attr("Simplex4");
     m.attr("Face4_4") = m.attr("Simplex4");
