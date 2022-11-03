c212ce43c3fadf26959ab2f4d0029635cafc7823
Benjamin Burton <bab@maths.uq.edu.au>
2022-11-03 20:57:11 +1000

    Incorporate new docstrings.

--- python/maths/perm.cpp
+++ python/maths/perm.cpp
@@ -61,7 +61,7 @@ void addPerm(pybind11::module_& m, const char* name) {
         .def_static("isImagePack", &Perm<n>::isImagePack, rdoc::isImagePack)
         .def(pybind11::self * pybind11::self, rdoc::__mul)
         .def("inverse", &Perm<n>::inverse, rdoc::inverse)
-        .def("pow", &Perm<n>::pow, RDOC_TODO)
+        .def("pow", &Perm<n>::pow, rdoc::pow)
         .def("reverse", &Perm<n>::reverse, rdoc::reverse)
         .def("sign", &Perm<n>::sign, rdoc::sign)
         .def("__getitem__", &Perm<n>::operator[], rdoc::__array)
@@ -70,7 +70,7 @@ void addPerm(pybind11::module_& m, const char* name) {
         .def("isIdentity", &Perm<n>::isIdentity, rdoc::isIdentity)
         .def("inc", [](Perm<n>& p) {
             return p++;
-        }, RDOC_TODO)
+        }, rdoc::__inc)
         .def_static("rot", &Perm<n>::rot, rdoc::rot)
         .def_static("rand", static_cast<Perm<n>(&)(bool)>(Perm<n>::rand),
             pybind11::arg("even") = false, rdoc::rand)
@@ -79,7 +79,8 @@ void addPerm(pybind11::module_& m, const char* name) {
         .def("SnIndex", &Perm<n>::SnIndex, rdoc::SnIndex)
         .def("orderedSnIndex", &Perm<n>::orderedSnIndex,
             rdoc::orderedSnIndex)
-        .def("isConjugacyMinimal", &Perm<n>::isConjugacyMinimal, RDOC_TODO)
+        .def("isConjugacyMinimal", &Perm<n>::isConjugacyMinimal,
+            rdoc::isConjugacyMinimal)
         .def_readonly_static("codeType", &Perm<n>::codeType)
         .def_readonly_static("imageBits", &Perm<n>::imageBits)
         .def_readonly_static("imageMask", &Perm<n>::imageMask)
@@ -111,21 +112,24 @@ void addPerm(pybind11::module_& m, const char* name) {
 
 template <int n>
 void addPermClass(pybind11::module_& m, const char* name) {
-    auto c = pybind11::class_<PermClass<n>>(m, name, RDOC_TODO)
-        .def(pybind11::init<>(), RDOC_TODO)
-        .def(pybind11::init<const PermClass<n>&>(), RDOC_TODO)
-        .def("isIdentity", &PermClass<n>::isIdentity, RDOC_TODO)
-        .def("rep", &PermClass<n>::rep, RDOC_TODO)
-        .def("str", &PermClass<n>::str, RDOC_TODO)
+    RDOC_SCOPE_BEGIN(PermClass)
+
+    auto c = pybind11::class_<PermClass<n>>(m, name, rdoc_scope)
+        .def(pybind11::init<>(), rdoc::__default)
+        .def(pybind11::init<const PermClass<n>&>(), rdoc::__copy)
+        .def("isIdentity", &PermClass<n>::isIdentity, rdoc::isIdentity)
+        .def("rep", &PermClass<n>::rep, rdoc::rep)
         .def("inc", [](PermClass<n>& p) {
             return p++;
-        }, RDOC_TODO)
-        .def("__bool__", &PermClass<n>::operator bool, RDOC_TODO)
-        .def("centraliser", &PermClass<n>::centraliser, RDOC_TODO)
+        }, rdoc::__inc)
+        .def("__bool__", &PermClass<n>::operator bool, rdoc::__as_bool)
+        .def("centraliser", &PermClass<n>::centraliser, rdoc::centraliser)
         .def_readonly_static("count", &PermClass<n>::count)
     ;
-    regina::python::add_output_basic(c, RDOC_TODO);
-    regina::python::add_eq_operators(c, RDOC_TODO, RDOC_TODO);
+    regina::python::add_output_basic(c, rdoc::str);
+    regina::python::add_eq_operators(c, rdoc::__eq, rdoc::__ne);
+
+    RDOC_SCOPE_END
 }
 
 void addPerm(pybind11::module_& m) {
