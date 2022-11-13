5be5e69df5e4b66be098688c1571ba9854566c82
Benjamin Burton <bab@maths.uq.edu.au>
2022-11-13 16:00:38 +1100

    Move the contents of perm-impl.h into perm.cpp, which is the only file that uses it.

--- engine/maths/perm.cpp
+++ engine/maths/perm.cpp
@@ -30,9 +30,36 @@
  *                                                                        *
  **************************************************************************/
 
-#include "maths/perm-impl.h"
+#include "maths/perm.h"
 
 namespace regina {
+
+template <int n>
+void Perm<n>::precompute() {
+    std::scoped_lock lock(precomputeMutex);
+    if (invLower_)
+        return;
+
+    invLower_ = new ImagePack[lowerCount];
+    invUpper_ = new ImagePack[upperCount];
+
+    LowerSlice lower;
+    do {
+        ImagePack d = 0;
+        for (int i = 0; i < LowerSlice::length; ++i)
+            d |= (static_cast<ImagePack>(i) << (imageBits * lower.image[i]));
+        invLower_[lower.pack()] = d;
+    } while (lower.inc());
+
+    UpperSlice upper;
+    do {
+        ImagePack d = 0;
+        for (int i = 0; i < UpperSlice::length; ++i)
+            d |= (static_cast<ImagePack>(i + LowerSlice::length)
+                << (imageBits * upper.image[i]));
+        invUpper_[upper.pack()] = d;
+    } while (upper.inc());
+}
 
 template void Perm<8>::precompute();
 template void Perm<9>::precompute();
@@ -44,6 +71,79 @@ template void Perm<14>::precompute();
 template void Perm<15>::precompute();
 template void Perm<16>::precompute();
 
+template <int n>
+Perm<n>& Perm<n>::operator ++() {
+    // We implement a lexicographic "next permutation" algorithm; however,
+    // we want to increment according to Sn index, not orderedSn index.
+    // Thus some mod 2 fiddling around signs is required.
+
+    if (sign() > 0) {
+        // Going from even to odd, we always just swap the last two elements.
+        swapImages(n - 2, n - 1);
+        return *this;
+    }
+
+    bool needSignChange = true;
+
+    // The algorithm for lexicographical "next permutation": find the *last*
+    // point in the sequence of images where successive images increase:
+    // ..... p < q > r > s > ... > z
+    // We then change p to whichever of z,...,s,r,q first exceeds it,
+    // and then place the remaining images (along with p) in increasing order.
+    // The sign changes iff the length of the sequence q > r > ... > z
+    // is 0 or 1 (mod 4).
+
+    int q = (*this)[n - 1];
+    int p = (*this)[n - 2];
+    int pIdx = n - 2;
+    if (p < q) {
+        // The next permutation just swaps the last two images.
+        // However, since we started with an odd permutation, this will be
+        // going *backwards* in the Sn ordering.
+        // Swap them now, and carry on to get the *following* permutation.
+        swapImages(n - 2, n - 1);
+        std::swap(p, q);
+        needSignChange = false;
+    }
+    while (p > q && pIdx > 0) {
+        q = p;
+        --pIdx;
+        p = (*this)[pIdx];
+    }
+    if (p > q) {
+        // The sequence was entirely decreasing.
+        // We have reached the end of our iteration.
+        code_ = idCode_;
+        return *this;
+    }
+
+    // Reverse the sequence from pIdx onwards.
+    for (int i = 1; pIdx + i < n - i; ++i)
+        swapImages(pIdx + i, n - i);
+
+    // Now identify which element needs to be swapped with p.
+    for (int i = pIdx + 1; i < n; ++i)
+        if ((*this)[i] > p) {
+            swapImages(pIdx, i);
+            break;
+        }
+
+    // Did the sign change?
+    int seqRem = (n - 1 - pIdx) % 4;
+    if (seqRem == 0 || seqRem == 1) {
+        // The sign changed.
+        if (! needSignChange)
+            swapImages(n - 2, n - 1);
+    } else {
+        // The sign did not change.
+        if (needSignChange)
+            swapImages(n - 2, n - 1);
+    }
+
+    // Woof.
+    return *this;
+}
+
 template Perm<8>& Perm<8>::operator ++();
 template Perm<9>& Perm<9>::operator ++();
 template Perm<10>& Perm<10>::operator ++();
@@ -53,6 +153,114 @@ template Perm<13>& Perm<13>::operator ++();
 template Perm<14>& Perm<14>::operator ++();
 template Perm<15>& Perm<15>::operator ++();
 template Perm<16>& Perm<16>::operator ++();
+
+template <int n>
+std::vector<Perm<n>> PermClass<n>::centraliser() const {
+    // The centraliser could in the worst case have size (n!).
+    // Throw an exception on systems where size_t is not large enough for this.
+    //
+    // Note: the C++ standard guarantees that sizeof(size_t) ≥ 2.
+    // The only values we really expect to see in the wild are 2, 4 or ≥ 8,
+    // but this is a compile-time test and so we will be pedantic and check
+    // for unusual sizes (3, 5) also.
+    //
+    // Also, note: we exclude n == 8 on 16-bit systems because 8! is too
+    // large for a _signed_ 16-bit integer (even though it fits into uint8_t).
+    //
+    if constexpr (sizeof(size_t) == 2 && n >= 8)
+        throw FailedPrecondition("This system only supports 16-bit array "
+            "sizes, which is not large enough to hold all of S_n for n ≥ 8");
+    else if constexpr (sizeof(size_t) == 3 && n >= 11)
+        throw FailedPrecondition("This system only supports 24-bit array "
+            "sizes, which is not large enough to hold all of S_n for n ≥ 11");
+    else if constexpr (sizeof(size_t) == 4 && n >= 13)
+        throw FailedPrecondition("This system only supports 32-bit array "
+            "sizes, which is not large enough to hold all of S_n for n ≥ 13");
+    else if constexpr (sizeof(size_t) == 5 && n >= 15)
+        throw FailedPrecondition("This system only supports 40-bit array "
+            "sizes, which is not large enough to hold all of S_n for n ≥ 15");
+
+    size_t count = 1;
+
+    // Identify groups of cycles of the same size.
+    int nGroups = 0;
+    int groupCycle[n]; // cycle length for this group
+    int groupSize[n];  // number of cycles in this group
+    int groupStart[n]; // first cycle in this group
+
+    int start = 0;
+    int end = 1;
+    while (true) {
+        groupStart[nGroups] = start;
+        groupCycle[nGroups] = cycle_[start];
+        count *= groupCycle[nGroups];
+        while (end < nCycles_ && cycle_[start] == cycle_[end]) {
+            ++end;
+            count *= groupCycle[nGroups];
+        }
+        groupSize[nGroups] = end - start;
+        count *= regina::factorial(groupSize[nGroups]);
+
+        ++nGroups;
+
+        if (end == nCycles_)
+            break;
+        start = end;
+        ++end;
+    }
+
+    std::vector<Perm<n>> ans;
+    ans.reserve(count);
+
+    int cycleStart[n + 1];
+    cycleStart[0] = 0;
+    for (int i = 0; i < nCycles_; ++i)
+        cycleStart[i + 1] = cycleStart[i] + cycle_[i];
+
+    // Prepare to iterate through permutations.
+    int cycleMap[n];   // mapping between cycles
+    int cycleShift[n]; // shift to apply to each permutation
+    std::array<int, n> img;
+
+    for (int i = 0; i < nCycles_; ++i)
+        cycleMap[i] = i;
+    std::fill(cycleShift, cycleShift + nCycles_, 0);
+
+    int mapDepth, shiftDepth;
+    while (true) {
+        // Process this mapping between cycles.
+        while (true) {
+            // Process this set of shifts.
+            for (int i = 0; i < nCycles_; ++i)
+                for (int j = 0; j < cycle_[i]; ++j)
+                    img[cycleStart[i] + j] = cycleStart[cycleMap[i]] +
+                        ((j + cycleShift[i]) % cycle_[i]);
+            ans.emplace_back(img);
+
+            // Move to the next set of shifts.
+            for (shiftDepth = nCycles_ - 1; shiftDepth >= 0; --shiftDepth) {
+                if (++cycleShift[shiftDepth] < cycle_[shiftDepth])
+                    break;
+                cycleShift[shiftDepth] = 0;
+            }
+            if (shiftDepth < 0)
+                break;
+        }
+
+        // Move to the next mapping.
+        for (mapDepth = nGroups - 1; mapDepth >= 0; --mapDepth) {
+            if (groupSize[mapDepth] == 1)
+                continue;
+            if (std::next_permutation(cycleMap + groupStart[mapDepth],
+                    cycleMap + groupStart[mapDepth] + groupSize[mapDepth]))
+                break;
+        }
+        if (mapDepth < 0)
+            break;
+    }
+
+    return ans;
+}
 
 template std::vector<Perm<2>> PermClass<2>::centraliser() const;
 template std::vector<Perm<3>> PermClass<3>::centraliser() const;
