5f06795688ca1426c419650fbfc8d3ad7a64b558
Benjamin Burton <bab@maths.uq.edu.au>
2023-04-02 11:58:36 +1000

    Re-upgrade snappy version string in master.

--- preconfig/pypi/regina-config.h
+++ preconfig/pypi/regina-config.h
@@ -121,5 +121,5 @@
 #define PACKAGE_BUILD_STRING "Regina 7.3 (Self-contained Python wheel)"
 
 /* Define to the version of SnapPy that is bundled with Regina. */
-#define SNAPPY_VERSION "2.8"
+#define SNAPPY_VERSION "3.0.3"
 
