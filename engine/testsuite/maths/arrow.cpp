fc18b7059e4d309162a7032b29f6a21f3c022521
Benjamin Burton <bab@maths.uq.edu.au>
2025-05-29 08:33:40 +1000

    Add a few more tests for the standalone Arrow class.

--- engine/testsuite/maths/arrow.cpp
+++ engine/testsuite/maths/arrow.cpp
@@ -42,13 +42,29 @@ using regina::Laurent;
 class ArrowTest : public testing::Test {
     protected:
         Arrow zero {};
-        Arrow one { { {}, {0, {1}} } };
-        Arrow minusOne { { {}, {0, {-1}} } };
-        Arrow two { { {}, {0, {2}} } };
-        Arrow minusTwo { { {}, {0, {-2}} } };
-        Arrow virtualTrefoil { { {}, {-4, {1}} },
-                               { {1}, {-10, {-1,0,0,0,1}} } };
+        Arrow one { {{}, {0, {1}}} };
+        Arrow minusOne { {{}, {0, {-1}}} };
+        Arrow two { {{}, {0, {2}}} };
+        Arrow minusTwo { {{}, {0, {-2}}} };
+        Arrow k1 { {{1}, {0, {1}}} };
+        Arrow k2 { {{0,1}, {0, {1}}} };
+        Arrow virtualTrefoil {
+            {{}, {-4, {1}}},
+            {{1}, {-10, {-1,0,0,0,1}}} };
+        Arrow kishino {
+            {{}, {-4, {1,0,0,0,1,0,0,0,1}}},
+            {{0,1}, {0, {2}}},
+            {{2}, {-4, {-1,0,0,0,-2,0,0,0,-1}}} };
+
 };
+
+// We could probably do with a lot more tests here (though much of the
+// arithmetic is actually performed by Laurent<Integer>, which does already
+// come with its own arithmetic tests).
+//
+// Note also that we do have implicit testing of the Arrow class via the Link
+// tests, where we verify computations of many different arrow polynomials of
+// classical and virtual links.
 
 TEST_F(ArrowTest, output) {
     EXPECT_EQ(zero.str(), "0");
@@ -56,7 +72,11 @@ TEST_F(ArrowTest, output) {
     EXPECT_EQ(minusOne.str(), "-1");
     EXPECT_EQ(two.str(), "2");
     EXPECT_EQ(minusTwo.str(), "-2");
+    EXPECT_EQ(k1.str(), "K_1");
+    EXPECT_EQ(k2.str(), "K_2");
     EXPECT_EQ(virtualTrefoil.str(), "A^-4 + (A^-6 - A^-10) K_1");
+    EXPECT_EQ(kishino.str(),
+        "(A^4 + 1 + A^-4) + 2 K_2 + (-A^4 - 2 - A^-4) K_1^2");
 }
 
 TEST_F(ArrowTest, tightEncoding) {
@@ -65,5 +85,8 @@ TEST_F(ArrowTest, tightEncoding) {
     TightEncodingTest<Arrow>::verifyTightEncoding(minusOne);
     TightEncodingTest<Arrow>::verifyTightEncoding(two);
     TightEncodingTest<Arrow>::verifyTightEncoding(minusTwo);
+    TightEncodingTest<Arrow>::verifyTightEncoding(k1);
+    TightEncodingTest<Arrow>::verifyTightEncoding(k2);
     TightEncodingTest<Arrow>::verifyTightEncoding(virtualTrefoil);
+    TightEncodingTest<Arrow>::verifyTightEncoding(kishino);
 }
