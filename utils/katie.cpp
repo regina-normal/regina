0cd35f480d6f343815c4ab2b96223cc390f76632
raburke <35946131+raburke@users.noreply.github.com>
2025-08-19 12:29:06 +1000

    Updated copyright blurb for katie.cpp

--- utils/katie.cpp
+++ utils/katie.cpp
@@ -1,11 +1,31 @@
-//
-// Katie
-// Kirby Diagrams to Graphs and Triangulations
-//
-// Katie v2 Created by Rhuaidi Antonio Burke on 17 May 2024.
-// Katie v1 (originally "DGT") created by Rhuaidi Antonio Burke on 12 March 2021.
-// Copyright © 2024 Regina Development Team. All rights reserved.
-//
+/**************************************************************************
+ *                                                                        *
+ *  Katie - Kirby Diagrams to Graphs and Triangulations                   *
+ *  Builds a triangulation of a 3- or 4-manifold                          *
+ *  from a decorated link diagram                                         *
+ *                                                                        *
+ *  Created by Rhuaidi Antonio Burke                                      *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  As an exception, when this program is distributed through (i) the     *
+ *  App Store by Apple Inc.; (ii) the Mac App Store by Apple Inc.; or     *
+ *  (iii) Google Play by Google Inc., then that store may impose any      *
+ *  digital rights management, device limits and/or redistribution        *
+ *  restrictions that are required by its terms of service.               *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>. *
+ *                                                                        *
+ **************************************************************************/
 
 #include <iostream>
 #include <map>
