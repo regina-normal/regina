87b3d6d7a2b4d20c3cab352b237aca26b5dd06c7
alex-he <mr.a.he98@gmail.com>
2025-11-15 20:09:28 +1000

    Fix it.

--- python/link/link.cpp
+++ python/link/link.cpp
@@ -187,18 +187,12 @@ void addLink(pybind11::module_& m, pybind11::module_& internal) {
         .def_static("fromDT", [](const std::vector<int>& v) {
             return Link::fromDT(v.begin(), v.end());
         }, pybind11::arg("integers"), rdoc::fromDT_2)
-        //TODO Don't know why this breaks all of the following tests:
-        //  --> python-test
-        //  --> python-basic
-        //  --> python-sub
-        //  --> python-callback
-        //TODO
-        //.def_static("fromBraid", [](const std::string& s) {
-        //    return Link::fromBraid(s);
-        //}, pybind11::arg("word"), rdoc::fromBraid)
-        //.def_static("fromBraid", [](const std::vector<int>& v) {
-        //    return Link::fromBraid(v.begin(), v.end());
-        //}, pybind11::arg("integers"), rdoc::fromBraid_2)
+        .def_static("fromBraid", [](const std::string& s) {
+            return Link::fromBraid(s);
+        }, pybind11::arg("word"), rdoc::fromBraid)
+        .def_static("fromBraid", [](const std::vector<int>& v) {
+            return Link::fromBraid(v.begin(), v.end());
+        }, pybind11::arg("integers"), rdoc::fromBraid_2)
         .def_static("fromPD", [](const std::string& s) {
             return Link::fromPD(s);
         }, rdoc::fromPD)
