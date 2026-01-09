f1cdeccf3c8a3d22cd62d58fb48636087f4acc66
alex-he <mr.a.he98@gmail.com>
2026-01-09 15:49:46 +1000

    Preparation for testing countBoundaries().

---
+++ engine/testsuite/surface/boundaries.cpp
@@ -0,0 +1,43 @@
+
+/**************************************************************************
+ *                                                                        *
+ *  Regina - A Normal Surface Theory Calculator                           *
+ *  Test Suite                                                            *
+ *                                                                        *
+ *  Copyright (c) 1999-2025, Alex He                                      *
+ *  For further details contact Ben Burton (bab@debian.org).              *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or         *
+ *  modify it under the terms of the GNU General Public License as        *
+ *  published by the Free Software Foundation; either version 2 of the    *
+ *  License, or (at your option) any later version.                       *
+ *                                                                        *
+ *  As an exception, when this program is distributed through (i) the     *
+ *  App Store by Apple Inc.; (ii) the Mac App Store by Apple Inc.; or     *
+ *  (iii) Google Play by Google Inc., then that store may impose any      *
+ *  digital rights management, device limits and/or redistribution        *
+ *  restrictions that are required by its terms of service.               *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *  General Public License for more details.                              *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>. *
+ *                                                                        *
+ **************************************************************************/
+
+#include "surface/normalsurfaces.h"
+#include "triangulation/example3.h"
+#include "triangulation/dim3.h"
+
+#include "testhelper.h"
+
+using regina::Example;
+using regina::NormalSurfaces;
+using regina::Triangulation;
+
+TEST(BoundariesTest, countBoundaries) {
+    //TODO
+}
