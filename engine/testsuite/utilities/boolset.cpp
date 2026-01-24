ad65fb6c9a9bf4a90c4e76e3f7d51ec0e66f4ae1
Benjamin Burton <bab@maths.uq.edu.au>
2026-01-25 07:13:19 +1100

    Finish off tests for BoolSet.

--- engine/testsuite/utilities/boolset.cpp
+++ engine/testsuite/utilities/boolset.cpp
@@ -29,6 +29,7 @@
  **************************************************************************/
 
 #include <array>
+#include <cctype>
 #include "utilities/boolset.h"
 
 #include "testhelper.h"
@@ -46,20 +47,204 @@ class BoolSetTest : public testing::Test {
             BoolSet(false, true), BoolSet(true, false), BoolSet(true, true) };
 };
 
+TEST_F(BoolSetTest, byteCode) {
+    EXPECT_EQ(bsTrue.byteCode(), 1);
+    EXPECT_EQ(bsFalse.byteCode(), 2);
+    EXPECT_EQ(bsBoth.byteCode(), 3);
+    EXPECT_EQ(bsNone.byteCode(), 0);
+
+    for (const auto x : cases) {
+        EXPECT_EQ(BoolSet::fromByteCode(x.byteCode()), x);
+
+        for (const auto y : cases) {
+            {
+                BoolSet tmp = x;
+                EXPECT_TRUE(tmp.setByteCode(y.byteCode()));
+                EXPECT_EQ(tmp, y);
+            }
+        }
+
+        BoolSet tmp = x;
+        EXPECT_FALSE(tmp.setByteCode(-1));
+        EXPECT_FALSE(tmp.setByteCode(4));
+        EXPECT_EQ(tmp, x);
+    }
+}
+
+TEST_F(BoolSetTest, comparisons) {
+    for (const auto x : cases) {
+        auto xc = x.byteCode();
+        for (const auto y : cases) {
+            auto yc = y.byteCode();
+            EXPECT_EQ(x == y, xc == yc);
+            EXPECT_EQ(x != y, xc != yc);
+            EXPECT_EQ(x <= y, (xc & yc) == xc);
+            EXPECT_EQ(x >= y, (xc & yc) == yc);
+            EXPECT_EQ(x < y, xc != yc && (xc & yc) == xc);
+            EXPECT_EQ(x > y, xc != yc && (xc & yc) == yc);
+        }
+    }
+}
+
+TEST_F(BoolSetTest, constructors) {
+    EXPECT_EQ(BoolSet(), bsNone);
+    EXPECT_EQ(BoolSet(true), bsTrue);
+    EXPECT_EQ(BoolSet(false), bsFalse);
+
+    for (const auto x : cases)
+        EXPECT_EQ(BoolSet(x), x);
+
+    for (int t = 0; t < 2; ++t)
+        for (int f = 0; f < 2; ++f) {
+            BoolSet tmp(static_cast<bool>(t), static_cast<bool>(f));
+            EXPECT_EQ(tmp.hasTrue(), static_cast<bool>(t));
+            EXPECT_EQ(tmp.hasFalse(), static_cast<bool>(f));
+        }
+}
+
+TEST_F(BoolSetTest, assignment) {
+    for (const auto x : cases) {
+        {
+            BoolSet tmp = x;
+            tmp = true;
+            EXPECT_EQ(tmp, bsTrue);
+        }
+        {
+            BoolSet tmp = x;
+            tmp = false;
+            EXPECT_EQ(tmp, bsFalse);
+        }
+
+        for (const auto y : cases) {
+            BoolSet tmp = x;
+            tmp = y;
+            EXPECT_EQ(tmp, y);
+        }
+    }
+}
+
+TEST_F(BoolSetTest, simpleTests) {
+    EXPECT_FALSE(bsNone.full());
+    EXPECT_FALSE(bsNone.hasTrue());
+    EXPECT_FALSE(bsNone.hasFalse());
+    EXPECT_FALSE(bsNone.contains(true));
+    EXPECT_FALSE(bsNone.contains(false));
+
+    EXPECT_FALSE(bsTrue.full());
+    EXPECT_TRUE(bsTrue.hasTrue());
+    EXPECT_FALSE(bsTrue.hasFalse());
+    EXPECT_TRUE(bsTrue.contains(true));
+    EXPECT_FALSE(bsTrue.contains(false));
+
+    EXPECT_FALSE(bsFalse.full());
+    EXPECT_FALSE(bsFalse.hasTrue());
+    EXPECT_TRUE(bsFalse.hasFalse());
+    EXPECT_FALSE(bsFalse.contains(true));
+    EXPECT_TRUE(bsFalse.contains(false));
+
+    EXPECT_TRUE(bsBoth.full());
+    EXPECT_TRUE(bsBoth.hasTrue());
+    EXPECT_TRUE(bsBoth.hasFalse());
+    EXPECT_TRUE(bsBoth.contains(true));
+    EXPECT_TRUE(bsBoth.contains(false));
+}
+
+TEST_F(BoolSetTest, simpleOperations) {
+    for (const auto x : cases) {
+        {
+            BoolSet tmp = x;
+            tmp.clear();
+            EXPECT_EQ(tmp, bsNone);
+        }
+        {
+            BoolSet tmp = x;
+            tmp.fill();
+            EXPECT_EQ(tmp, bsBoth);
+        }
+        {
+            BoolSet tmp = x;
+            tmp.insertTrue();
+            EXPECT_EQ(tmp, x.hasFalse() ? bsBoth : bsTrue);
+        }
+        {
+            BoolSet tmp = x;
+            tmp.insertFalse();
+            EXPECT_EQ(tmp, x.hasTrue() ? bsBoth : bsFalse);
+        }
+        {
+            BoolSet tmp = x;
+            tmp.removeTrue();
+            EXPECT_EQ(tmp, x.hasFalse() ? bsFalse : bsNone);
+        }
+        {
+            BoolSet tmp = x;
+            tmp.removeFalse();
+            EXPECT_EQ(tmp, x.hasTrue() ? bsTrue : bsNone);
+        }
+    }
+}
+
+TEST_F(BoolSetTest, logicalOperations) {
+    for (const auto x : cases) {
+        EXPECT_EQ((~x).byteCode() + x.byteCode(), 3);
+
+        for (const auto y : cases) {
+            EXPECT_EQ((x | y).byteCode(), x.byteCode() | y.byteCode());
+            EXPECT_EQ((x & y).byteCode(), x.byteCode() & y.byteCode());
+            EXPECT_EQ((x ^ y).byteCode(), x.byteCode() ^ y.byteCode());
+
+            {
+                BoolSet tmp = x;
+                tmp |= y;
+                EXPECT_EQ(tmp.byteCode(), x.byteCode() | y.byteCode());
+            }
+            {
+                BoolSet tmp = x;
+                tmp &= y;
+                EXPECT_EQ(tmp.byteCode(), x.byteCode() & y.byteCode());
+            }
+            {
+                BoolSet tmp = x;
+                tmp ^= y;
+                EXPECT_EQ(tmp.byteCode(), x.byteCode() ^ y.byteCode());
+            }
+        }
+    }
+}
+
 TEST_F(BoolSetTest, stringCode) {
     EXPECT_STREQ(bsTrue.stringCode(), "T-");
     EXPECT_STREQ(bsFalse.stringCode(), "-F");
     EXPECT_STREQ(bsBoth.stringCode(), "TF");
     EXPECT_STREQ(bsNone.stringCode(), "--");
 
-    for (auto x : cases) {
-        for (auto y : cases) {
+    for (const auto x : cases) {
+        for (const auto y : cases) {
+            std::string code = y.stringCode();
+            ASSERT_EQ(code.size(), 2);
             {
                 BoolSet tmp = x;
-                EXPECT_TRUE(tmp.setStringCode(y.stringCode()));
+                EXPECT_TRUE(tmp.setStringCode(code));
                 EXPECT_EQ(tmp, y);
             }
-            // TODO: Play with lower-case
+            code[0] = std::tolower(code[0]);
+            {
+                BoolSet tmp = x;
+                EXPECT_TRUE(tmp.setStringCode(code));
+                EXPECT_EQ(tmp, y);
+            }
+            code[1] = std::tolower(code[1]);
+            {
+                BoolSet tmp = x;
+                EXPECT_TRUE(tmp.setStringCode(code));
+                EXPECT_EQ(tmp, y);
+            }
+            code[0] = std::toupper(code[0]);
+            {
+                BoolSet tmp = x;
+                EXPECT_TRUE(tmp.setStringCode(code));
+                EXPECT_EQ(tmp, y);
+            }
         }
 
         BoolSet tmp = x;
@@ -73,4 +258,3 @@ TEST_F(BoolSetTest, stringCode) {
         EXPECT_EQ(tmp, x);
     }
 }
-
