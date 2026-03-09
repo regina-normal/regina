f3ba44375fdfe3298f393854c83110b599885ad8
Benjamin Burton <bab@maths.uq.edu.au>
2026-03-09 17:29:55 +1000

    Fix typo.

--- engine/utilities/snapshot.h
+++ engine/utilities/snapshot.h
@@ -201,7 +201,7 @@ class SnapshotWriteError : public std::exception {
  * This Snapshot class itself should remain forever behind the scenes:
  * end users cannot access it and should not know about it.
  * Images should always work through their base class Snapshottable<T>,
- * and viewers shoudl always work through SnapshotRef<T>.
+ * and viewers should always work through SnapshotRef<T>.
  *
  * \nopython
  *
